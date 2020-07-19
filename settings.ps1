b76aab8203cd54acb4f82e5f999e6c91d63ee3a7
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-07-19 23:07:42 +0200

    Updated mods and forge

--- settings.ps1
+++ settings.ps1
@@ -23,7 +23,7 @@ $MODPACK_VERSION = "0.1.5"
 # Only used by the Changelog Generator
 $LAST_MODPACK_VERSION = "0.1.4"
 
-$FORGE_VERSION = 32.0.66
+$FORGE_VERSION = 32.0.69
 
 $CLIENT_FILENAME = "Enigmatica6"
 
@@ -39,7 +39,7 @@ $GAME_VERSIONS = @(7892) # 7675 is 1.15.1, 7722 is 1.15.2, 7892 is 1.16.1
 $CLIENT_CHANGELOG_TYPE = "markdown"
 
 $CLIENT_CHANGELOG = "### Enigmatica 6 v$MODPACK_VERSION <br /> <br />" +
-"Using Forge-1.16.1-32.0.66 <br /> <br /> " +
+"Using Forge-1.16.1-32.0.69 <br /> <br /> " +
 "The Changelog is currently being written, it should be done within 10 minutes. "
 
 
