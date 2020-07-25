ec383f5ddeef20af3c232f73898c64d25c2f21bc
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-07-25 22:43:45 +0200

    Possibly fixes #18

--- kubejs/data/enigmatica/kubejs/base/tags/items/forge/wool.js
+++ kubejs/data/enigmatica/kubejs/base/tags/items/forge/wool.js
@@ -1,4 +1,4 @@
-events.listen('server.datapack.tags.items', function (event) {
+events.listen('item.tags', function (event) {
     for (var i = 0; i < colors.length; i++) {
         event.get('forge:wool').add('minecraft:' + colors[i] + '_wool');
     }
