ec1974dfe152c43c1fe7f0144f87d22e71ab00fb
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-07-25 23:38:49 +0200

    Update removals.js

--- kubejs/data/enigmatica/kubejs/base/unification/removals.js
+++ kubejs/data/enigmatica/kubejs/base/unification/removals.js
@@ -1,7 +1,7 @@
 //priority: 950
 events.listen('recipes', function (event) {
     // Remove Prismarine dupe bug
-    // event.remove({ id: 'jaopca:mekanism.dust_to_material.prismarine' });
+    event.remove({ id: 'jaopca:mekanism.dust_to_material.prismarine' });
     // event.remove({ id: 'integrateddynamics:mechanical_squeezer/ore/prismarine_crystals' });
     event.remove({ type: 'mekanism:enriching', output: 'minecraft:prismarine_crystals' });
 
