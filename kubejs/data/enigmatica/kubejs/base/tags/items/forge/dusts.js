c601f73ea5c914b4fdb372a708db98ed3abe7a56
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-07-29 20:46:35 +0200

    Fixes #47

--- kubejs/data/enigmatica/kubejs/base/tags/items/forge/dusts.js
+++ kubejs/data/enigmatica/kubejs/base/tags/items/forge/dusts.js
@@ -12,4 +12,7 @@ events.listen('item.tags', function (event) {
         .add('immersiveengineering:dust_coke')
         .add('immersiveengineering:dust_hop_graphite')
         .add('immersiveengineering:dust_saltpeter');
+		
+	event.get('forge:dusts').remove('minecraft:prismarine_shard');
+    event.get('forge:dusts/prismarine').remove('minecraft:prismarine_shard');
 });
