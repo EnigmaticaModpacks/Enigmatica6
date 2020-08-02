e619376a7364f812a582bc89a38c48898746d6af
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-08-02 22:30:30 +0200

    Updated Forge to 32.0.88

--- settings.ps1
+++ settings.ps1
@@ -18,12 +18,12 @@ $CURSEFORGE_PROJECT_ID = 389471
 # =====================================================================//
 
 # Default: "1.0.0"
-$MODPACK_VERSION = "0.2.3"
+$MODPACK_VERSION = "0.2.4"
 
 # Only used by the Changelog Generator
-$LAST_MODPACK_VERSION = "0.2.2"
+$LAST_MODPACK_VERSION = "0.2.3"
 
-$FORGE_VERSION = 32.0.75
+$FORGE_VERSION = 32.0.88
 
 $CLIENT_FILENAME = "Enigmatica6"
 
@@ -39,7 +39,7 @@ $GAME_VERSIONS = @(7892) # 7675 is 1.15.1, 7722 is 1.15.2, 7892 is 1.16.1
 $CLIENT_CHANGELOG_TYPE = "markdown"
 
 $CLIENT_CHANGELOG = "### Enigmatica 6 v$MODPACK_VERSION <br /> <br />" +
-"Using Forge-1.16.1-32.0.75 <br /> <br /> " +
+"Using Forge-1.16.1-32.0.88 <br /> <br /> " +
 "The Changelog is currently being written, it should be done within 10 minutes. "
 
 
