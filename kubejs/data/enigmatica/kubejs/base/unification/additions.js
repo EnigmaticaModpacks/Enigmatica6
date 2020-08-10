f6de1fa7581d3c6b6f8d4b18fbb4f0b26c373950
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-08-10 23:36:24 +0200

    Nearly done with #101

--- kubejs/data/enigmatica/kubejs/base/unification/additions.js
+++ kubejs/data/enigmatica/kubejs/base/unification/additions.js
@@ -1,18 +1,102 @@
 //priority: 975
 events.listen('recipes', function (event) {
+    console.log('Starting material unification.');
+    appliedenergistics2_gem_processing(event);
     materialsToUnify.forEach(function (material) {
+        console.log('Unifying ' + material + '...');
         enigmatica_ore_deposit_processing(event, material);
         //occultism_ore_ingot_crushing(event, material);
         appliedenergistics2_ore_ingot_gem_crushing(event, material);
+        immersiveengineering_gem_ore_processing(event, material);
     });
 });
+
+function appliedenergistics2_gem_processing(event) {
+    var certus_quartz = 'certus_quartz';
+    var charged_certus_quartz = 'charged_certus_quartz';
+
+    event.recipes.immersiveengineering.crusher({
+        secondaries: [],
+        result: {
+            base_ingredient: {
+                tag: 'forge:gems/' + certus_quartz
+            },
+            count: 2
+        },
+        input: {
+            tag: 'forge:ores/' + certus_quartz
+        },
+        energy: 2000
+    });
+
+    event.recipes.immersiveengineering.crusher({
+        secondaries: [],
+        result: {
+            base_ingredient: {
+                tag: 'forge:gems/' + charged_certus_quartz
+            },
+            count: 2
+        },
+        input: {
+            tag: 'forge:ores/' + charged_certus_quartz
+        },
+        energy: 2000
+    });
+
+    event.recipes.immersiveengineering.crusher({
+        secondaries: [],
+        result: {
+            tag: 'forge:dusts/' + certus_quartz
+        },
+        input: {
+            tag: 'forge:gems/' + charged_certus_quartz
+        },
+        energy: 2000
+    });
+
+    event.recipes.mekanism.crushing({
+        input: {
+            ingredient: { tag: 'forge:gems/' + charged_certus_quartz }
+        },
+        output: { item: getPreferredItemInTag(ingredient.of('#forge:dusts/' + certus_quartz)).id, count: 1 }
+    });
+}
+
+function immersiveengineering_gem_ore_processing(event, material) {
+    var gemTag = ingredient.of('#forge:gems/' + material);
+    var gem = getPreferredItemInTag(gemTag).id;
+
+    var oreTag = ingredient.of('#forge:ores/' + material);
+    var ore = getPreferredItemInTag(oreTag).id;
+
+    var dustTag = ingredient.of('#forge:dusts/' + material);
+    var dust = getPreferredItemInTag(dustTag).id;
+
+    if (gem === air || dust === air) {
+        return;
+    }
+
+    event.recipes.immersiveengineering.crusher({
+        secondaries: [],
+        result: {
+            item: dust
+        },
+        input: {
+            tag: 'forge:gems/' + material
+        },
+        energy: 2000
+    });
+
+    if (ore === air) {
+        return;
+    }
+}
 
 function enigmatica_ore_deposit_processing(event, material) {
     var oreDepositTag = ingredient.of('#forge:ore_deposits/' + material);
     var oreDeposit = oreDepositTag.first.id;
 
     var nuggetTag = ingredient.of('#forge:nuggets/' + material);
-    var nugget = getPreferredItemInTag(nuggetTag).id;
 
     var dustTag = ingredient.of('#forge:dusts/' + material);
     var dust = getPreferredItemInTag(dustTag).id;
@@ -25,8 +109,8 @@ function enigmatica_ore_deposit_processing(event, material) {
         event.recipes.minecraft.smelting(dustTag, oreDepositTag).xp(1.0);
         event.recipes.minecraft.blasting(dustTag, oreDepositTag).xp(1.0);
     } else {
-        event.recipes.minecraft.smelting(nugget, oreDepositTag).xp(1.0);
-        event.recipes.minecraft.blasting(nugget, oreDepositTag).xp(1.0);
+        event.recipes.minecraft.smelting(nuggetTag, oreDepositTag).xp(1.0);
+        event.recipes.minecraft.blasting(nuggetTag, oreDepositTag).xp(1.0);
     }
 
     event.recipes.mekanism.enriching({
@@ -98,6 +182,7 @@ function appliedenergistics2_ore_ingot_gem_crushing(event, material) {
 
     var gemTag = ingredient.of('#forge:gems/' + material);
     var gem = getPreferredItemInTag(gemTag).id;
+    console.log('gem: ' + gem);
 
     if (oreDeposit === air || dust === air) {
         return;
@@ -153,8 +238,11 @@ function appliedenergistics2_ore_ingot_gem_crushing(event, material) {
     });
 }
 
-function occultism_ore_ingot_crushing(event, material) {
-    var blacklistedMaterials = ['redstone', 'lapis', 'emerald', 'diamond', 'quartz', 'coal'];
+function occultism_ore_ingot_crushing(event, material, blacklistedMaterials) {
+    if (!blacklistedMaterials) {
+        blacklistedMaterials = ['redstone', 'lapis', 'emerald', 'diamond', 'quartz', 'coal'];
+    }
+
     for (var i = 0; i < blacklistedMaterials.length; i++) {
         if (blacklistedMaterials[i] === material) {
             return;
