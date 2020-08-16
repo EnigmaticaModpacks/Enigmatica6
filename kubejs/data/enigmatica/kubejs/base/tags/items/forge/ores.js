e4658ff98b7b169b9a8105f20dcbd1143b02647a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-08-16 20:14:23 +0200

    Re-added tags to unused ores

--- kubejs/data/enigmatica/kubejs/base/tags/items/forge/ores.js
+++ kubejs/data/enigmatica/kubejs/base/tags/items/forge/ores.js
@@ -3,22 +3,4 @@ events.listen('item.tags', function (event) {
     // event.get('forge:ores/dimensional').add(oreDimensional);
     // event.get('forge:ores').add('create:zinc_ore');
     // event.get('forge:ores').add('occultism:iesnium_ore');
-
-    // Add IE Ores
-
-    var unusedOres = [
-        // 'create:copper_ore',
-        'mekanism:copper_ore',
-        'mekanism:lead_ore',
-        'mekanism:uranium_ore',
-        // 'occultism:copper_ore',
-        // 'occultism:silver_ore',
-        'tmechworks:aluminum_ore',
-        'tmechworks:copper_ore'
-    ];
-
-    event.get('forge:ores').remove(unusedOres);
-    event.get('forge:ores/copper').remove(['mekanism:copper_ore', 'tmechworks:copper_ore']);
-    // event.get('forge:ores/silver').remove('occultism:silver_ore');
-    event.get('forge:ores/aluminum').remove('tmechworks:aluminum_ore');
 });
