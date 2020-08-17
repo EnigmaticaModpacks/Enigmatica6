f7d6f5d0401720de3ac6c1dd025cfba37808fecb
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-08-17 19:25:12 +0200

    #132

--- kubejs/data/enigmatica/kubejs/base/recipetypes/powah/energizing.js
+++ kubejs/data/enigmatica/kubejs/base/recipetypes/powah/energizing.js
@@ -1,14 +1,21 @@
-// events.listen('recipes', function (event) {
-//     event.recipes.powah.energizing({
-//         ingredients: [
-//             { item: 'rftoolspower:dimensionalcell_simple' },
-//             { item: 'powah:capacitor_blazing' },
-//             { item: 'powah:capacitor_blazing' }
-//         ],
-//         energy: 250000,
-//         result: {
-//             item: 'tetra:magmatic_cell',
-//             count: 1
-//         }
-//     });
-// });
+events.listen('recipes', function (event) {
+    // event.recipes.powah.energizing({
+    //     ingredients: [
+    //         { item: 'rftoolspower:dimensionalcell_simple' },
+    //         { item: 'powah:capacitor_blazing' },
+    //         { item: 'powah:capacitor_blazing' }
+    //     ],
+    //     energy: 250000,
+    //     result: {
+    //         item: 'tetra:magmatic_cell',
+    //         count: 1
+    //     }
+    // });
+    event.recipes.powah.energizing({
+        ingredients: [{ tag: 'forge:gems/certus_quartz' }],
+        energy: 10000,
+        result: {
+            item: getPreferredItemInTag(ingredient.of('#forge:gems/charged_certus_quartz')).id
+        }
+    });
+});
