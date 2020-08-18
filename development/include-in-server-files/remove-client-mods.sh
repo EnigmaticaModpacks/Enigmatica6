43d51847c1fefd661f8006b1eff838ce0d9aff45
brckts <brckts@protonmail.com>
2020-08-18 12:57:32 +0200

    fixed remove-client-mods removing building-gadgets

--- development/include-in-server-files/remove-client-mods.sh
+++ development/include-in-server-files/remove-client-mods.sh
@@ -5,6 +5,6 @@ modfolder="$(dirname "$0")/mods"
 
 for client_mod in $client_mods; do
 
-	ls $modfolder | grep -i $client_mod | xargs -I{} rm -v "$modfolder/{}"
+	ls $modfolder | grep -i "^$client_mod" | xargs -I{} rm -v "$modfolder/{}"
 
 done
