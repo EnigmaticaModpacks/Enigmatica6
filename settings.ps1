91294eefa9715d53ab3904914b998531c56e9f12
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-08-21 21:37:42 +0200

    Update settings.ps1

--- settings.ps1
+++ settings.ps1
@@ -18,10 +18,10 @@ $CURSEFORGE_PROJECT_ID = 389471
 # =====================================================================//
 
 # Default: "1.0.0"
-$MODPACK_VERSION = "0.2.12"
+$MODPACK_VERSION = "0.2.13"
 
 # Only used by the Changelog Generator
-$LAST_MODPACK_VERSION = "0.2.11"
+$LAST_MODPACK_VERSION = "0.2.12"
 
 $FORGE_VERSION = 32.0.108
 
