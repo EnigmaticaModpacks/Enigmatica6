d66a53061be12406cd0eda4fdc9d2860ff43185c
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-08-24 21:44:28 +0200

    #187

--- kubejs/startup/item_modifiers/jei_descriptions.js
+++ kubejs/startup/item_modifiers/jei_descriptions.js
@@ -1,6 +1,6 @@
 events.listen('jei.information', (event) => {
     event.add(
         'appliedenergistics2:quantum_entangled_singularity',
-        'To create drop 1 Singularity and 1 Ender Dust (From AppliedEnergistics) and cause an explosion within range of the items.'
+        'To create drop 1 Singularity and 1 Ender Dust (From Applied Energistics) and cause an explosion within range of the items.'
     );
 });
