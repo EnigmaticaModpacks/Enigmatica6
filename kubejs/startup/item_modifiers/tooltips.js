5736cf9eb9caed621a0d5f931120d0a239d90a12
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-08-24 21:44:59 +0200

    tooltip optimizations #187

--- kubejs/startup/item_modifiers/tooltips.js
+++ kubejs/startup/item_modifiers/tooltips.js
@@ -2,9 +2,7 @@ events.listen('client.item_tooltip', (event) => {
     switch (event.item.id) {
         case 'appliedenergistics2:quantum_entangled_singularity':
             event.add(
-                'To create drop 1 Singularity and 1 Ender Dust (' +
-                    text.red('From Applied Energistics') +
-                    ') and cause an explosion within range of the items.'
+                'To create drop 1 Singularity and 1 Ender Dust (From Applied Energistics) and cause an explosion within range of the items.'
             );
     }
 });
