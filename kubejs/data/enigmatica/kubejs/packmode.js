7e940ee28d5788fea41112ce5f1c398297ef4e6a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-08-28 21:08:42 +0200

    Moved packmode.json file to root for future mod compat

--- kubejs/data/enigmatica/kubejs/packmode.js
+++ kubejs/data/enigmatica/kubejs/packmode.js
@@ -1,14 +1,14 @@
 //priority: 1000
 
 events.listen('recipes', function (event) {
-    var config = json.read('config/packmode.json');
+    var config = json.read('packmode.json');
     if (!config || !config.packmode) {
         config = {
             packmode: 'normal',
             message: 'Valid modes are normal and expert.'
         };
-        json.write('config/packmode.json', config);
-        console.log('Wrote new config/packmode.json');
+        json.write('packmode.json', config);
+        console.log('Wrote new packmode.json');
     }
     global.packmode = config.packmode;
     console.log('Current packmode is: ' + global.packmode);
