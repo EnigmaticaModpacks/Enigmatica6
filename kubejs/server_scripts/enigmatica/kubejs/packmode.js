6b614cfe582cde0feda792639768d467f25338d5
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-10-24 23:38:53 +0200

    adapted packmode scripts to configswapper

--- kubejs/server_scripts/enigmatica/kubejs/packmode.js
+++ kubejs/server_scripts/enigmatica/kubejs/packmode.js
@@ -1,14 +1,15 @@
 //priority: 1000
 
 events.listen('recipes', function (event) {
-    var config = json.read('packmode.json');
+    var configName = 'mode.json';
+    var config = json.read(configName);
     if (!config || !config.packmode) {
         config = {
             packmode: 'normal',
             message: 'Valid modes are normal and expert.'
         };
-        json.write('packmode.json', config);
-        console.log('Wrote new packmode.json');
+        json.write(configName, config);
+        console.log('Created new ' + configName);
     }
     global.packmode = config.packmode;
     console.log('Current packmode is: ' + global.packmode);
