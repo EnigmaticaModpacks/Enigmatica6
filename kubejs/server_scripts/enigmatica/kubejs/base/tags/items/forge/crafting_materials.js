a69094a22e5859054f3f2ed2b4181846cbd86dfc
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-11-05 22:25:34 +0100

    removed productive bees :(

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/crafting_materials.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/crafting_materials.js
@@ -4,6 +4,5 @@ events.listen('item.tags', function (event) {
         .add('mekanism:enriched_diamond')
         .add('botania:mana_diamond')
         .add('titanium:diamond_gear')
-        .add('productivebees:honeycomb_diamond')
         .add('minecraft:diamond_block');
 });
