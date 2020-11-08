58d151daddd012209a802ba2222af425c433b4db
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-11-08 23:47:20 +0100

    only missing cinnabar dust & apatite dust from thermal unification now #364

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/gems.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/gems.js
@@ -1,9 +1,8 @@
 events.listen('item.tags', function (event) {
     var gems = 'forge:gems';
     var gems_ender = gems + '/ender';
-    event.get('forge:gems').add('rftoolsbase:dimensionalshard');
-    event.get('forge:gems/dimensional').add('rftoolsbase:dimensionalshard');
 
+    event.get(gems).add('rftoolsbase:dimensionalshard').add('minecraft:ender_pearl');
     event.get(gems_ender).add('minecraft:ender_pearl');
-    event.get(gems).add('minecraft:ender_pearl');
+    event.get('forge:gems/dimensional').add('rftoolsbase:dimensionalshard');
 });
