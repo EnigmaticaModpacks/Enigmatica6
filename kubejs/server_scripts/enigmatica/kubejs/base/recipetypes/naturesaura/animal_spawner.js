46c5f7fa48947264ad911fdc1eb7e78877d363a4
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-11-09 21:27:09 +0100

    refactor of animal_spawner (NaturesAura summoning)

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/naturesaura/animal_spawner.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/naturesaura/animal_spawner.js
@@ -1,151 +1,73 @@
-events.listen('recipes', function(event) {
-    event.recipes.naturesaura.animal_spawner({
-        "type": "naturesaura:animal_spawner",
-        "ingredients": [{
-                "item": "naturesaura:birth_spirit"
+events.listen('recipes', (event) => {
+    var data = {
+        recipes: [
+            {
+                ingredients: ['ars_nouveau:mana_gem', 'naturesaura:gold_leaf'],
+                entity: 'ars_nouveau:carbuncle',
+                aura: 100000,
+                time: 100
             },
             {
-                "item": "ars_nouveau:mana_gem"
+                ingredients: ['ars_nouveau:mana_gem', 'naturesaura:ancient_sapling'],
+                entity: 'ars_nouveau:sylph',
+                aura: 100000,
+                time: 100
             },
             {
-                "item": "naturesaura:gold_leaf"
+                ingredients: ['minecraft:cod', 'minecraft:iron_bars'],
+                entity: 'quark:crab',
+                aura: 30000,
+                time: 40
+            },
+            {
+                ingredients: ['minecraft:spider_eye', 'minecraft:lily_pad'],
+                entity: 'quark:frog',
+                aura: 30000,
+                time: 40
+            },
+            {
+                ingredients: ['minecraft:leather', 'minecraft:coal'],
+                entity: 'quark:foxhound',
+                aura: 150000,
+                time: 120
+            },
+            {
+                ingredients: ['thermal:blitz_rod', 'thermal:blitz_powder'],
+                entity: 'thermal:blitz',
+                aura: 150000,
+                time: 120
+            },
+            {
+                ingredients: ['thermal:blizz_rod', 'thermal:blizz_powder'],
+                entity: 'thermal:blizz',
+                aura: 150000,
+                time: 120
+            },
+            {
+                ingredients: ['thermal:basalz_rod', 'thermal:basalz_powder'],
+                entity: 'thermal:basalz',
+                aura: 150000,
+                time: 120
             }
-        ],
-        "entity": "ars_nouveau:carbuncle",
-        "aura": 100000,
-        "time": 100
-    });
-});
-
-events.listen('recipes', function(event) {
-    event.recipes.naturesaura.animal_spawner({
-        "type": "naturesaura:animal_spawner",
-        "ingredients": [{
-                "item": "naturesaura:birth_spirit"
-            },
-            {
-                "item": "ars_nouveau:mana_gem"
-            },
-            {
-                "item": "naturesaura:ancient_sapling"
-            }
-        ],
-        "entity": "ars_nouveau:sylph",
-        "aura": 100000,
-        "time": 100
-    });
-});
-
-events.listen('recipes', function(event) {
-    event.recipes.naturesaura.animal_spawner({
-        "type": "naturesaura:animal_spawner",
-        "ingredients": [{
-                "item": "naturesaura:birth_spirit"
-            },
-            {
-                "item": "minecraft:cod"
-            },
-            {
-                "item": "minecraft:iron_bars"
-            }
-        ],
-        "entity": "quark:crab",
-		"aura": 30000,
-		"time": 40
-    });
-});
-
-events.listen('recipes', function(event) {
-    event.recipes.naturesaura.animal_spawner({
-        "type": "naturesaura:animal_spawner",
-        "ingredients": [{
-                "item": "naturesaura:birth_spirit"
-            },
-            {
-                "item": "minecraft:spider_eye"
-            },
-            {
-                "item": "minecraft:lily_pad"
-            }
-        ],
-        "entity": "quark:frog",
-		"aura": 30000,
-		"time": 40
-    });
-});
-
-events.listen('recipes', function(event) {
-    event.recipes.naturesaura.animal_spawner({
-        "type": "naturesaura:animal_spawner",
-        "ingredients": [{
-                "item": "naturesaura:birth_spirit"
-            },
-            {
-                "item": "minecraft:leatherb"
-            },
-            {
-                "item": "minecraft:coal"
-            }
-        ],
-        "entity": "quark:foxhound",
-        "aura": 150000,
-        "time": 120
-    });
-});
-
-events.listen('recipes', function(event) {
-    event.recipes.naturesaura.animal_spawner({
-        "type": "naturesaura:animal_spawner",
-        "ingredients": [{
-                "item": "naturesaura:birth_spirit"
-            },
-            {
-                "item": "thermal:blitz_rod"
-            },
-            {
-                "item": "thermal:blitz_powder"
-            }
-        ],
-        "entity": "thermal:blitz",
-        "aura": 150000,
-        "time": 120
-    });
-});
-
-events.listen('recipes', function(event) {
-    event.recipes.naturesaura.animal_spawner({
-        "type": "naturesaura:animal_spawner",
-        "ingredients": [{
-                "item": "naturesaura:birth_spirit"
-            },
-            {
-                "item": "thermal:blizz_rod"
-            },
-            {
-                "item": "thermal:blizz_powder"
-            }
-        ],
-        "entity": "thermal:blizz",
-        "aura": 150000,
-		"time": 120
-    });
-});
-
-events.listen('recipes', function(event) {
-    event.recipes.naturesaura.animal_spawner({
-        "type": "naturesaura:animal_spawner",
-        "ingredients": [{
-                "item": "naturesaura:birth_spirit"
-            },
-            {
-                "item": "thermal:basalz_rod"
-            },
-            {
-                "item": "thermal:basalz_powder"
-            }
-        ],
-        "entity": "thermal:basalz",
-        "aura": 150000,
-        "time": 120
+        ]
+    };
+    data.recipes.forEach((recipe) => {
+        event.recipes.naturesaura.animal_spawner({
+            type: 'naturesaura:animal_spawner',
+            ingredients: [
+                {
+                    item: 'naturesaura:birth_spirit'
+                },
+                {
+                    item: recipe.ingredients[0]
+                },
+                {
+                    item: recipe.ingredients[1]
+                }
+            ],
+            entity: recipe.entity,
+            aura: recipe.aura,
+            time: recipe.time
+        });
     });
 });
