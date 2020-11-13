5dfb8750d84f179c1910428a15e1af5bb1c39770
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-11-13 21:42:56 +0100

    ran prettier

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/industrialforegoing/laser_drill_fluid.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/industrialforegoing/laser_drill_fluid.js
@@ -1,29 +1,29 @@
 events.listen('recipes', function (event) {
     event.recipes.industrialforegoing.laser_drill_fluid({
-        "output": "{FluidName:\"pneumaticcraft:oil\",Amount:10}",
-        "rarity": [
-          {
-            "whitelist": {},
-            "blacklist": {
-              "type": "minecraft:worldgen/biome",
-              "values": [
-                "minecraft:nether_wastes",
-                "minecraft:basalt_deltas",
-                "minecraft:warped_forest",
-                "minecraft:crimson_forest",
-                "minecraft:soul_sand_valley"
-              ]
-            },
-            "depth_min": 5,
-            "depth_max": 20,
-            "weight": 8
-          }
+        output: '{FluidName:"pneumaticcraft:oil",Amount:10}',
+        rarity: [
+            {
+                whitelist: {},
+                blacklist: {
+                    type: 'minecraft:worldgen/biome',
+                    values: [
+                        'minecraft:nether_wastes',
+                        'minecraft:basalt_deltas',
+                        'minecraft:warped_forest',
+                        'minecraft:crimson_forest',
+                        'minecraft:soul_sand_valley'
+                    ]
+                },
+                depth_min: 5,
+                depth_max: 20,
+                weight: 8
+            }
         ],
-        "pointer": 0,
-        "catalyst": {
-          "item": "industrialforegoing:laser_lens15"
+        pointer: 0,
+        catalyst: {
+            item: 'industrialforegoing:laser_lens15'
         },
-        "entity": "minecraft:empty",
-        "type": "industrialforegoing:laser_drill_fluid"
-      });
+        entity: 'minecraft:empty',
+        type: 'industrialforegoing:laser_drill_fluid'
+    });
 });
