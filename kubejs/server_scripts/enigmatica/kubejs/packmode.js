dc0936e96640556456389820fd51f989f6c44072
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-11-15 21:21:14 +0100

    refined the packmode script to take Darkere's configswapper mod into account

--- kubejs/server_scripts/enigmatica/kubejs/packmode.js
+++ kubejs/server_scripts/enigmatica/kubejs/packmode.js
@@ -1,16 +1,22 @@
 //priority: 1000
 
-events.listen('recipes', function (event) {
-    var configName = 'mode.json';
-    var config = json.read(configName);
-    if (!config || !config.packmode) {
-        config = {
-            packmode: 'normal',
-            message: 'Valid modes are normal and expert.'
-        };
-        json.write(configName, config);
+events.listen('recipes', (event) => {
+    const defaultConfig = {
+        mode: 'normal',
+        message: 'Valid modes are normal and expert.'
+    };
+    const configName = 'mode.json';
+    let config = json.read(configName);
+    if (!config || !config.mode) {
+        json.write(configName, defaultConfig);
         console.log('Created new ' + configName);
     }
-    global.packmode = config.packmode;
+    if (config.mode === 'none') {
+        json.write(configName, defaultConfig);
+        console.log(
+            `Overwrote ${configName}, because the mode 'none' was found. Valid modes are 'normal' and 'expert'.`
+        );
+    }
+    global.packmode = config.mode;
     console.log('Current packmode is: ' + global.packmode);
 });
