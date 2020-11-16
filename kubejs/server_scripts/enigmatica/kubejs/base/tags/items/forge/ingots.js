873417c5c3fb3ff7a6569d83dfea297cf67c7ad1
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-11-16 22:10:02 +0100

    Update ingots.js

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/ingots.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/ingots.js
@@ -14,17 +14,26 @@ events.listen('item.tags', function (event) {
         .add('immersiveengineering:ingot_uranium')
         .add('immersiveengineering:ingot_constantan')
         .add('immersiveengineering:ingot_electrum')
-        .add('immersiveengineering:ingot_hop_graphite');
+        .add('immersiveengineering:ingot_hop_graphite')
+        .add('mythicbotany:alfsteel_ingot')
+        .add('industrialforegoing:pink_slime_ingot')
+        .add('good_nights_sleep:zitrite_ingot')
+        .add('good_nights_sleep:rainbow_ingot');
 
     event.get('forge:ingots/copper').add('immersiveengineering:ingot_copper');
-    // event.get('forge:ingots').add('psi:ivory_psimetal').add('psi:ebony_psimetal').add('psi:psimetal');
+    event.get('forge:ingots/alfsteel').add('mythicbotany:alfsteel_ingot');
+    event.get('forge:ingots/pink_slime').add('industrialforegoing:pink_slime_ingot');
+    event.get('forge:ingots/gaia').add('botania:gaia_ingot');
+    event.get('forge:ingots/gaia_spirit').add('botania:gaia_ingot');
+    event.get('forge:ingots/rainbow').add('good_nights_sleep:rainbow_ingot');
+    event.get('forge:ingots/zitrite').add('good_nights_sleep:zitrite_ingot');
     // event.get('forge:ingots').add('occultism:iesnium_ingot');
 
-    // event
-    //     .get('forge:ingots')
-    //     // .add('naturesaura:sky_ingot')
-    //     // .add('naturesaura:tainted_gold')
-    //     // .add('naturesaura:infused_iron');
-    // event.get('forge:ingots').add('industrialforgoing:pink_slime_ingot');
+    event
+        .get('forge:ingots')
+        .add('naturesaura:sky_ingot')
+        .add('naturesaura:tainted_gold')
+        .add('naturesaura:infused_iron');
+    event.get('forge:ingots').add('industrialforgoing:pink_slime_ingot');
     event.get('forge:ingots').add('botania:gaia_ingot');
 });
