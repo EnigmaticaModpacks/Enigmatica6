a68795c1617b98ee32c1136f5111cf025ff27e88
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-11-30 19:52:06 +0100

    the player is now given the quest book instead of the modded for dummies book on first login

--- kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
@@ -1,6 +1,7 @@
-events.listen('player.logged_in', function (event) {
+events.listen('player.logged_in', (event) => {
     if (!event.hasGameStage('starting_items')) {
-        event.player.give(item.of('patchouli:guide_book', { 'patchouli:book': 'patchouli:modded_for_dummies' }));
+        // event.player.give(item.of('patchouli:guide_book', { 'patchouli:book': 'patchouli:modded_for_dummies' }));
+        event.player.give(item.of('ftbquests:book'));
         event.addGameStage('starting_items');
     }
 });
