6f59dc59d1c2fbfa05904517d526a892de85e6e4
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-12-01 23:12:00 +0100

    Update tooltips.js

--- kubejs/client_scripts/item_modifiers/tooltips.js
+++ kubejs/client_scripts/item_modifiers/tooltips.js
@@ -1,8 +1,8 @@
-events.listen('client.item_tooltip', function (event) {
-    // switch (event.item.id) {
-    //     case 'appliedenergistics2:quantum_entangled_singularity':
-    //         event.add(
-    //             'To create drop 1 Singularity and 1 Ender Dust (From Applied Energistics) and cause an explosion within range of the items.'
-    //         );
-    // }
-});
+// events.listen('client.item_tooltip', function (event) {
+//     switch (event.item.id) {
+//         case 'appliedenergistics2:quantum_entangled_singularity':
+//             event.add(
+//                 'To create drop 1 Singularity and 1 Ender Dust (From Applied Energistics) and cause an explosion within range of the items.'
+//             );
+//     }
+// });
