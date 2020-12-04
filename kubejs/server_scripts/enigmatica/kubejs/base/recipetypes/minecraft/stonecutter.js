49df45f2c67274e7957de88516fc48d9323405de
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-12-04 22:46:15 +0100

    disabled forge:stone stonecutter conversion, #445

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/minecraft/stonecutter.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/minecraft/stonecutter.js
@@ -1,29 +1,6 @@
 events.listen('recipes', (event) => {
-    var stoneCutterRemovals = [
-        'minecraft:iron_ingot',
-        'minecraft:string',
-        'minecraft:iron_nugget',
-        'minecraft:bone',
-        'minecraft:gold_ingot',
-        'minecraft:gold_nugget',
-        'minecraft:light_weighted_pressure_plate'
-    ];
-    stoneCutterRemovals.forEach(function (item) {
-        event.remove({ type: 'minecraft:stonecutting', output: item });
-    });
-
-    ingredient.of('#forge:stone').stacks.forEach(function (stone) {
-        event.recipes.minecraft.stonecutting({
-            ingredient: {
-                tag: 'forge:stone'
-            },
-            result: stone.id,
-            count: 1
-        });
-    });
-
-    var conversionTypes = ['storage_blocks', 'ores'];
     // Conversion between different storage_blocks of the same material
+    var conversionTypes = ['storage_blocks', 'ores'];
     conversionTypes.forEach(function (type) {
         materialsToUnify.forEach(function (material) {
             let storage_block_tag = ingredient.of('#forge:' + type + '/' + material);
