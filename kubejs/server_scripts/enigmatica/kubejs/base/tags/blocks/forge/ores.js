5088586ed8cac6add31671fcda64de19e8f2efa4
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-12-14 00:32:57 +0100

    readded forge:ores tags for dimensional ores

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/blocks/forge/ores.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/blocks/forge/ores.js
@@ -3,7 +3,12 @@ events.listen('block.tags', function (event) {
     event
         .get('forge:ores')
         .add(['powah:uraninite_ore_poor', 'powah:uraninite_ore', 'powah:uraninite_ore_dense'])
-        .add('minecraft:ancient_debris');
+        .add('minecraft:ancient_debris')
+        .add([
+            'rftoolsbase:dimensionalshard_overworld',
+            'rftoolsbase:dimensionalshard_nether',
+            'rftoolsbase:dimensionalshard_end'
+        ]);
     event
         .get('forge:ores/dimensional')
         .add([
@@ -17,4 +22,3 @@ events.listen('block.tags', function (event) {
     // event.get('forge:ores').add('occultism:iesnium_ore');
     //event.get('forge:ores/yellorium').add('bigreactors:yellorite_ore');
 });
-
