1a99453b3babd1c4e9039b61d3aa14becccaff16
Ridanis <khaled.chikhou@gmail.com>
2020-12-18 12:29:58 +0100

    Lootboxes, Delights, Tetra Geode, IE Crate
    Added:
    - Loot Box and Delight Items through KubeJS:
       - Texures
       - Lang File
    
    Updated:
    - Tetra Geode Loot Table:
       - Change old nuggets from forge tag to EE items
       - Added nuggets for Lead, Nickel, Uranium, Osmium, Tin, and Zinc
    
    - IE Crate Loot Table
       - Updated for IE Ingots and Nuget to EE Ingots and Nuggets
    
    Removed:
    - ALL Extra Disk recipes through KubeJS

--- kubejs/startup_scripts/factory/items.js
+++ kubejs/startup_scripts/factory/items.js
@@ -14,3 +14,15 @@
 //         }
 //     }
 // });
+
+events.listen('item.registry', function (event) {
+    event.create('rare_lootbox').group('KubeJS').texture('kubejs:item/rare_lootbox').add();
+    event.create('epic_lootbox').group('KubeJS').texture('kubejs:item/epic_lootbox').add();
+    event.create('legendary_lootbox').group('KubeJS').texture('kubejs:item/legendary_lootbox').add();
+    event.create('miners_delight').group('KubeJS').texture('kubejs:item/miners_delight').add();
+    event.create('sorcerers_delight').group('KubeJS').texture('kubejs:item/sorcerers_delight').add();
+    event.create('farmers_delight').group('KubeJS').texture('kubejs:item/farmers_delight').add();
+    event.create('blacksmiths_delight').group('KubeJS').texture('kubejs:item/blacksmiths_delight').add();
+    event.create('scavangers_delight').group('KubeJS').texture('kubejs:item/scavangers_delight').add();
+    event.create('alchemists_delight').group('KubeJS').texture('kubejs:item/alchemists_delight').add();
+});
