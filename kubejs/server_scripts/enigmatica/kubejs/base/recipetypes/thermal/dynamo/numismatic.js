cae0cc46d5a6a060dda8c0e33a77a9b8810fa49a
MuteTiefling <mutetiefling@gmail.com>
2020-12-17 21:56:03 -0500

    Thermal Dynamos Rebalance Attempt
    Dynamos proved to be pretty underwhelming with default values. In game, I had 4 arboreal extractors going and couldn't supply enough fuel to keep a single compression dynamo running. Needless to say, it wasn't even sufficient to keep a small RS system online.
    
    I have yet to find any config that would allow us to change the base rf/t generation, so dynamos are still capped at 640/rf/t at 73% fuel efficiency, or or 160rf/t at 133% efficiency.
    
    These changes let a single fully upgraded compression dynamo run approximately 11 dynamos at high efficiency, generating about 1.7k rf/t. It's not going to beat out Ethylene, but I think it makes these much more viable for early game instead of relying on solar or wind. Mind, the starter dynamo with no upgrades only manages 40rf/t per dynamo, and fuel generation would be much slower at that point as well.
    
    Similarly, lapidary generators get a massive boost, as do numismatics, considering they burn rare items for fuel. Still low output, but much more efficient in their burn rate.
    
    Magmatic and Stirling remain at their default rates as that's really quite sufficient to get started and encourages upgrading to something a little more involved to run.
    
    As a bit of a test, I set up a compression dynamo with 1 bucket of tree oil in it. This ended up giving the expected roughly 4 million RF total, but at a slow rate of 160rf/t. At the max rate of 640 rf/t, I'd expect somewhere around 2.9 million rf.
    
    Lastly, since sapphire and ruby are disabled throughout the pack, this also removes them as possible fuels for the lapidary dynamo.

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/thermal/dynamo/numismatic.js
@@ -0,0 +1,72 @@
+events.listen('recipes', (event) => {
+    event.remove({ type: 'thermal:numismatic_fuel' });
+    var multiplier = 40;
+    var data = {
+        recipes: [
+            {
+                coin: 'forge:coins/gold',
+                energy: 64000
+            },
+            {
+                coin: 'forge:coins/invar',
+                energy: 48000
+            },
+            {
+                coin: 'forge:coins/iron',
+                energy: 32000
+            },
+            {
+                coin: 'forge:coins/enderium',
+                energy: 160000
+            },
+            {
+                coin: 'forge:coins/lead',
+                energy: 48000
+            },
+            {
+                coin: 'forge:coins/lumium',
+                energy: 80000
+            },
+            {
+                coin: 'forge:coins/nickel',
+                energy: 64000
+            },
+            {
+                coin: 'forge:coins/signalum',
+                energy: 80000
+            },
+            {
+                coin: 'forge:coins/silver',
+                energy: 48000
+            },
+            {
+                coin: 'forge:coins/tin',
+                energy: 32000
+            },
+            {
+                coin: 'forge:coins/bronze',
+                energy: 40000
+            },
+            {
+                coin: 'forge:coins/constantan',
+                energy: 56000
+            },
+            {
+                coin: 'forge:coins/copper',
+                energy: 32000
+            },
+            {
+                coin: 'forge:coins/electrum',
+                energy: 60000
+            }
+        ]
+    };
+    data.recipes.forEach((recipe) => {
+        event.recipes.thermal.numismatic_fuel({
+            ingredient: {
+                tag: recipe.coin
+            },
+            energy: recipe.energy * multiplier
+        });
+    });
+});
