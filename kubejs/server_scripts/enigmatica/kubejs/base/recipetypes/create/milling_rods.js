f123ba103d49b8ee0dfdcd8a3af52a0450268f4f
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-12-19 12:26:41 +0100

    formatting

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/create/milling_rods.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/create/milling_rods.js
@@ -1,31 +1,35 @@
 events.listen('recipes', (event) => {
     var data = {
         recipes: [
-            {input: 'thermal:blizz_rod', primary: 'thermal:blizz_powder', secondary: 'minecraft:snowball'},
-            {input: 'thermal:blitz_rod', primary: 'thermal:blitz_powder', secondary: 'emendatusenigmatica:potassium_nitrate_gem'},
-            {input: 'thermal:basalz_rod', primary: 'thermal:basalz_powder', secondary: 'thermal:slag'}
+            { input: 'thermal:blizz_rod', primary: 'thermal:blizz_powder', secondary: 'minecraft:snowball' },
+            {
+                input: 'thermal:blitz_rod',
+                primary: 'thermal:blitz_powder',
+                secondary: 'emendatusenigmatica:potassium_nitrate_gem'
+            },
+            { input: 'thermal:basalz_rod', primary: 'thermal:basalz_powder', secondary: 'thermal:slag' }
         ]
     };
     data.recipes.forEach((recipe) => {
         event.recipes.create.milling({
-          type: 'create:milling',
-          ingredients: [
-            {
-              item: recipe.input
-            }
-          ],
-          results: [
-            {
-              item: recipe.primary,
-              count: 3
-            },
-            {
-              item: recipe.secondary,
-              count: 1,
-              chance: 0.25
-            }
-          ],
-          processingTime: 50
-        });        
+            type: 'create:milling',
+            ingredients: [
+                {
+                    item: recipe.input
+                }
+            ],
+            results: [
+                {
+                    item: recipe.primary,
+                    count: 3
+                },
+                {
+                    item: recipe.secondary,
+                    count: 1,
+                    chance: 0.25
+                }
+            ],
+            processingTime: 50
+        });
     });
 });
