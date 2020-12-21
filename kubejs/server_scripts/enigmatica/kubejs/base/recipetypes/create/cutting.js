8ffc8c959e4c106c8ab484bdf1499fba1c5f057b
MuteTiefling <mutetiefling@gmail.com>
2020-12-20 23:07:39 -0500

    missed saw recipes and a type
    Simple Farming Insolator recipes: typo :(
    
    Added Immersive Engineering Saw Mill recipes, but not happy with them. For whatever reason, no matter how I try to add the secondaries (for sawdust output) it just fails to load the recipe entirely.
    
    Good enough for now, I guess. I hope we don't need to do this with datapacks.
    
    undergarden added stripped varieties, so removed exception handling for that.

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/create/cutting.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/create/cutting.js
@@ -7,53 +7,34 @@ events.listen('recipes', function (event) {
             return;
         }
 
-        if (modID == 'undergarden') {
-            data = {
-                recipes: [
-                    {
-                        input: variant.logBlock,
-                        output: variant.plankBlock,
-                        count: 5,
-                        time: 100
-                    },
-                    {
-                        input: variant.woodBlock,
-                        output: variant.plankBlock,
-                        count: 5,
-                        time: 100
-                    }
-                ]
-            };
-        } else {
-            data = {
-                recipes: [
-                    {
-                        input: variant.logBlock,
-                        output: variant.logBlockStripped,
-                        count: 1,
-                        time: 50
-                    },
-                    {
-                        input: variant.woodBlock,
-                        output: variant.woodBlockStripped,
-                        count: 1,
-                        time: 50
-                    },
-                    {
-                        input: variant.logBlockStripped,
-                        output: variant.plankBlock,
-                        count: 5,
-                        time: 100
-                    },
-                    {
-                        input: variant.woodBlockStripped,
-                        output: variant.plankBlock,
-                        count: 5,
-                        time: 100
-                    }
-                ]
-            };
-        }
+        data = {
+            recipes: [
+                {
+                    input: variant.logBlock,
+                    output: variant.logBlockStripped,
+                    count: 1,
+                    time: 50
+                },
+                {
+                    input: variant.woodBlock,
+                    output: variant.woodBlockStripped,
+                    count: 1,
+                    time: 50
+                },
+                {
+                    input: variant.logBlockStripped,
+                    output: variant.plankBlock,
+                    count: 5,
+                    time: 100
+                },
+                {
+                    input: variant.woodBlockStripped,
+                    output: variant.plankBlock,
+                    count: 5,
+                    time: 100
+                }
+            ]
+        };
 
         data.recipes.forEach((recipe) => {
             event.recipes.create.cutting({
