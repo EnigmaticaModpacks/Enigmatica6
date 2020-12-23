bb844f1188c961433a9a5ddf36f3159238d95f1c
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-12-23 22:37:38 +0100

    crafting station recipe, fixes #602

--- kubejs/server_scripts/enigmatica/kubejs/base/recipes/remove.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipes/remove.js
@@ -12,6 +12,8 @@ events.listen('recipes', function (event) {
         'byg:blue_enchanted_crafting_table',
         'byg:green_enchanted_crafting_table',
 
+        'craftingstation:crafting_station',
+
         'decorative_blocks:lattice',
 
         //'akashictome:tome',
