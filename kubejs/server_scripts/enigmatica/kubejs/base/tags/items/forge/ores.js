8d05d4355a706c42e95496b95e361101a76d9555
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-12-25 21:20:31 +0100

    netherite tagging

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/ores.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/ores.js
@@ -1,5 +1,4 @@
 events.listen('item.tags', function (event) {
-    event.get('forge:ores/ancient_debris').add('minecraft:ancient_debris');
     event
         .get('forge:ores')
         .add(['powah:uraninite_ore_poor', 'powah:uraninite_ore', 'powah:uraninite_ore_dense'])
@@ -13,6 +12,9 @@ events.listen('item.tags', function (event) {
         ]);
     event.get('forge:ores/nether/gold').add('minecraft:nether_gold_ore');
     event.get('forge:ores/arcane').add('ars_nouveau:arcane_ore');
+    event.get('forge:ores/netherite_scrap').remove('minecraft:ancient_debris');
+    event.get('forge:ores/netherite').add('minecraft:ancient_debris');
+
     // event.get('forge:ores').add('occultism:iesnium_ore');
     //event.get('forge:ores/yellorium').add('bigreactors:yellorite_ore');
 });
