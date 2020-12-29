1da3448b73de51851f24e1b48626bd491144d7b3
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-12-29 20:52:15 +0100

    Adding forge tags to Occultism #652

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/blocks/forge/ores.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/blocks/forge/ores.js
@@ -2,7 +2,8 @@ events.listen('block.tags', function (event) {
     event
         .get('forge:ores')
         .add(['powah:uraninite_ore_poor', 'powah:uraninite_ore', 'powah:uraninite_ore_dense'])
-        .add('minecraft:ancient_debris');
+        .add('minecraft:ancient_debris')
+        .add('occultism:iesnium_ore');
     event
         .get('forge:ores/dimensional')
         .add([
@@ -19,7 +20,4 @@ events.listen('block.tags', function (event) {
     event.get('forge:ores/nether/gold').add('minecraft:nether_gold_ore');
     event.get('forge:ores/netherite_scrap').remove('minecraft:ancient_debris');
     event.get('forge:ores/netherite').add('minecraft:ancient_debris');
-
-    // event.get('forge:ores').add('occultism:iesnium_ore');
-    //event.get('forge:ores/yellorium').add('bigreactors:yellorite_ore');
 });
