ae6af6816c45a09ec5635aab3c124aa6c9ce6e87
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-01-04 20:25:03 +0100

    Update blocks.js

--- kubejs/startup_scripts/factory/blocks.js
+++ kubejs/startup_scripts/factory/blocks.js
@@ -1,3 +1,4 @@
+//TODO: Remove
 // events.listen('block.registry', function (event) {
 //     for (var i = 0; i < materials.length; i++) {
 //         for (var j = 0; j < materials[i].blockParts.length; j++) {
