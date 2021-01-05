79ac9b18d84967854e6c67cb867341efa34ea2cf
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-01-05 21:10:09 +0100

    #746

--- kubejs/server_scripts/enigmatica/kubejs/base/recipes/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipes/shaped.js
@@ -297,7 +297,21 @@ events.listen('recipes', function (event) {
             A: 'mekanism:superheating_element',
             B: 'pneumaticcraft:heat_sink',
             C: 'mekanism:ultimate_thermodynamic_conductor'
+        }),
+        // Remove in 0.4.0
+        shapedRecipe(item.of('minecraft:lapis_lazuli', 4), ['AAA', 'ABA', 'AAA'], {
+            A: '#forge:gems/certus',
+            B: '#forge:dyes/blue'
+        }),
+        shapedRecipe(item.of('minecraft:diamond', 1), ['AAA', 'ABA', 'AAA'], {
+            A: '#forge:gems/certus',
+            B: '#forge:dyes/light_blue'
+        }),
+        shapedRecipe(item.of('minecraft:emerald', 2), ['AAA', 'ABA', 'AAA'], {
+            A: '#forge:gems/certus',
+            B: '#forge:dyes/green'
         })
+        //
     ];
 
     recipes.forEach(function (recipe) {
