e718c6c3fb171e67e3849b552e0662aec15ee7e5
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-01-21 22:34:43 +0100

    starmetal ingot tag

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/ingots.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/ingots.js
@@ -16,14 +16,15 @@ events.listen('item.tags', function (event) {
         .add('immersiveengineering:ingot_hop_graphite')
         .add('mythicbotany:alfsteel_ingot')
         .add('industrialforegoing:pink_slime_ingot')
-        .add('occultism:iesnium_ingot');
+        .add('occultism:iesnium_ingot')
+        .add('astralsorcery:starmetal_ingot');
 
     event.get('forge:ingots/copper').add('immersiveengineering:ingot_copper');
     event.get('forge:ingots/alfsteel').add('mythicbotany:alfsteel_ingot');
     event.get('forge:ingots/pink_slime').add('industrialforegoing:pink_slime_ingot');
     event.get('forge:ingots/gaia').add('botania:gaia_ingot');
     event.get('forge:ingots/gaia_spirit').add('botania:gaia_ingot');
-
+    event.add('forge:ingots/starmetal', ['astralsorcery:starmetal_ingot']);
     event
         .get('forge:ingots')
         .add('naturesaura:sky_ingot')
