78dafa561c7448777129b8febbce1e63f6a32606
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-01-25 23:23:15 +0100

    RS tooltips + descriptions, cleanup

---
+++ kubejs/client_scripts/item_modifiers/jei_hide_fluids.js
@@ -0,0 +1,4 @@
+onEvent('jei.hide.fluids', (event) => {
+    event.hide('cofh_core:honey');
+    event.hide('create:honey');
+});
--- kubejs/client_scripts/item_modifiers/tooltips.js
+++ kubejs/client_scripts/item_modifiers/tooltips.js
@@ -1,15 +1,7 @@
- events.listen('client.item_tooltip', function (event) {
- //    switch (event.item.id) {
- //        case 'appliedenergistics2:quantum_entangled_singularity':
- //            event.add(
- //                'To create drop 1 Singularity and 1 Ender Dust (From Applied Energistics) and cause an explosion within range of the items.'
- //            );
- //    }
-    global.rsitems.forEach(function(rsitem) {
-        if(event.item.id == 'refinedstorage:' + rsitem) {
-            event.add(
-                'The color of this block can be configured by crafting it with a dye or right clicking it with dye in the world.'
-            );
+onEvent('client.item_tooltip', (event) => {
+    refinedStorageItems.forEach((item) => {
+        if (event.item.id == 'refinedstorage:' + item) {
+            event.add('Can be dyed through crafting or by right clicking it with dye in-world.');
         }
-    })
+    });
 });
