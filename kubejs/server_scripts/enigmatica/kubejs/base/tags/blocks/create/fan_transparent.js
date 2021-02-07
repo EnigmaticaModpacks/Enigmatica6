99dd8a84468a0cccbcc6f9b34085d5b2a3ac3fc0
MuteTiefling <mutetiefling@gmail.com>
2021-02-07 11:39:00 -0500

    Allow Create Fan to blow through more blocks
    Allow Create Fan to blow through more blocks

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/blocks/create/fan_transparent.js
@@ -0,0 +1,12 @@
+events.listen('block.tags', function (event) {
+    event
+        .get('create:fan_transparent')
+        .add(/bars/)
+        .add(/chain_link/)
+        .add(/leaves/)
+        .add('darkutils:item_grate')
+        .add('engineersdecor:steel_floor_grating')
+        .add('quark:grate')
+        .add('minecraft:chain')
+        .add('decorative_blocks:chain');
+});
