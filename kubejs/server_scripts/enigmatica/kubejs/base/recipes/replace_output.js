cebc69d2b2549a792ad6591bf60d6030d988669b
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-02-08 21:49:56 +0100

    mod updates and #812

--- kubejs/server_scripts/enigmatica/kubejs/base/recipes/replace_output.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipes/replace_output.js
@@ -5,6 +5,7 @@ events.listen('recipes', function (event) {
     event.replaceOutput({}, 'thermal:niter', '#forge:gems/niter');
     event.replaceOutput({}, 'thermal:bitumen', '#forge:gems/bitumen');
     event.replaceOutput({}, 'thermal:coal_coke', '#forge:gems/coal_coke');
+    event.replaceOutput({}, 'refinedstorage:silicon', '#forge:gems/silicon');
 
     event.replaceOutput({ mod: 'dustrial_decor' }, 'minecraft:iron_ingot', 'dustrial_decor:rusty_iron_ingot');
     event.replaceOutput({ mod: 'dustrial_decor' }, 'minecraft:iron_nugget', 'dustrial_decor:rusty_iron_nugget');
