ac269e2295bfe2190b694b6ffd88a5ed0c2e58df
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-02-11 23:20:35 +0100

    cleanup

--- kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
@@ -1,6 +1,5 @@
 events.listen('player.logged_in', (event) => {
     if (!event.hasGameStage('starting_items')) {
-        // event.player.give(item.of('patchouli:guide_book', { 'patchouli:book': 'patchouli:modded_for_dummies' }));
         event.player.give(item.of('ftbquests:book'));
         event.addGameStage('starting_items');
     }
