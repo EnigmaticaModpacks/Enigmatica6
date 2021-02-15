3b0beae534075729f7b511a6a60265993f9fdc7d
MuteTiefling <mutetiefling@gmail.com>
2021-02-15 15:28:21 -0500

    Tag Maintenance Pass - Clean up Tag Search Scripts
    These have been bugging me for a while, so I'm condensing them a bit and using regex to hopefully reduce the amount I need to touch them.

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/armor/boots.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/armor/boots.js
@@ -1,50 +1,26 @@
 events.listen('item.tags', (event) => {
     var items = [
-        'aquaculture:neptunium_boots',
-        'ars_nouveau:apprentice_boots',
-        'ars_nouveau:archmage_boots',
-        'ars_nouveau:novice_boots',
-        'botania:elementium_boots',
-        'botania:manasteel_boots',
-        'botania:manaweave_boots',
-        'botania:terrasteel_boots',
-        'byg:ametrine_boots',
         'immersiveengineering:armor_faraday_feet',
         'immersiveengineering:armor_steel_feet',
-        'mapperbase:steel_boots',
         'mekanism:free_runners',
-        'mekanism:hazmat_boots',
-        'mekanism:mekasuit_boots',
-        'mekanismtools:bronze_boots',
-        'mekanismtools:lapis_lazuli_boots',
-        'mekanismtools:osmium_boots',
-        'mekanismtools:refined_glowstone_boots',
-        'mekanismtools:refined_obsidian_boots',
-        'minecraft:chainmail_boots',
-        'minecraft:diamond_boots',
-        'minecraft:golden_boots',
-        'minecraft:iron_boots',
-        'minecraft:leather_boots',
-        'minecraft:netherite_boots',
-        'mythicbotany:alfsteel_boots',
         'naturesaura:infused_iron_shoes',
-        'naturesaura:sky_shoes',
-        'pneumaticcraft:pneumatic_boots',
-        'thermal:beekeeper_boots',
-        'thermal:diving_boots',
-        'thermal:hazmat_boots',
-        'undergarden:froststeel_boots',
-        'undergarden:cloggrum_boots',
-        'undergarden:utheric_boots',
-        'bloodmagic:livingboots',
-        'mekanismtools:steel_boots',
-        'dustrial_decor:cardboard_boots',
-        'alexsmobs:roadrunner_boots',
-        'eidolon:warlock_boots',
-        'betterendforge:crystalite_boots',
-        'betterendforge:aeternium_boots',
-        'betterendforge:terminite_boots'
+        'naturesaura:sky_shoes'
     ];
-    event.get('forge:armor').add(items);
-    event.get('forge:armor/boots').add(items);
+    var exceptions = [
+        'pneumaticcraft:jet_boots_upgrade_1',
+        'pneumaticcraft:jet_boots_upgrade_2',
+        'pneumaticcraft:jet_boots_upgrade_3',
+        'pneumaticcraft:jet_boots_upgrade_4',
+        'pneumaticcraft:jet_boots_upgrade_5'
+    ];
+
+    var tags = ['forge:armor', 'forge:armor/boots'];
+
+    tags.forEach((tag) => {
+        event
+            .get(tag)
+            .add(items)
+            .add(/_boots/)
+            .remove(exceptions);
+    });
 });
--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/armor/chests.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/armor/chests.js
@@ -1,54 +1,26 @@
 events.listen('item.tags', (event) => {
     var items = [
-        'aquaculture:neptunium_chestplate',
         'ars_nouveau:apprentice_robes',
         'ars_nouveau:archmage_robes',
         'ars_nouveau:novice_robes',
-        'botania:elementium_chestplate',
-        'botania:manasteel_chestplate',
-        'botania:manaweave_chestplate',
-        'botania:terrasteel_chestplate',
-        'byg:ametrine_chestplate',
         'immersiveengineering:armor_faraday_chest',
         'immersiveengineering:armor_steel_chest',
-        'mapperbase:steel_chestplate',
         'mekanism:hazmat_gown',
         'mekanism:jetpack',
         'mekanism:jetpack_armored',
         'mekanism:mekasuit_bodyarmor',
-        'mekanismtools:bronze_chestplate',
-        'mekanismtools:lapis_lazuli_chestplate',
-        'mekanismtools:osmium_chestplate',
-        'mekanismtools:refined_glowstone_chestplate',
-        'mekanismtools:refined_obsidian_chestplate',
-        'minecraft:chainmail_chestplate',
-        'minecraft:diamond_chestplate',
-        'minecraft:golden_chestplate',
-        'minecraft:iron_chestplate',
-        'minecraft:leather_chestplate',
-        'minecraft:netherite_chestplate',
-        'mythicbotany:alfsteel_chestplate',
         'naturesaura:infused_iron_chest',
         'naturesaura:sky_chest',
-        'pneumaticcraft:pneumatic_chestplate',
-        'thermal:beekeeper_chestplate',
-        'thermal:diving_chestplate',
-        'thermal:hazmat_chestplate',
-        'undergarden:utheric_chestplate',
-        'undergarden:froststeel_chestplate',
-        'undergarden:cloggrum_chestplate',
-        'undergarden:masticated_chestplate',
         'astralsorcery:mantle',
         'bloodmagic:livingplate',
-        'alexsmobs:crocodile_chestplate',
-        'dustrial_decor:cardboard_chestplate',
-        'mekanismtools:steel_chestplate',
-        'eidolon:warlock_cloak',
-        'betterendforge:crystalite_chestplate',
-        'betterendforge:aeternium_chestplate',
-        'betterendforge:terminite_chestplate',
-        'autumnity:snail_shell_chestplate'
+        'eidolon:warlock_cloak'
     ];
-    event.get('forge:armor').add(items);
-    event.get('forge:armor/chest').add(items);
+    var tags = ['forge:armor', 'forge:armor/chest'];
+
+    tags.forEach((tag) => {
+        event
+            .get(tag)
+            .add(items)
+            .add(/_chestplate/);
+    });
 });
