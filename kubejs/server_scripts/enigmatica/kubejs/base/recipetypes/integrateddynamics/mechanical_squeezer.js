929a177dd2ba7dc36abeed5cd430c1397a2880b7
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-02-15 22:33:49 +0100

    fix incorrect tag inputs in mech squeezer recipes

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/integrateddynamics/mechanical_squeezer.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/integrateddynamics/mechanical_squeezer.js
@@ -2,73 +2,73 @@ events.listen('recipes', function (event) {
     var data = {
         recipes: [
             {
-                input: '#forge:gems/ender',
+                input: { tag: 'forge:gems/ender' },
                 output: 'emendatusenigmatica:ender_dust',
                 count: 1,
                 duration: 10
             },
             {
-                input: 'byg:pink_sandstone',
+                input: { item: 'byg:pink_sandstone' },
                 output: 'byg:pink_sand',
                 count: 2,
                 duration: 10
             },
             {
-                input: 'byg:purple_sandstone',
+                input: { item: 'byg:purple_sandstone' },
                 output: 'byg:purple_sand',
                 count: 2,
                 duration: 10
             },
             {
-                input: 'byg:blue_sandstone',
+                input: { item: 'byg:blue_sandstone' },
                 output: 'byg:blue_sand',
                 count: 2,
                 duration: 10
             },
             {
-                input: 'byg:white_sandstone',
+                input: { item: 'byg:white_sandstone' },
                 output: 'byg:white_sand',
                 count: 2,
                 duration: 10
             },
             {
-                input: 'byg:black_sandstone',
+                input: { item: 'byg:black_sandstone' },
                 output: 'byg:black_sand',
                 count: 2,
                 duration: 10
             },
             {
-                input: 'atmospheric:arid_sandstone',
+                input: { item: 'atmospheric:arid_sandstone' },
                 output: 'atmospheric:arid_sand',
                 count: 2,
                 duration: 10
             },
             {
-                input: 'atmospheric:red_arid_sandstone',
+                input: { item: 'atmospheric:red_arid_sandstone' },
                 output: 'atmospheric:red_arid_sand',
                 count: 2,
                 duration: 10
             },
             {
-                input: '#forge:stone',
+                input: { tag: 'forge:stone' },
                 output: 'minecraft:cobblestone',
                 count: 1,
                 duration: 20
             },
             {
-                input: 'buildinggadgets:construction_block_dense',
+                input: { item: 'buildinggadgets:construction_block_dense' },
                 output: 'buildinggadgets:construction_paste',
                 count: 3,
                 duration: 20
             },
             {
-                input: 'minecraft:obsidian',
+                input: { item: 'minecraft:obsidian' },
                 output: 'emendatusenigmatica:obsidian_dust',
                 count: 4,
                 duration: 60
             },
             {
-                input: 'betterendforge:aurora_crystal',
+                input: { item: 'betterendforge:aurora_crystal' },
                 output: 'betterendforge:crystal_shards',
                 count: 4,
                 duration: 300
@@ -79,9 +79,7 @@ events.listen('recipes', function (event) {
     data.recipes.forEach((recipe) => {
         event.custom({
             type: 'integrateddynamics:mechanical_squeezer',
-            item: {
-                item: recipe.input
-            },
+            item: recipe.input,
             result: {
                 items: [
                     {
