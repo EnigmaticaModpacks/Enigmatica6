2170a392d80c8f697561a5372fb04396eca2cb1c
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-02-15 21:25:27 +0100

    description and tooltip to "fix" #1150

--- kubejs/client_scripts/item_modifiers/tooltips.js
+++ kubejs/client_scripts/item_modifiers/tooltips.js
@@ -1,7 +1,22 @@
-onEvent('client.item_tooltip', (event) => {
-    refinedStorageItems.forEach((item) => {
-        if (event.item.id == 'refinedstorage:' + item) {
-            event.add('Can be dyed through crafting or by right clicking it with dye in-world.');
+onEvent('item.tooltip', (event) => {
+    const recipes = [
+        {
+            item: 'bountifulbaubles:potion_wormhole',
+            text: [
+                Text.of(
+                    'Do not use, you will crash immediately if in multiplayer. We have notified the mod author about the issue.'
+                ).red()
+            ]
         }
+    ];
+
+    refinedStorageItems.forEach((item) => {
+        recipes.push('refinedstorage:' + item, [
+            'Can be dyed through crafting or by right clicking it with dye in-world.'
+        ]);
+    });
+
+    recipes.forEach((recipe) => {
+        event.add(recipe.item, recipe.text);
     });
 });
