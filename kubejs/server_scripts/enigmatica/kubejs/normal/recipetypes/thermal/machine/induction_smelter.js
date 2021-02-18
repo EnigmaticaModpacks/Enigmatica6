8c0b80ecc94e9ccb6a2fb4c4c8000239ee9d717f
Matteo Merler <matteo.merler@gmail.com>
2021-02-18 20:10:51 +0100

    wall recipes

---
+++ kubejs/server_scripts/enigmatica/kubejs/normal/recipetypes/thermal/machine/induction_smelter.js
@@ -0,0 +1,23 @@
+events.listen('recipes', (event) => {
+    if (global.packmode != 'normal') {
+        return;
+    }
+
+    var data = {
+        recipes: [
+            {
+                inputs: [
+                    '#forge:storage_blocks/enderium',
+                    Item.of('fluxnetworks:flux_dust', 8)
+                ],
+                outputs: [
+                    Item.of('compactmachines:wall', 32)
+                ]
+            }
+        ]
+    };
+
+    data.recipes.forEach((recipe) => {
+        event.recipes.thermal.smelter(recipe.outputs, recipe.inputs);
+    });
+});
