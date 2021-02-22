dcfb755497d85bc58f936e4c84b31b6d423c9678
MuteTiefling <mutetiefling@gmail.com>
2021-02-22 09:44:04 -0500

    Migrate FTB ultimine exclusions to tags
    As per Max's warning, these are moving from config based to tag based.

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/ftbultimine/excluded_tools.js
@@ -0,0 +1,12 @@
+events.listen('item.tags', (event) => {
+    event
+        .get('ftbultimine:excluded_tools')
+        .add([
+            'mininggadgets:mininggadget',
+            'ars_nouveau:wand',
+            'ars_nouveau:novice_spell_book',
+            'ars_nouveau:creative_spell_book',
+            'ars_nouveau:archmage_spell_book',
+            'ars_nouveau:apprentice_spell_book'
+        ]);
+});
