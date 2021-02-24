67c1c1ed664c379e5d06da72a48c766a9d5b6464
Matteo Merler <matteo.merler@gmail.com>
2021-02-24 22:34:11 +0100

    Added tags to backpacks to use in quest hand-ins

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/sophisticatedbackpacks/backpacks.js
@@ -0,0 +1,9 @@
+events.listen('item.tags', (event) => {
+    event.add('sophisticatedbackpacks:base_backpack', 'sophisticatedbackpacks:backpack');
+
+    event.add('sophisticatedbackpacks:iron_backpack', 'sophisticatedbackpacks:iron_backpack');
+
+    event.add('sophisticatedbackpacks:gold_backpack', 'sophisticatedbackpacks:gold_backpack');
+
+    event.add('sophisticatedbackpacks:diamond_backpack', 'sophisticatedbackpacks:diamond_backpack');
+});
