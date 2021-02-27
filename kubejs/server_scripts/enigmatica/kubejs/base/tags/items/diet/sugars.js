4ef7a7b45c9893b1b7b1e1a648c190456ec153b7
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-02-27 19:33:06 +0100

    Revert most of #1378

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/diet/sugars.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/diet/sugars.js
@@ -1,11 +1,4 @@
 events.listen('item.tags', (event) => {
-    event.add('diet:sugars', [
-        'resourcefulbees:oreo_cookie',
-        'betterendforge:sweet_berry_jelly',
-        'betterendforge:shadow_berry_jelly',
-        'meetyourfight:aether_glazed_cupcake'
-    ]);
-
     combVariants.forEach((comb) => {
         event.get('diet:sugars').add('resourcefulbees:' + comb + '_honeycomb');
     });
