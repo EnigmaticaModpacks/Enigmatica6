865b29629cc711b6fbda5586e3d442319a028438
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-03-01 21:45:38 +0100

    fixed #1492

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/workbench.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/workbench.js
@@ -1,6 +1,9 @@
 events.listen('item.tags', (event) => {
     event.add('forge:workbench', [
         'minecraft:crafting_table',
+        'byg:bulbis_crafting_table',
+        'byg:ether_crafting_table',
+        'byg:nightshade_crafting_table',
         'betterendforge:jellyshroom_crafting_table',
         'betterendforge:umbrella_tree_crafting_table',
         'betterendforge:helix_tree_crafting_table',
