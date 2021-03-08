7916eedf3d753e03a30279cb66459a494db1c529
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-03-08 22:37:10 +0100

    Update jei_descriptions.js

--- kubejs/client_scripts/item_modifiers/jei_descriptions.js
+++ kubejs/client_scripts/item_modifiers/jei_descriptions.js
@@ -1,51 +1,51 @@
 events.listen('jei.information', (event) => {
     const recipes = [
         {
-            item: ['simplefarming:cheese_slice'],
+            items: ['simplefarming:cheese_slice'],
             description: ['Obtained by right-clicking a Wheel of Cheese.']
         },
         {
-            item: ['astralsorcery:stardust'],
+            items: ['astralsorcery:stardust'],
             description: [
                 'Obtained by left-clicking a Starmetal Ingot in-world. See the Astral Tome for more information.'
             ]
         },
         {
-            item: ['buildinggadgets:construction_paste'],
+            items: ['buildinggadgets:construction_paste'],
             description: ['Obtained by breaking a Dense Construction Block with a Pickaxe.']
         },
         {
-            item: ['bountifulbaubles:potion_wormhole'],
+            items: ['bountifulbaubles:potion_wormhole'],
             description: [
                 'Do not use, you will crash immediately if in multiplayer. We have notified the mod author about the issue.'
             ]
         },
         {
-            item: ['powah:uraninite'],
+            items: ['powah:uraninite'],
             description: [
                 'Uraninite Ore has been removed and does not spawn naturally. To obtain Uraninite, use the Powah Energizing Orb recipe.'
             ]
         },
         {
-            item: ['powah:uraninite_block'],
+            items: ['powah:uraninite_block'],
             description: [
                 'Uraninite Ore has been removed and does not spawn naturally. To obtain Uraninite, use the Powah Energizing Orb recipe.'
             ]
         },
         {
-            item: ['thermal:blizz_rod', 'thermal:blizz_spawn_egg'],
+            items: ['thermal:blizz_rod', 'thermal:blizz_spawn_egg'],
             description: [
                 'The Blizz spawn naturally in cold biomes. Mechanical Dirt and Nocturnal Powder work wonders for farming them.'
             ]
         },
         {
-            item: ['thermal:blitz_rod', 'thermal:blitz_spawn_egg'],
+            items: ['thermal:blitz_rod', 'thermal:blitz_spawn_egg'],
             description: [
                 'The Blitz spawn naturally in warm, dry biomes. Mechanical Dirt and Nocturnal Powder work wonders for farming them.'
             ]
         },
         {
-            item: ['thermal:basalz_rod', 'thermal:basalz_spawn_egg'],
+            items: ['thermal:basalz_rod', 'thermal:basalz_spawn_egg'],
             description: [
                 'The Basalz spawn naturally in barren or mountainous biomes. Mechanical Dirt and Nocturnal Powder work wonders for farming them.'
             ]
@@ -83,12 +83,22 @@ events.listen('jei.information', (event) => {
         );
     });
 
-    var framedDrawers = ['compact_drawer', 'drawer_controller', 'slave', 'trim', 'full_one', 'full_two', 'full_four', 'half_one', 'half_two', 'half_four'];
+    var framedDrawers = [
+        'compact_drawer',
+        'drawer_controller',
+        'slave',
+        'trim',
+        'full_one',
+        'full_two',
+        'full_four',
+        'half_one',
+        'half_two',
+        'half_four'
+    ];
     framedDrawers.forEach((drawer) => {
-        event.add(
-            'framedcompactdrawers:framed_' + drawer,
-            ['Must be painted with any block to be used. To paint, put in any crafting grid with one block directly above and to its left diagonally.',
-            'You can also put a different block directly on top of the drawer and/or directly to its left to customize it even further!']
-        );
+        event.add('framedcompactdrawers:framed_' + drawer, [
+            'Must be painted with any block to be used. To paint, put in any crafting grid with one block directly above and to its left diagonally.',
+            'You can also put a different block directly on top of the drawer and/or directly to its left to customize it even further!'
+        ]);
     });
 });
