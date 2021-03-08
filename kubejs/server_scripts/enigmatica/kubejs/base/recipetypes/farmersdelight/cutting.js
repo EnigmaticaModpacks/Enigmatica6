12d20dbf23abf7541af704efd16e6e45bfc5509b
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-03-08 11:07:58 +0100

    Clean this up a bit

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/farmersdelight/cutting.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/farmersdelight/cutting.js
@@ -1,155 +1,71 @@
+function cuttingRecipe(ingredient, tool, result) {
+    return { 
+        type: 'farmersdelight:cutting',
+        ingredients: [ingredient],
+        tool: tool,
+        result: result
+    };
+}
+
+function filletRecipe(fish, filletCount) {
+    return cuttingRecipe(
+        Ingredient.of(fish), 
+        Ingredient.of('#forge:tools/knives'), 
+        [Item.of('aquaculture:fish_fillet_raw', filletCount), Item.of('minecraft:bone_meal', Math.ceil(filletCount / 3))]
+    )
+}
+
 events.listen('recipes', (event) => {
     var data = {
         recipes: [
-            {
-                ingredient: Ingredient.of('aquaculture:frog'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('quark:frog_leg', 2)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:goldfish'),
-                tool: Ingredient.of('#forge:tools/pickaxes'),
-                result: [Item.of('emendatusenigmatica:gold_chunk', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:atlantic_cod'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('farmersdelight:cod_slice', 6), Item.of('minecraft:bone_meal', 3)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:pink_salmon'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('farmersdelight:salmon_slice', 2), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('minecraft:pufferfish'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 1), Item.of('minecraft:bone_meal', 3)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:boulti'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 1), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:smallmouth_bass'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 2), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:atlantic_halibut'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 14), Item.of('minecraft:bone_meal', 5)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:pollock'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 2), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:bayad'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 4), Item.of('minecraft:bone_meal', 2)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:atlantic_herring'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 1), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:synodontis'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 1), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:piranha'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 1), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:red_grouper'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 3), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:pacific_halibut'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 12), Item.of('minecraft:bone_meal', 4)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:perch'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 1), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:rainbow_trout'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 2), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:catfish'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 6), Item.of('minecraft:bone_meal', 2)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:muskellunge'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 3), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:tambaqui'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 3), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:carp'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 2), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:blackfish'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 2), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:capitaine'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 10), Item.of('minecraft:bone_meal', 4)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:brown_trout'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 2), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:arapaima'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 10), Item.of('minecraft:bone_meal', 4)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:tuna'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 10), Item.of('minecraft:bone_meal', 4)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:bluegill'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 1), Item.of('minecraft:bone_meal', 1)]
-            },
-            {
-                ingredient: Ingredient.of('aquaculture:gar'),
-                tool: Ingredient.of('#forge:tools/knives'),
-                result: [Item.of('aquaculture:fish_fillet_raw', 4), Item.of('minecraft:bone_meal', 2)]
-            }
+            cuttingRecipe(
+                Ingredient.of('aquaculture:frog'),
+                Ingredient.of('#forge:tools/knives'),
+                [Item.of('quark:frog_leg', 2)]
+            ),
+            cuttingRecipe(
+                Ingredient.of('aquaculture:goldfish'),
+                Ingredient.of('#forge:tools/pickaxes'),
+                [Item.of('emendatusenigmatica:gold_chunk', 1)]
+            ),
+            cuttingRecipe(
+                Ingredient.of('aquaculture:atlantic_cod'),
+                Ingredient.of('#forge:tools/knives'),
+                [Item.of('farmersdelight:cod_slice', 6), Item.of('minecraft:bone_meal', 3)]
+            ),
+            cuttingRecipe(
+                Ingredient.of('aquaculture:pink_salmon'),
+                Ingredient.of('#forge:tools/knives'),
+                [Item.of('farmersdelight:salmon_slice', 2), Item.of('minecraft:bone_meal', 1)]
+            ),
+            filletRecipe('minecraft:pufferfish', 2),
+            filletRecipe('aquaculture:boulti', 1),
+            filletRecipe('aquaculture:smallmouth_bass', 2),
+            filletRecipe('aquaculture:atlantic_halibut', 14),
+            filletRecipe('aquaculture:pollock', 2),
+            filletRecipe('aquaculture:bayad', 4),
+            filletRecipe('aquaculture:atlantic_herring', 1),
+            filletRecipe('aquaculture:synodontis', 1),
+            filletRecipe('aquaculture:piranha', 1),
+            filletRecipe('aquaculture:red_grouper', 3),
+            filletRecipe('aquaculture:pacific_halibut', 12),
+            filletRecipe('aquaculture:perch', 1),
+            filletRecipe('aquaculture:rainbow_trout', 2),
+            filletRecipe('aquaculture:catfish', 6),
+            filletRecipe('aquaculture:muskellunge', 3),
+            filletRecipe('aquaculture:tambaqui', 3),
+            filletRecipe('aquaculture:carp', 2),
+            filletRecipe('aquaculture:blackfish', 2),
+            filletRecipe('aquaculture:capitaine', 10),
+            filletRecipe('aquaculture:brown_trout', 2),
+            filletRecipe('aquaculture:arapaima', 10),
+            filletRecipe('aquaculture:tuna', 10),
+            filletRecipe('aquaculture:bluegill', 1),
+            filletRecipe('aquaculture:gar', 4)
         ]
     };
 
     data.recipes.forEach((recipe) => {
-        event.custom({
-            type: 'farmersdelight:cutting',
-            ingredients: [recipe.ingredient],
-            tool: recipe.tool,
-            result: recipe.result
-        });
+        event.custom(recipe);
     });
 });
