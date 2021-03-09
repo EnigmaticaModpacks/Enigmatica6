1487538c54012216a5c7fb8e705cfe730c973340
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-03-09 21:25:50 +0100

    forgot about the id ^^ #1615

--- kubejs/server_scripts/enigmatica/kubejs/base/recipes/shapeless.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipes/shapeless.js
@@ -203,7 +203,8 @@ events.listen('recipes', (event) => {
         },
         {
             output: Item.of('refinedstorage:quartz_enriched_iron', 4),
-            inputs: ['#forge:ingots/iron', '#forge:ingots/iron', '#forge:ingots/iron', '#forge:gems/quartz']
+            inputs: ['#forge:ingots/iron', '#forge:ingots/iron', '#forge:ingots/iron', '#forge:gems/quartz'],
+            id: 'refinedstorage:quartz_enriched_iron'
         }
     ];
 
