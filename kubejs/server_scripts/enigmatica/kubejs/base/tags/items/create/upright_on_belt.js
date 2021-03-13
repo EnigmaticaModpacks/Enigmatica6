84f43543ef30085b3a29fe763d8f902336852fb1
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-03-13 13:13:44 +0100

    cleanup

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/create/upright_on_belt.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/create/upright_on_belt.js
@@ -1,8 +1,8 @@
 events.listen('item.tags', (event) => {
     event.add('create:upright_on_belt', [
-        '/bucket/',
-        '/bottle$/',
-    	'/juice/',
+        /bucket/,
+        /bottle$/,
+    	/juice/,
         'botania:vial',
         'botania:brew_vial',
     	'quark:bottled_cloud'
