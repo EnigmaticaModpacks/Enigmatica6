2d63351d65e1648b73449347c7d181766da66123
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-03-17 17:24:56 +0100

    Switch to tags

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/thermal/machine/press.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/thermal/machine/press.js
@@ -18,35 +18,35 @@ events.listen('recipes', (event) => {
                 output: Item.of('immersiveengineering:mold_rod', 1)
             },
             {
-                inputs: [Ingredient.of('#forge:ingots/copper'), Ingredient.of('immersiveengineering:mold_bullet_casing')],
+                inputs: [Ingredient.of('#forge:ingots/copper'), Ingredient.of('#thermal:crafting/dies/bullet_casing')],
                 output: Item.of('immersiveengineering:empty_casing', 2)
             },
             {
-                inputs: [Item.of('byg:pink_sandstone', 1), Item.of('immersiveengineering:mold_unpacking')],
+                inputs: [Item.of('byg:pink_sandstone', 1), Ingredient.of('#thermal:crafting/dies/unpacking')],
                 output: Item.of('byg:pink_sand', 4)
             },
             {
-                inputs: [Item.of('byg:purple_sandstone', 1), Item.of('immersiveengineering:mold_unpacking')],
+                inputs: [Item.of('byg:purple_sandstone', 1), Ingredient.of('#thermal:crafting/dies/unpacking')],
                 output: Item.of('byg:purple_sand', 4)
             },
             {
-                inputs: [Item.of('byg:blue_sandstone', 1), Item.of('immersiveengineering:mold_unpacking')],
+                inputs: [Item.of('byg:blue_sandstone', 1), Ingredient.of('#thermal:crafting/dies/unpacking')],
                 output: Item.of('byg:blue_sand', 4)
             },
             {
-                inputs: [Item.of('byg:white_sandstone', 1), Item.of('immersiveengineering:mold_unpacking')],
+                inputs: [Item.of('byg:white_sandstone', 1), Ingredient.of('#thermal:crafting/dies/unpacking')],
                 output: Item.of('byg:white_sand', 4)
             },
             {
-                inputs: [Item.of('byg:black_sandstone', 1), Item.of('immersiveengineering:mold_unpacking')],
+                inputs: [Item.of('byg:black_sandstone', 1), Ingredient.of('#thermal:crafting/dies/unpacking')],
                 output: Item.of('byg:black_sand', 4)
             },
             {
-                inputs: [Item.of('atmospheric:arid_sandstone', 1), Item.of('immersiveengineering:mold_unpacking')],
+                inputs: [Item.of('atmospheric:arid_sandstone', 1), Ingredient.of('#thermal:crafting/dies/unpacking')],
                 output: Item.of('atmospheric:arid_sand', 4)
             },
             {
-                inputs: [Item.of('atmospheric:red_arid_sandstone', 1), Item.of('immersiveengineering:mold_unpacking')],
+                inputs: [Item.of('atmospheric:red_arid_sandstone', 1), Ingredient.of('#thermal:crafting/dies/unpacking')],
                 output: Item.of('atmospheric:red_arid_sand', 4)
             },
             {
@@ -81,12 +81,12 @@ events.listen('recipes', (event) => {
             //ID Overrides
             {
                 id: 'thermal:machine/press/packing2x2/press_honeycomb_packing',
-                inputs: [Item.of('minecraft:honeycomb', 9), Item.of('immersiveengineering:mold_packing_9')],
+                inputs: [Item.of('minecraft:honeycomb', 9), Ingredient.of('#thermal:crafting/dies/unpacking')],
                 output: Item.of('minecraft:honeycomb_block', 1)
             },
             {
                 id: 'thermal:machine/press/unpacking/press_honeycomb_unpacking',
-                inputs: [Item.of('minecraft:honeycomb_block', 1), Item.of('immersiveengineering:mold_unpacking')],
+                inputs: [Item.of('minecraft:honeycomb_block', 1), Ingredient.of('#thermal:crafting/dies/unpacking')],
                 output: Item.of('minecraft:honeycomb', 9)
             }
         ]
@@ -97,14 +97,14 @@ events.listen('recipes', (event) => {
             {
                 inputs: [
                     Item.of('resourcefulbees:' + variant + '_honeycomb', 9),
-                    Item.of('immersiveengineering:mold_packing_9')
+                    Ingredient.of('#thermal:crafting/dies/packing_3x3')
                 ],
                 output: Item.of('resourcefulbees:' + variant + '_honeycomb_block', 1)
             },
             {
                 inputs: [
                     Item.of('resourcefulbees:' + variant + '_honeycomb_block', 1),
-                    Item.of('immersiveengineering:mold_unpacking')
+                    Ingredient.of('#thermal:crafting/dies/unpacking')
                 ],
                 output: Item.of('resourcefulbees:' + variant + '_honeycomb', 9)
             }
