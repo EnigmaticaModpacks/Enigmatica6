000b6e0713f70ae5ad10c97c7ffbdf65be208291
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-03-27 07:05:20 +0100

    fixed typo

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/dirt.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/dirt.js
@@ -1,4 +1,5 @@
 events.listen('item.tags', (event) => {
     event.add('forge:dirt', ['minecraft:dirt', 'byg:meadow_dirt']);
-    evemt.remove('forge:dirt', ['supplementaries:fodder']);
+
+    event.remove('forge:dirt', ['supplementaries:fodder']);
 });
