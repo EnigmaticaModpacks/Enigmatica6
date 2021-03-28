1aa8e8ab85f22fd9c3918fb7925f8375d2eb55f5
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-03-28 23:58:00 +0200

    refactored gear, plate, rod and wire unification a bit. Made them packmode dependant.

--- kubejs/server_scripts/enigmatica/kubejs/normal/recipes/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/normal/recipes/shaped.js
@@ -3,40 +3,60 @@ events.listen('recipes', (event) => {
         return;
     }
 
-    var recipes = [
-        shapedRecipe('torchmaster:megatorch', ['AAA', 'BCB', 'DCD'], {
-            A: 'torchmaster:feral_flare_lantern',
-            B: '#enigmatica:crafting_materials/diamond',
-            C: '#minecraft:logs',
-            D: '#forge:storage_blocks/gold'
-        }),
-        shapedRecipe('torchmaster:feral_flare_lantern', [' A ', 'BCB', ' A '], {
-            A: '#forge:ingots/gold',
-            B: '#forge:glass',
-            C: '#forge:storage_blocks/glowstone'
-        }),
-        shapedRecipe(
-            Item.of('compactmachines:tunnel', { definition: { id: 'compactmachines:item' } }),
-            ['ABA', 'BCB', 'DBD'],
-            {
+    const recipes = [
+        {
+            output: 'torchmaster:megatorch',
+            pattern: ['AAA', 'BCB', 'DCD'],
+            key: {
+                A: 'torchmaster:feral_flare_lantern',
+                B: '#enigmatica:crafting_materials/diamond',
+                C: '#minecraft:logs',
+                D: '#forge:storage_blocks/gold'
+            }
+        },
+        {
+            output: 'torchmaster:feral_flare_lantern',
+            pattern: [' A ', 'BCB', ' A '],
+            key: {
+                A: '#forge:ingots/gold',
+                B: '#forge:glass',
+                C: '#forge:storage_blocks/glowstone'
+            }
+        },
+        {
+            output: Item.of('compactmachines:tunnel', { definition: { id: 'compactmachines:item' } }),
+            pattern: ['ABA', 'BCB', 'DBD'],
+            key: {
                 A: 'minecraft:hopper',
                 B: '#forge:gems/dimensional',
                 C: 'occultism:wormhole_frame',
                 D: '#forge:chests'
             }
-        ),
-        shapedRecipe(
-            Item.of('compactmachines:tunnel', { definition: { id: 'compactmachines:redstone_in' } }),
-            ['ABA', 'BCB', 'DBD'],
-            {
+        },
+        {
+            output: Item.of('compactmachines:tunnel', { definition: { id: 'compactmachines:redstone_in' } }),
+            pattern: ['ABA', 'BCB', 'DBD'],
+            key: {
                 A: 'glassential:glass_redstone',
                 B: '#forge:gems/dimensional',
                 C: 'occultism:wormhole_frame',
                 D: 'minecraft:redstone_torch'
             }
-        )
+        },
+        {
+            output: 'minecraft:furnace',
+            pattern: ['LLL', 'L L', 'LLL'],
+            key: {
+                L: '#forge:stone'
+            }
+        }
     ];
-    recipes.forEach(function (recipe) {
-        event.shaped(recipe.result, recipe.pattern, recipe.key);
+
+    recipes.forEach((recipe) => {
+        if (recipe.id) {
+            event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
+        } else {
+            event.shaped(recipe.output, recipe.pattern, recipe.key);
+        }
     });
 });
