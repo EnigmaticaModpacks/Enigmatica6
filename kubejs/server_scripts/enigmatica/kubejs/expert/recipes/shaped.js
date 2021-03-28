122b35b79ccac8c9b9a6a0cc919e310f584eb991
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-03-28 21:51:02 +0200

    thermal machine frame #1300

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipes/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipes/shaped.js
@@ -2,4 +2,25 @@ events.listen('recipes', (event) => {
     if (!expertMode) {
         return;
     }
+
+    const newRecipes = [
+        {
+            output: 'thermal:machine_frame',
+            pattern: ['ABA', 'B B', 'ABA'],
+            key: {
+                A: '#forge:plates/iron_tin',
+                B: '#forge:glass_panes',
+                C: '#forge:gears/iron_aluminum'
+            },
+            id: 'thermal:machine_frame'
+        }
+    ];
+
+    newRecipes.forEach((recipe) => {
+        if (recipe.id) {
+            event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
+        } else {
+            event.shaped(recipe.output, recipe.pattern, recipe.key);
+        }
+    });
 });
