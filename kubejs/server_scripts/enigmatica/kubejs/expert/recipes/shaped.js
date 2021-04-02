7b1cd46fadf367a64379b02eec379694ab62cff2
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-04-02 22:11:54 +0200

    piston recipe #1300

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipes/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipes/shaped.js
@@ -34,6 +34,42 @@ events.listen('recipes', (event) => {
                 B: '#minecraft:coals'
             },
             id: 'minecraft:furnace'
+        },
+        {
+            output: 'minecraft:piston',
+            pattern: ['EDE', 'ABA', 'ACA'],
+            key: {
+                A: '#forge:stone',
+                B: ['#forge:ingots/iron', '#forge:ingots/aluminum', '#forge:ingots/copper', '#forge:ingots/tin'],
+                C: '#forge:dusts/redstone',
+                D: ['#forge:rods/iron', '#forge:rods/aluminum', '#forge:rods/copper', '#forge:rods/tin'],
+                E: '#minecraft:planks'
+            },
+            id: 'minecraft:piston'
+        },
+        {
+            output: Item.of('minecraft:piston', 2),
+            pattern: ['EDE', 'ABA', 'ACA'],
+            key: {
+                A: '#forge:stone',
+                B: [
+                    '#forge:ingots/silver',
+                    '#forge:ingots/lead',
+                    '#forge:ingots/gold',
+                    '#forge:ingots/nickel',
+                    '#forge:ingots/zinc'
+                ],
+                C: '#forge:dusts/redstone',
+                D: [
+                    '#forge:rods/silver',
+                    '#forge:rods/lead',
+                    '#forge:rods/gold',
+                    '#forge:rods/nickel',
+                    '#forge:rods/zinc'
+                ],
+                E: '#minecraft:planks'
+            },
+            id: 'minecraft:piston_alternative'
         }
     ];
 
