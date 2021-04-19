1b051e3fd704d1e40b24087e54856deb2631eb35
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-04-19 22:27:24 +0200

    uncommented orechid script, removed orechid datapack override - fiddles with weights a bit

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/botania/orechid.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/botania/orechid.js
@@ -3,15 +3,15 @@ events.listen('server.datapack.low_priority', (event) => {
         recipes: [
             { result: 'emendatusenigmatica:aluminum_ore', weight: 1200 },
             { result: 'emendatusenigmatica:apatite_ore', weight: 700 },
-            { result: 'emendatusenigmatica:bitumen_ore', weight: 1000 },
-            { result: 'emendatusenigmatica:cinnabar_ore', weight: 2585 },
-            { result: 'emendatusenigmatica:coal_ore', weight: 5000 },
+            { result: 'emendatusenigmatica:bitumen_ore', weight: 600 },
+            { result: 'emendatusenigmatica:cinnabar_ore', weight: 800 },
+            { result: 'emendatusenigmatica:coal_ore', weight: 3200 },
             { result: 'emendatusenigmatica:copper_ore', weight: 2000 },
             { result: 'emendatusenigmatica:diamond_ore', weight: 50 },
             { result: 'emendatusenigmatica:emerald_ore', weight: 30 },
             { result: 'emendatusenigmatica:fluorite_ore', weight: 50 },
             { result: 'emendatusenigmatica:gold_ore', weight: 500 },
-            { result: 'emendatusenigmatica:iron_ore', weight: 3000 },
+            { result: 'emendatusenigmatica:iron_ore', weight: 2500 },
             { result: 'emendatusenigmatica:lapis_ore', weight: 250 },
             { result: 'emendatusenigmatica:lead_ore', weight: 1500 },
             { result: 'emendatusenigmatica:mana_ore', weight: 500 },
@@ -43,6 +43,6 @@ events.listen('server.datapack.low_priority', (event) => {
         }
     });
 
-    //event.addJson('botania:orechid_ore_weights/orechid.json', { values: recipeList });
-    //console.log({ values: recipeList });
+    event.addJson('botania:orechid_ore_weights/orechid.json', { values: recipeList });
+    console.log({ values: recipeList });
 });
