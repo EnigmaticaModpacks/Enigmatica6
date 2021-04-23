e044e810d4c53ff12d9e97308268655a1b4f8272
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-04-23 20:44:05 +0200

    Update glyph_recipe.js

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/ars_nouveau/glyph_recipe.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/ars_nouveau/glyph_recipe.js
@@ -1,17 +1,21 @@
 events.listen('recipes', (event) => {
-    const recipes = [
-    ];
+    const recipes = [];
 
     recipes.forEach((recipe) => {
-        const re = 
-            event.custom({
-                type: 'ars_nouveau:glyph_recipe',
-                tier: recipe.tier,
-                input: recipe.input,
-                output: recipe.output
-            });
-        if (recipe.id) {
-            re.id(recipe.id);
-        }
+        recipe.id
+            ? event
+                  .custom({
+                      type: 'ars_nouveau:glyph_recipe',
+                      tier: recipe.tier,
+                      input: recipe.input,
+                      output: recipe.output
+                  })
+                  .id(recipe.id)
+            : event.custom({
+                  type: 'ars_nouveau:glyph_recipe',
+                  tier: recipe.tier,
+                  input: recipe.input,
+                  output: recipe.output
+              });
     });
 });
