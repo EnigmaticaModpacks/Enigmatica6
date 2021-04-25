b78032ddf4ca5ec9a1e2c4534cb8823e5a212ca3
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-04-25 22:39:08 +0200

    moved packmode into event, fixes loading into servers issues?

--- kubejs/server_scripts/enigmatica/kubejs/packmode.js
+++ kubejs/server_scripts/enigmatica/kubejs/packmode.js
@@ -1,26 +1,30 @@
 //priority: 1004
-const defaultConfig = {
-    mode: 'normal',
-    message: 'Valid modes are normal and expert.'
-};
-const configName = 'mode.json';
-let config = json.read(configName);
-if (!config || !config.mode) {
-    json.write(configName, defaultConfig);
-    console.log(`Created new ${configName}`);
-}
-if (config.mode == 'none') {
-    json.write(configName, defaultConfig);
-    config.mode = defaultConfig.mode;
-    console.log(`Overwrote ${configName}, because the mode 'none' was found. Valid modes are 'normal' and 'expert'.`);
-}
-global.packmode = config.mode;
-console.log(`Current packmode is: ${global.packmode}`);
-
 events.listen('server.datapack.high_priority', (event) => {
+    const defaultConfig = {
+        mode: 'normal',
+        message: 'Valid modes are normal and expert.'
+    };
+    const configName = 'mode.json';
+    let config = json.read(configName);
+    if (!config || !config.mode) {
+        json.write(configName, defaultConfig);
+        console.log(`Created new ${configName}`);
+    }
+    if (config.mode == 'none') {
+        json.write(configName, defaultConfig);
+        config.mode = defaultConfig.mode;
+        console.log(
+            `Overwrote ${configName}, because the mode 'none' was found. Valid modes are 'normal' and 'expert'.`
+        );
+    }
+    global.packmode = config.mode;
+    console.log(`Current packmode is: ${global.packmode}`);
+
     if (event.server) {
-        event.server.players.forEach((player) => {
-            setMode(player);
-        });
+        if (event.server.players) {
+            event.server.players.forEach((player) => {
+                setMode(player);
+            });
+        }
     }
 });
