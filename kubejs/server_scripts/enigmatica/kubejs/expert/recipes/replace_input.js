4ddf248e1325f0dca4f949199d45c055be488166
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-04-27 22:15:39 +0200

    thermal machine pyrolyzer #1300

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipes/replace_input.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipes/replace_input.js
@@ -8,6 +8,11 @@ events.listen('recipes', (event) => {
             filter: { id: 'create:crafting/kinetics/mechanical_piston' },
             toReplace: '#minecraft:planks',
             replaceWith: ['minecraft:piston', 'minecraft:sticky_piston']
+        },
+        {
+            filter: { id: 'thermal:machine_pyrolyzer' },
+            toReplace: 'minecraft:blaze_rod',
+            replaceWith: 'forge:gems/coal_coke'
         }
     ];
 
