9261094cbea977da2021cc676d71a45e45b4977b
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-04-27 06:54:18 +0200

    prepping for 0.4.10

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -29,11 +29,11 @@ $MODPACK_NAME = "Enigmatica6"
 $CLIENT_NAME = "Enigmatica6"
 
 # Version Of The Modpack
-$MODPACK_VERSION = "0.4.9"
+$MODPACK_VERSION = "0.4.10"
 
 # Last Version Of The Modpack
 # Needed For Changelog Parsing
-$LAST_MODPACK_VERSION = "0.4.8"
+$LAST_MODPACK_VERSION = "0.4.9"
 
 # =====================================================================//
 #  CHANGELOG SETTINGS
