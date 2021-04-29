55b86aad719ed1104597c2d0ccda19d2e86a92c2
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-04-30 00:31:08 +0200

    Update packmode.js

--- kubejs/server_scripts/enigmatica/kubejs/packmode.js
+++ kubejs/server_scripts/enigmatica/kubejs/packmode.js
@@ -1,32 +1,26 @@
 //priority: 1004
-events.listen('server.datapack.high_priority', (event) => {
-    const defaultConfig = {
-        mode: 'normal',
-        message: 'Valid modes are normal and expert.'
-    };
-    const configName = 'mode.json';
-    let config = json.read(configName);
-    if (!config || !config.mode) {
-        json.write(configName, defaultConfig);
-        console.log(`Created new ${configName}`);
-    }
-    if (config.mode == 'none') {
-        json.write(configName, defaultConfig);
-        config.mode = defaultConfig.mode;
-        console.log(
-            `Overwrote ${configName}, because the mode 'none' was found. Valid modes are 'normal' and 'expert'.`
-        );
-    }
-    global.mode = config.mode;
-    global.isNormalMode = global.mode == 'normal';
-    global.isExpertMode = global.mode == 'expert';
-    console.log(`Current packmode is: ${global.mode}`);
+const defaultConfig = {
+    mode: 'normal',
+    message: 'Valid modes are normal and expert.'
+};
+const configName = 'mode.json';
+let config = json.read(configName);
+if (!config || !config.mode) {
+    json.write(configName, defaultConfig);
+    console.log(`Created new ${configName}`);
+}
+if (config.mode == 'none') {
+    json.write(configName, defaultConfig);
+    config.mode = defaultConfig.mode;
+    console.log(`Overwrote ${configName}, because the mode 'none' was found. Valid modes are 'normal' and 'expert'.`);
+}
+global.packmode = config.mode;
+console.log(`Current packmode is: ${global.packmode}`);
 
+events.listen('server.datapack.high_priority', (event) => {
     if (event.server) {
-        if (event.server.players) {
-            event.server.players.forEach((player) => {
-                setMode(player);
-            });
-        }
+        event.server.players.forEach((player) => {
+            setMode(player);
+        });
     }
 });
