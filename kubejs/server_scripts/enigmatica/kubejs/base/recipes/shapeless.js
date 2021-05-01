41da541f8143582a515dd91267169d609f2d5c7c
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-05-01 22:19:05 +0200

    changed standing fire recipes #2074

--- kubejs/server_scripts/enigmatica/kubejs/base/recipes/shapeless.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipes/shapeless.js
@@ -304,6 +304,31 @@ events.listen('recipes', (event) => {
         {
             output: Item.of('minecraft:terracotta'),
             inputs: ['#enigmatica:washables/terracotta', 'minecraft:water_bucket']
+        },
+        {
+            inputs: ['minecraft:fire_charge', '#minecraft:coals'],
+            output: Item.of('additional_lights:fire_for_standing_torch_s', 4),
+            id: 'additional_lights:fire_for_standing_torch_s'
+        },
+        {
+            inputs: ['minecraft:fire_charge', '#minecraft:coals', '#minecraft:coals'],
+            output: Item.of('additional_lights:fire_for_standing_torch_l', 4),
+            id: 'additional_lights:fire_for_standing_torch_l'
+        },
+        {
+            inputs: ['minecraft:fire_charge', '#minecraft:coals', '#minecraft:soul_fire_base_blocks'],
+            output: Item.of('additional_lights:soul_fire_for_standing_torch_s', 4),
+            id: 'additional_lights:soul_fire_for_standing_torch_s'
+        },
+        {
+            inputs: [
+                '#minecraft:fire_charge',
+                '#minecraft:coals',
+                '#minecraft:coals',
+                '#minecraft:soul_fire_base_blocks'
+            ],
+            output: Item.of('additional_lights:soul_fire_for_standing_torch_l', 4),
+            id: 'additional_lights:soul_fire_for_standing_torch_l'
         }
     ];
 
