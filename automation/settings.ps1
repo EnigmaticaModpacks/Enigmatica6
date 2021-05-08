a7bad3045ab78fecc9e85d5262897e1cd0da6b65
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-05-08 20:16:51 +0200

    fixed modlists (thanks MelanX!)

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -74,7 +74,7 @@ $TwitchExportBuilderDLMac = "twitch-export-builder_darwin_amd64"
 $ChangelogGeneratorDL = "ChangelogGenerator-2.0.0-pre10.jar"
 
 # File name of the latest https://github.com/MelanX/ModListCreator/releases
-$ModlistCreatorJar = "ModListCreator-1.2.0.jar"
+$ModlistCreatorJar = "ModListCreator-1.2.1.jar"
 
 #=====================================================================//
 #  CLIENT FILE SETTINGS
