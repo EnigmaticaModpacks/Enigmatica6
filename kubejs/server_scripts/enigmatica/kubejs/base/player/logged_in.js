2de992773e088919f27c5beafb05b2c9d704eb64
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-05-12 22:15:17 +0200

    players now get a waystone on first login #1803

--- kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
@@ -1,6 +1,13 @@
 events.listen('player.logged_in', (event) => {
+    randomWaystone = () => {
+        const waystones = ['waystones:waystone', 'waystones:mossy_waystone', 'waystones:sandy_waystone'];
+        return waystones[Math.floor(Math.random() * waystones.length + 1)];
+    };
+
     if (!event.hasGameStage('starting_items')) {
         event.player.give(Item.of('ftbquests:book'));
+        event.player.give(Item.of(randomWaystone()));
+
         event.addGameStage('starting_items');
     }
 });
