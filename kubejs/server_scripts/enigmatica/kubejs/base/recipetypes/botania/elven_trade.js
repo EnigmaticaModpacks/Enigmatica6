43dba0cd3f0f24d8624c5a77f805ca738d0f778f
TemperedFool <TemperedFool@users.noreply.github.com>
2021-05-14 14:54:04 -0400

    Initial Magic Bee support - Botania
    Adding initial support for some magic bees - starting with botania since it seems straightforward. One bee for each metal - these are created with the process appropriate material (infusion, terra_plate, elven trading), and their combs are used for a mana discount when producing their product The bees are starting off as very simple one-trick bees, we can add more complication if something makes sense.
    
    Added an Iron Block -> Iron Bee Spawn Egg mutation to the Iron bee to support this.
    
    Added support for mana infusion, terrestrial agglomeration plate, and elven trading recipes to support this. Since I'm not using all the possible features of mana infusion (no multiple-output recipes, no catalysts) i've left a commented template in place for future expansion - I have tested it to work.

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/botania/elven_trade.js
@@ -0,0 +1,41 @@
+events.listen('recipes', (event) => {
+    const recipes = [
+        {
+            inputs: [
+                { item: 'resourcefulbees:mana_bee_spawn_egg' }
+            ],
+            output: {
+                item: 'resourcefulbees:elven_bee_spawn_egg'
+            }
+        },
+        {
+            inputs: [
+                { item: 'resourcefulbees:elven_honeycomb' },
+                { item: "resourcefulbees:elven_honeycomb" }
+            ],
+            output: {
+                item: 'botania:elementium_ingot'
+            }
+        },
+        {
+            inputs: [
+                { item: 'resourcefulbees:elven_honeycomb_block' },
+                { item: "resourcefulbees:elven_honeycomb_block" }
+            ],
+            output: {
+                item: 'botania:elementium_block'
+            },
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        const re = event.custom({
+            type: 'botania:elven_trade',
+            ingredients: recipe.inputs,
+            output: recipe.output
+        });
+        if (recipe.id) {
+            re.id(recipe.id);
+        }
+    });
+});
