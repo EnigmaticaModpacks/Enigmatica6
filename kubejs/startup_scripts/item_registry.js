6b5ac8463ab9e2933f9dca89a14096b23d331e41
pm065 <81559564+pm065@users.noreply.github.com>
2021-05-15 20:59:33 +0100

    Update item_registry.js

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -12,23 +12,8 @@ events.listen('item.registry', (event) => {
         'quintuple_alfsteel_ingot',
         'altered_recipe_indicator'
     ];
-    
-    const chunkItems = [
-        'iesnium_chunk',
-        'thallasium_chunk',
-        'starmetal_chunk',
-        'froststeel_chunk',
-        'cloggrum_chunk',
-        'utherium_chunk',
-        'regalium_chunk',
-        'elementium_chunk'
-    ];
 
     generalItems.forEach((item) => {
         event.create(item).group('KubeJS').texture(`kubejs:item/${item}`);
-    });
-    
-    chunkItems.forEach((item) => {
-        event.create(item).group('KubeJS').texture(`kubejs:item/chunks/${item}`);
     });
 });
