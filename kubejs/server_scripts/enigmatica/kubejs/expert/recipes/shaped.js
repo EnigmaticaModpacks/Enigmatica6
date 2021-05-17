48bd2c1aa95265d79651a9dfe11448d7b45cd559
TemperedFool <TemperedFool@users.noreply.github.com>
2021-05-17 16:05:23 -0400

    Bees with PhD's
    Move T1 Hive recipe override from base to normal. Add expert T1 recipe to kubejs/server_scripts/enigmatica/kubejs/expert/recipes/shaped.js - utilizing Gold Powder from NA to enforce gating.
    
    Add expert override for Apiary Breeder - requires Token of Joy from NA>

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipes/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipes/shaped.js
@@ -387,6 +387,27 @@ events.listen('recipes', (event) => {
                 E: 'eidolon:polished_wood_pillar'
             },
             id: 'bloodmagic:incense_altar'
+        },
+        { //Gate Beehive behind starting Nature's Aura
+            output: 'resourcefulbees:t1_beehive',
+            pattern: ['ACA', 'ABA', 'ACA'],
+            key: {
+                A: 'minecraft:grass',
+                B: '#forge:beehives/tier_0',
+                C: 'naturesaura:gold_powder'
+            },
+            id: 'resourcefulbees:t1_beehive'
+        },
+        {
+            output: 'resourcefulbees:apiary_breeder',
+            pattern: ['ACA', 'DBD', 'ACA'],
+            key: {
+                A: 'resourcefulbees:bee_jar',
+                B: 'resourcefulbees:apiary_storage',
+                C: 'naturesaura:token_joy',
+                D: '#minecraft:flowers'
+            },
+            id: 'resourcefulbees:apiary_breeder'
         }
     ];
 
