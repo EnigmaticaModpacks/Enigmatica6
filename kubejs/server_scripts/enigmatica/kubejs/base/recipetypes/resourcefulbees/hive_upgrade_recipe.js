b0d9f5bc93df48805eb99381141bd549aef61f27
TemperedFool <TemperedFool@users.noreply.github.com>
2021-06-08 09:11:48 -0400

    T4 Hive/T1 Apiary use all Honey Blocks
    What it says on the tin. Turns out I can take 50% responsibility here; the default T4 Hive recipe is coded to Minecraft Honey Blocks, and I did the same with the T1 Apiary recipe. Fixed both to use all honey blocks.

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/resourcefulbees/hive_upgrade_recipe.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/resourcefulbees/hive_upgrade_recipe.js
@@ -4,7 +4,17 @@ events.listen('recipes', (event) => {
             pattern: ['ABA', 'BCB', 'ABA'],
             key: {
                 A: {tag: 'resourcefulbees:resourceful_honeycomb_block'},
-                B: {item: 'minecraft:honey_block'},
+                B: {tag: 'resourcefulbees:resourceful_honey_block'},
+                C: {item: 'resourcefulbees:t3_beehive'}
+            },
+            result: {item: 'resourcefulbees:t4_beehive'},
+            id: 'resourcefulbees:t4_beehive'
+        },
+        {
+            pattern: ['ABA', 'BCB', 'ABA'],
+            key: {
+                A: {tag: 'resourcefulbees:resourceful_honeycomb_block'},
+                B: {tag: 'resourcefulbees:resourceful_honey_block'},
                 C: {item: 'resourcefulbees:t4_beehive'}
             },
             result: {item: 'resourcefulbees:t1_apiary'},
