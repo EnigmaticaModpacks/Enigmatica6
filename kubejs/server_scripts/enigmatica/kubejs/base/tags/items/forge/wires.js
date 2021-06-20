6c0648951a20dd170359a016af7471173d71b204
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-06-20 21:05:35 +0200

    added lead wire to forge:wires, removed tags that IE added natively

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/wires.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/wires.js
@@ -1,13 +1,9 @@
 onEvent('item.tags', (event) => {
-    event
-        .get('forge:wires')
-        .add('immersiveengineering:wire_steel')
-        .add('immersiveengineering:wire_aluminum')
-        .add('immersiveengineering:wire_copper')
-        .add('immersiveengineering:wire_electrum');
-
-    event.get('forge:wires/steel').add('immersiveengineering:wire_steel');
-    event.get('forge:wires/aluminum').add('immersiveengineering:wire_aluminum');
-    event.get('forge:wires/copper').add('immersiveengineering:wire_copper');
-    event.get('forge:wires/electrum').add('immersiveengineering:wire_electrum');
+    event.add('forge:wires', [
+        'immersiveengineering:wire_steel',
+        'immersiveengineering:wire_aluminum',
+        'immersiveengineering:wire_copper',
+        'immersiveengineering:wire_electrum',
+        'immersiveengineering:wire_lead'
+    ]);
 });
