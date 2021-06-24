373179556e3898ed718c4a0610eb48cb7d29b7b8
MuteTiefling <mutetiefling@gmail.com>
2021-06-24 00:34:40 -0400

    Update animal_spawner.js
    HAM!

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/naturesaura/animal_spawner.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/naturesaura/animal_spawner.js
@@ -6,64 +6,102 @@ onEvent('recipes', (event) => {
     var data = {
         recipes: [
             {
-                inputs: ['quark:bottled_cloud', 'resourcefulbees:sand_honeycomb', 'minecraft:sand'],
+                inputs: [
+                    'naturesaura:birth_spirit',
+                    'quark:bottled_cloud',
+                    'resourcefulbees:sand_honeycomb',
+                    'minecraft:sand'
+                ],
                 entity: 'alexsmobs:guster',
                 aura: 150000,
                 time: 120
             },
             {
-                inputs: ['#forge:dusts/fluorite', 'resourcefulbees:electrum_honeycomb', 'powah:charged_snowball'],
+                inputs: [
+                    'naturesaura:birth_spirit',
+                    '#forge:dusts/fluorite',
+                    'resourcefulbees:electrum_honeycomb',
+                    'powah:charged_snowball'
+                ],
                 entity: 'thermal:blitz',
                 aura: 150000,
                 time: 120
             },
             {
-                inputs: ['#forge:dusts/lapis', 'resourcefulbees:icy_honeycomb', 'minecraft:blue_ice'],
+                inputs: [
+                    'naturesaura:birth_spirit',
+                    '#forge:dusts/lapis',
+                    'resourcefulbees:icy_honeycomb',
+                    'minecraft:blue_ice'
+                ],
                 entity: 'thermal:blizz',
                 aura: 150000,
                 time: 120
             },
             {
-                inputs: ['#forge:dusts/apatite', 'resourcefulbees:rocky_honeycomb', 'minecraft:basalt'],
+                inputs: [
+                    'naturesaura:birth_spirit',
+                    '#forge:dusts/apatite',
+                    'resourcefulbees:rocky_honeycomb',
+                    'minecraft:basalt'
+                ],
                 entity: 'thermal:basalz',
                 aura: 150000,
                 time: 120
             },
             {
-                inputs: ['#forge:dusts/sulfur', 'resourcefulbees:coal_honeycomb', 'minecraft:nether_bricks'],
+                inputs: [
+                    'naturesaura:birth_spirit',
+                    '#forge:dusts/sulfur',
+                    'resourcefulbees:coal_honeycomb',
+                    'minecraft:nether_bricks'
+                ],
                 entity: 'minecraft:blaze',
                 aura: 150000,
                 time: 120,
                 id: 'naturesaura:animal_spawner/blaze'
             },
             {
-                inputs: ['resourcefulbees:bloody_honeycomb', 'minecraft:blue_ice'],
+                inputs: ['naturesaura:birth_spirit', 'resourcefulbees:bloody_honeycomb', 'minecraft:blue_ice'],
                 entity: 'ars_nouveau:wilden_guardian',
                 aura: 250000,
                 time: 120
             },
             {
-                inputs: ['resourcefulbees:bloody_honeycomb', 'valhelsia_structures:bone_pile'],
+                inputs: [
+                    'naturesaura:birth_spirit',
+                    'resourcefulbees:bloody_honeycomb',
+                    'valhelsia_structures:bone_pile'
+                ],
                 entity: 'ars_nouveau:wilden_hunter',
                 aura: 150000,
                 time: 120
             },
             {
-                inputs: ['resourcefulbees:bloody_honeycomb', 'astralsorcery:nocturnal_powder'],
+                inputs: [
+                    'naturesaura:birth_spirit',
+                    'resourcefulbees:bloody_honeycomb',
+                    'astralsorcery:nocturnal_powder'
+                ],
                 entity: 'ars_nouveau:wilden_stalker',
                 aura: 150000,
                 time: 120
             },
             {
-                inputs: ['naturesaura:token_anger', 'eidolon:shadow_gem', '#forge:ingots/forgotten_metal'],
+                inputs: [
+                    'farmersdelight:honey_glazed_ham_block',
+                    'naturesaura:token_anger',
+                    'eidolon:shadow_gem',
+                    '#forge:ingots/forgotten_metal'
+                ],
                 entity: 'undergarden:masticator',
-                aura: 500000,
+                aura: 5000000,
                 time: 200
             }
         ]
     };
     data.recipes.forEach((recipe) => {
-        let ingredients = [Ingredient.of('naturesaura:birth_spirit').toJson()];
+        let ingredients = [];
 
         recipe.inputs.forEach((input) => {
             ingredients.push(Ingredient.of(input).toJson());
