70a497dfb6c2db7b6e8823bca98f24a6232547dc
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-07-02 21:22:42 +0200

    Update settings.ps1

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -29,11 +29,11 @@ $MODPACK_NAME = "Enigmatica6"
 $CLIENT_NAME = "Enigmatica6"
 
 # Version Of The Modpack
-$MODPACK_VERSION = "0.5.2"
+$MODPACK_VERSION = "0.5.3"
 
 # Last Version Of The Modpack
 # Needed For Changelog Parsing
-$LAST_MODPACK_VERSION = "0.5.1"
+$LAST_MODPACK_VERSION = "0.5.2"
 
 # =====================================================================//
 #  CHANGELOG SETTINGS
