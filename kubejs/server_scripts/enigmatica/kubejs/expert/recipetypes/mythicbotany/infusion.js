1d3c06f789c0c97de01a1e6e567f2ea214e4c973
MuteTiefling <mutetiefling@gmail.com>
2021-07-14 14:00:12 -0400

    [Expert] Botania Tweaks
    Mana Ring moved to Infusion crafting
    ![image](https://user-images.githubusercontent.com/9543430/125663167-6210ceda-a22e-46d5-8472-55822e8e7b14.png)
    
    Terrasteel. Uses materials from three realms now
    ![image](https://user-images.githubusercontent.com/9543430/125668599-d837494a-23bd-4873-af0d-ddcc0e507c43.png)
    
    Alfsteel. Similar, spans three realms.
    ![image](https://user-images.githubusercontent.com/9543430/125668546-f8185390-b2f1-4b05-84ab-c1d60c04bf5a.png)
    
    Gaia Ingot moved to infusion. Moderately more difficult.
    ![image](https://user-images.githubusercontent.com/9543430/125669695-65c03ec3-3c51-4eeb-8366-b06816f80ea6.png)
    
    Terrestrial Spawn Egg:
    ![image](https://user-images.githubusercontent.com/9543430/125669819-7c9507a7-8c18-4b4b-9304-876466934613.png)

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/mythicbotany/infusion.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/mythicbotany/infusion.js
@@ -5,42 +5,99 @@ onEvent('recipes', (event) => {
     const recipes = [
         {
             inputs: [
-                { item: 'resourcefulbees:mana_bee_spawn_egg' }
+                { item: 'resourcefulbees:mana_bee_spawn_egg' },
+                { item: 'botania:mana_pearl' },
+                { item: 'botania:mana_diamond' },
+                { item: 'botania:quartz_mana' }
             ],
-            output: {
-                item: 'resourcefulbees:terrestrial_bee_spawn_egg'
-            },
+            output: { item: 'resourcefulbees:terrestrial_bee_spawn_egg' },
             mana: 2000000,
             fromColor: 255,
             toColor: 65280
         },
         {
             inputs: [
-                { item: 'resourcefulbees:terrestrial_honeycomb' },
+                { tag: 'forge:nuggets/terminite' },
+                { tag: 'forge:nuggets/terminite' },
+                { tag: 'forge:nuggets/terminite' },
                 { item: 'botania:mana_pearl' },
-                { item: 'botania:mana_diamond' }
+                { item: 'resourcefulbees:terrestrial_honeycomb' },
+                { item: 'resourcefulbees:terrestrial_honeycomb' },
+                { item: 'resourcefulbees:terrestrial_honeycomb' },
+                { item: 'botania:mana_diamond' },
+                { tag: 'forge:nuggets/iesnium' },
+                { tag: 'forge:nuggets/iesnium' },
+                { tag: 'forge:nuggets/iesnium' },
+                { item: 'botania:quartz_mana' }
             ],
-            output: {
-                item: 'botania:terrasteel_ingot'
-            },
+            output: { item: 'botania:terrasteel_ingot' },
             mana: 300000,
             fromColor: 255,
             toColor: 65280
         },
         {
             inputs: [
-                { item: 'resourcefulbees:elven_honeycomb' },
-                { tag: 'forge:gems/dragonstone' },
-                { item: 'botania:pixie_dust' }
+                { tag: 'forge:nuggets/terminite' },
+                { tag: 'forge:nuggets/terminite' },
+                { tag: 'forge:nuggets/terminite' },
+                { item: 'botania:mana_pearl' },
+                { tag: 'forge:nuggets/manasteel' },
+                { tag: 'forge:nuggets/manasteel' },
+                { tag: 'forge:nuggets/manasteel' },
+                { item: 'botania:mana_diamond' },
+                { tag: 'forge:nuggets/iesnium' },
+                { tag: 'forge:nuggets/iesnium' },
+                { tag: 'forge:nuggets/iesnium' },
+                { item: 'botania:quartz_mana' }
             ],
-            output: {
-                item: 'mythicbotany:alfsteel_ingot'
-            },
+            output: { item: 'botania:terrasteel_ingot' },
+            mana: 500000,
+            fromColor: 255,
+            toColor: 65280,
+            id: 'mythicbotany:mythicbotany_infusion/terrasteel_ingot'
+        },
+        {
+            inputs: [
+                { tag: 'forge:nuggets/nebu' },
+                { tag: 'forge:nuggets/nebu' },
+                { tag: 'forge:nuggets/nebu' },
+                { item: 'botania:pixie_dust' },
+                { item: 'resourcefulbees:elven_honeycomb' },
+                { item: 'resourcefulbees:elven_honeycomb' },
+                { item: 'resourcefulbees:elven_honeycomb' },
+                { item: 'botania:dragonstone' },
+                { tag: 'forge:nuggets/utherium' },
+                { tag: 'forge:nuggets/utherium' },
+                { tag: 'forge:nuggets/utherium' },
+                { item: 'create:rose_quartz' }
+            ],
+            output: { item: 'mythicbotany:alfsteel_ingot' },
             mana: 1500000,
             fromColor: 16711821,
             toColor: 16750080
+        },
+        {
+            inputs: [
+                { tag: 'forge:nuggets/nebu' },
+                { tag: 'forge:nuggets/nebu' },
+                { tag: 'forge:nuggets/nebu' },
+                { item: 'botania:pixie_dust' },
+                { tag: 'forge:nuggets/elementium' },
+                { tag: 'forge:nuggets/elementium' },
+                { tag: 'forge:nuggets/elementium' },
+                { item: 'botania:dragonstone' },
+                { tag: 'forge:nuggets/utherium' },
+                { tag: 'forge:nuggets/utherium' },
+                { tag: 'forge:nuggets/utherium' },
+                { item: 'create:rose_quartz' }
+            ],
+            output: { item: 'mythicbotany:alfsteel_ingot' },
+            mana: 2000000,
+            fromColor: 16711821,
+            toColor: 16750080,
+            id: 'mythicbotany:mythicbotany_infusion/alfsteel_ingot'
         }
-];
+    ];
 
     recipes.forEach((recipe) => {
         const re = event.custom({
