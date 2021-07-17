328a43560033c741139f70097cbe133005ebbd4c
MuteTiefling <mutetiefling@gmail.com>
2021-07-16 23:14:04 -0400

    [Expert] Mid game relief recipes
    More mid game relief from Eidolon Crucible
    
    Soul Gem
    ![image](https://user-images.githubusercontent.com/9543430/126023223-7d3920f7-0dab-43af-9b50-baefd00d6631.png)
    
    Polished Planks:
    ![image](https://user-images.githubusercontent.com/9543430/126023249-79b8618e-0807-4d2a-a6f7-2b7ec6073225.png)
    
    Ender Calx
    ![image](https://user-images.githubusercontent.com/9543430/126023262-a309ea30-a474-4925-9874-e927f9f4bc6f.png)
    
    Warped Sprouts:
    ![image](https://user-images.githubusercontent.com/9543430/126023269-27391c77-58fb-4a2d-a81e-abe23866e435.png)

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/bloodmagic/alchemytable.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/bloodmagic/alchemytable.js
@@ -238,6 +238,22 @@ onEvent('recipes', (event) => {
                 syphon: 200,
                 ticks: 200,
                 orbLevel: 1
+            },
+            {
+                inputs: ['eidolon:enchanted_ash', '#forge:dusts/ender_pearl'],
+                output: 'eidolon:ender_calx',
+                count: 2,
+                syphon: 50,
+                ticks: 100,
+                orbLevel: 1
+            },
+            {
+                inputs: ['minecraft:warped_fungus', 'eidolon:ender_calx', '#forge:crops/nether_wart'],
+                output: 'eidolon:warped_sprouts',
+                count: 2,
+                syphon: 200,
+                ticks: 200,
+                orbLevel: 1
             }
         ]
     };
