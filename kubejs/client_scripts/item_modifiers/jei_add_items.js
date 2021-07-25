91ab3858fe27f2ea4e9cef747974ec21d28fc935
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-07-25 23:24:04 +0200

    added max level tetra hammer and short blade to jei (credit TheBoo)

--- kubejs/client_scripts/item_modifiers/jei_add_items.js
+++ kubejs/client_scripts/item_modifiers/jei_add_items.js
@@ -7,7 +7,30 @@ onEvent('jei.add.items', (event) => {
         Item.of('tconstruct:tinkers_anvil').nbt({ texture: 'emendatusenigmatica:steel_block' }),
         'lootr:lootr_chest',
         'lootr:lootr_barrel',
-        'lootr:lootr_trapped_chest'
+        'lootr:lootr_trapped_chest',
+        Item.of('tetra:modular_double', {
+            'double/handle': 'double/basic_handle',
+            HideFlags: 1,
+            'double/head_left': 'double/basic_hammer_left',
+            'double/head_right': 'double/basic_hammer_right',
+            Damage: 0,
+            'double/basic_hammer_right_material': 'basic_hammer/alfsteel',
+            'double/basic_handle_material': 'basic_handle/end_rod',
+            'double/basic_hammer_left_material': 'basic_hammer/alfsteel'
+        }),
+        Item.of('tetra:modular_sword', {
+            'sword/wide_guard_material': 'wide_guard/starmetal',
+            'sword/guard': 'sword/wide_guard',
+            'sword/short_blade_material': 'short_blade/eternal_crystal',
+            'sword/blade': 'sword/short_blade',
+            'sword/pommel': 'sword/decorative_pommel',
+            'sword/basic_hilt_material': 'basic_hilt/end_rod',
+            HideFlags: 1,
+            'sword/hilt': 'sword/basic_hilt',
+            Damage: 0,
+            'sword/blade:arrested': 0,
+            'sword/decorative_pommel_material': 'decorative_pommel/oak'
+        })
     ];
 
     items.forEach((item) => event.add(item));
