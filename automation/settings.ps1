e06d3bb2bdb8fa1452e68b9af962087a822a1626
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-07-28 22:50:57 +0200

    changelogs

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -59,7 +59,7 @@ $CLIENT_CHANGELOG = "The Changelog is currently being written, it should be done
 $GAME_VERSIONS = @(8203)
 
 # Can be "alpha", "beta" or "release"
-$CLIENT_RELEASE_TYPE = "alpha"
+$CLIENT_RELEASE_TYPE = "beta"
 
 #=====================================================================//
 #  DEPENDENCIES URL
