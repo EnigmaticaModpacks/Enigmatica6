dc0194fb83dc1c26d731052d3c86896c5f650ff5
MuteTiefling <mutetiefling@gmail.com>
2021-08-03 19:03:51 -0400

    [Expert] Enchanting Tweaks
    Hellshelf:
    ![image](https://user-images.githubusercontent.com/9543430/128085537-23316e84-6c7b-4e31-a38a-a7a891357849.png)
    
    Seashelf:
    ![image](https://user-images.githubusercontent.com/9543430/128085565-0b1e02bd-084c-44c2-8930-25aa6062642e.png)
    
    Endshelf:
    ![image](https://user-images.githubusercontent.com/9543430/128086222-99a28b03-d700-460b-b223-77d1a5aa2fe1.png)
    
    Got the override working by using patchouli safe removal methods. However, I've noticed that the JEI Yeeter is not working anymore. Not sure when this broke.
    
    Given the placement of these shelves is at the same level as Pedestals XP enchanter and XP anvil, I figured those ought to move back a bit.
    
    So:
    XP Anvil
    ![image](https://user-images.githubusercontent.com/9543430/128097164-e11a49fa-f1d8-41db-891a-ee85e08aafb4.png)
    
    Enchanter
    ![image](https://user-images.githubusercontent.com/9543430/128097175-2f9a0a25-b109-4ec1-b8a3-f8e6fc736dc2.png)

--- kubejs/client_scripts/expert/item_modifiers/jei_hide_recipes.js
+++ kubejs/client_scripts/expert/item_modifiers/jei_hide_recipes.js
@@ -2,6 +2,7 @@ onEvent('jei.yeet.recipes', (event) => {
     if (global.isExpertMode == false) {
         return;
     }
+    //console.log('JEI RECIPE TYPES: ' + event.getCategoryIds());
     recipesToHide.forEach((recipe) => {
         recipe.recipes_by_id.forEach((id) => {
             if (recipe.category == 'minecraft:crafting') {
@@ -17,6 +18,7 @@ onEvent('jei.yeet.recipes', (event) => {
                     // do nothing
                 }
             }
+            console.log(`Category: ${recipe.category}, Yeeting: ${id}`);
             event.yeet(recipe.category, id);
         });
     });
