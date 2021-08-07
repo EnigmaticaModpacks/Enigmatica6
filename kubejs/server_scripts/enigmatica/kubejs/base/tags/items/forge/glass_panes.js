4d628a36867d1f363db440c4b66a2b91e61f39d3
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-08-07 23:05:26 +0200

    expanded on glass_panes/color tags

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/glass_panes.js
@@ -0,0 +1,10 @@
+onEvent('item.tags', (event) => {
+    colors.forEach((color) => {
+        event.add(`forge:glass_panes/${color}`, [
+            `#chipped:${color}_stained_glass_pane`,
+            `connectedglass:borderless_glass_${color}_pane`,
+            `connectedglass:clear_glass_${color}_pane`,
+            `connectedglass:scratched_glass_${color}_pane`
+        ]);
+    });
+});
