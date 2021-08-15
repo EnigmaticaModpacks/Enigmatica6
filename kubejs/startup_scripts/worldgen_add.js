f311071be10f8af8ddee3a51b3a07516df599d5c
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-08-15 22:52:59 +0200

    this actually seems to work, completes #3000

--- kubejs/startup_scripts/worldgen_add.js
+++ kubejs/startup_scripts/worldgen_add.js
@@ -2,10 +2,10 @@ onEvent('worldgen.add', (event) => {
     event.addSpawn((spawn) => {
         spawn.category = 'creature';
         spawn.entity = 'minecraft:turtle';
-        spawn.weight = 40;
+        spawn.weight = 60;
         spawn.minCount = 1;
         spawn.maxCount = 3;
         spawn.biomes.blacklist = false;
-        spawn.biomes.values = ['#beach'];
+        spawn.biomes.values = ['#beach', '#ocean'];
     });
 });
