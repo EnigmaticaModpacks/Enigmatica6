dc000947f94e6c3842af8c33e31a4431305624b8
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-08-16 19:28:18 +0200

    ignore smoothboot.json

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -148,7 +148,8 @@ $CONFIGS_TO_REMOVE_FROM_CLIENT_FILES = @(
 	"contentcreatorintegration/default/constants.json",
 	"storagedrawers-client.toml",
 	"blue_skies-client.toml",
-	"dummmmmmy-client.toml"
+	"dummmmmmy-client.toml",
+	"smoothboot.json"
 )
 
 $FOLDERS_TO_REMOVE_FROM_CLIENT_FILES = @("local/ftbutilities", "local/ftbchunks/data", "local/ftbultimine")
