484f917f98a060681ba0514e52abd75f46e67fd6
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-08-23 22:53:02 +0200

    put function into the only place it's used

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/compound/crushing.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/compound/crushing.js
@@ -16,4 +16,41 @@ onEvent('recipes', (event) => {
     recipes.forEach((recipe) => {
         recipetypes_crushing(event, recipe);
     });
+
+    recipetypes_crushing = (event, recipe) => {
+        event.custom({
+            type: 'pedestals:pedestal_crushing',
+            ingredient: recipe.input,
+            result: recipe.output
+        });
+        event.custom({
+            type: 'occultism:crushing',
+            ingredient: recipe.input,
+            result: recipe.output,
+            crushing_time: recipe.duration,
+            ignore_crushing_multiplier: recipe.ignore_occultism_multiplier
+        });
+        event.custom({
+            type: 'astralsorcery:infuser',
+            fluidInput: 'astralsorcery:liquid_starlight',
+            input: recipe.input,
+            output: recipe.output,
+            consumptionChance: 0.1,
+            duration: recipe.duration,
+            consumeMultipleFluids: false,
+            acceptChaliceInput: true,
+            copyNBTToOutputs: false
+        });
+        event.custom({
+            type: 'industrialforegoing:crusher',
+            input: recipe.input,
+            output: recipe.output
+        });
+        event.recipes.thermal
+            .pulverizer([recipe.output, recipe.secondary_output], recipe.input)
+            .experience(recipe.experience);
+        event.recipes.mekanism.enriching(recipe.output, recipe.input);
+        event.recipes.immersiveengineering.crusher(recipe.output, recipe.input, recipe.secondary_output);
+        event.recipes.create.milling([recipe.output, recipe.secondary_output], recipe.input);
+    };
 });
