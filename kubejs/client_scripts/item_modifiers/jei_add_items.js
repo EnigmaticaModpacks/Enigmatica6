65f4833fe4c1d27c077dbf2cd24766e3afa0d990
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-08-23 22:54:37 +0200

    update to latest nbt format

--- kubejs/client_scripts/item_modifiers/jei_add_items.js
+++ kubejs/client_scripts/item_modifiers/jei_add_items.js
@@ -1,10 +1,10 @@
 onEvent('jei.add.items', (event) => {
     const items = [
-        Item.of('tconstruct:tinker_station').nbt({ texture: 'minecraft:oak_planks' }),
-        Item.of('tconstruct:crafting_station').nbt({ texture: 'minecraft:oak_planks' }),
-        Item.of('tconstruct:part_builder').nbt({ texture: 'minecraft:oak_planks' }),
-        Item.of('tconstruct:scorched_anvil').nbt({ texture: 'emendatusenigmatica:steel_block' }),
-        Item.of('tconstruct:tinkers_anvil').nbt({ texture: 'emendatusenigmatica:steel_block' }),
+        Item.of('tconstruct:tinker_station', { texture: 'minecraft:oak_planks' }),
+        Item.of('tconstruct:crafting_station', { texture: 'minecraft:oak_planks' }),
+        Item.of('tconstruct:part_builder', { texture: 'minecraft:oak_planks' }),
+        Item.of('tconstruct:scorched_anvil', { texture: 'emendatusenigmatica:steel_block' }),
+        Item.of('tconstruct:tinkers_anvil', { texture: 'emendatusenigmatica:steel_block' }),
         'lootr:lootr_chest',
         'lootr:lootr_barrel',
         'lootr:lootr_trapped_chest',
