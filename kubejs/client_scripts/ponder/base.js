84244a181fc55269f7f3e0248c0baa37b9391ac2
Max <maxh2709@gmail.com>
2021-09-01 00:01:23 +0200

    Add some example Industrial Foregoing ponders

---
+++ kubejs/client_scripts/ponder/base.js
@@ -0,0 +1,10 @@
+// priority: 10
+
+onEvent('ponder.tag.registry', (event) => {
+  event.create(
+    'enigmatica6:industrial_foregoing',
+    'industrialforegoing:latex_processing_unit',
+    'Industrial Foregoing',
+    'Learn more about the Industrial Foregoing mod and its various intriguing mechanics!'
+  );
+});
