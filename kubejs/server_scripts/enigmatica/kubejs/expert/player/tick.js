4f7314a9a6ee97a977261bea55ed1f8e6f85675b
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-08-31 23:35:30 +0200

    slight cleanup

--- kubejs/server_scripts/enigmatica/kubejs/expert/player/tick.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/player/tick.js
@@ -4,14 +4,10 @@ onEvent('player.tick', (event) => {
     }
     const hotIngot = 'kubejs:hot_compressed_iron_ingot';
     const player = event.player;
-
-    if (player.isInWater()) {
-        const playerHasHotIngot = playerHas(hotIngot, player);
-        if (playerHasHotIngot) {
-            console.log('trying to convert hot ingots');
-
+    const playerIsInWater = player.isInWater();
+    if (playerIsInWater) {
+        if (playerHas(hotIngot, player)) {
             let hotIngotCount = player.inventory.count(hotIngot);
-            console.log('hot ingot count: ' + hotIngotCount);
             player.inventory.clear(Item.of(hotIngot));
             player.give(Item.of('pneumaticcraft:ingot_iron_compressed', hotIngotCount));
             player.sendInventoryUpdate();
