5828f34916eb7446ec364a88c2b068a24538c930
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-09-06 22:52:58 +0200

    added dry rubber recipe pre-fractioning still

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/thermal/machine/bottler.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/thermal/machine/bottler.js
@@ -1,4 +1,5 @@
 onEvent('recipes', (event) => {
+    const idPrefix = 'enigmatica:expert/thermal/bottler/';
     if (global.isExpertMode == false) {
         return;
     }
@@ -8,9 +9,16 @@ onEvent('recipes', (event) => {
             input: 'minecraft:glass_bottle',
             fluid: Fluid.of('thermal:syrup', 250),
             output: 'kubejs:syrup_bottle'
+        },
+        {
+            input: '#forge:dusts/sulfur',
+            fluid: Fluid.of('industrialforegoing:latex', 900),
+            output: 'industrialforegoing:dryrubber'
         }
     ];
     recipes.forEach((recipe) => {
-        event.recipes.thermal.bottler(recipe.output, [recipe.fluid, recipe.input]);
+        event.recipes.thermal
+            .bottler(recipe.output, [recipe.fluid, recipe.input])
+            .id(idPrefix + recipe.output.split(':')[1]);
     });
 });
