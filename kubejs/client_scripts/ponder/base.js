c155b9d2261b60edd9ebb8bb346e9e061813107e
TemperedFool <TemperedFool@users.noreply.github.com>
2021-09-06 22:38:28 -0400

    Cobblegen and Stonegen Ponders
    Using PonderJS, and cribbing heavily off of the ponder work of @MaxNeedsSnacks, I've added two ponders to Cobblestone & Stone respectively, and all of the various stone types you can generate from the Targeted/Random generators. Should give a general overview of vanilla generators for anyone who's unfamiliar, and how to generate either specific or random variants.

--- kubejs/client_scripts/ponder/base.js
+++ kubejs/client_scripts/ponder/base.js
@@ -2,9 +2,15 @@
 
 onEvent('ponder.tag.registry', (event) => {
     event.create(
-        'enigmatica:industrial_foregoing',
-        'industrialforegoing:latex_processing_unit',
-        'Industrial Foregoing',
-        'Learn more about the Industrial Foregoing mod and its intriguing mechanics!'
+      'enigmatica:industrial_foregoing',
+      'industrialforegoing:latex_processing_unit',
+      'Industrial Foregoing',
+      'Learn more about the Industrial Foregoing mod and its intriguing mechanics!'
     );
+    event.create(
+      'enigmatica:basics',
+      'minecraft:cobblestone',
+      'Basic Mechanics',
+      'Learn about some basic (sometimes vanilla!) mechanics that will come in handy.'
+    )
 });
