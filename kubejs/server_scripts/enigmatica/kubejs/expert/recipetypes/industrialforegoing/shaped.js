13fd8836be29e03c56946d87c2d771a6f1559d71
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-09-09 21:58:29 +0200

    updated IF pity machine frame recipe #1300

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/industrialforegoing/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/industrialforegoing/shaped.js
@@ -18,11 +18,12 @@ onEvent('recipes', (event) => {
     const newRecipes = [
         {
             output: 'industrialforegoing:machine_frame_pity',
-            pattern: ['CCC', 'ABA', 'CCC'],
+            pattern: ['CDC', 'ABA', 'CDC'],
             key: {
-                A: { item: 'immersiveengineering:concrete_leaded' },
-                B: { item: 'thermal:machine_frame' },
-                C: { item: 'create:andesite_casing' }
+                A: 'immersiveengineering:concrete_leaded',
+                B: 'thermal:machine_frame',
+                C: 'create:andesite_casing',
+                D: 'immersiveengineering:component_iron'
             },
             id: 'industrialforegoing:machine_frame_pity'
         }
