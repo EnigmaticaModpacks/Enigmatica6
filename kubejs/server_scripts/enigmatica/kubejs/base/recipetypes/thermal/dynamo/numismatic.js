efd94cddcbb2400fcad9ebc5b79e277cb743daaf
MuteTiefling <mutetiefling@gmail.com>
2021-10-03 21:30:13 -0400

    refactor thermal fuel scripts
    refactor thermal fuel scripts

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/thermal/dynamo/numismatic.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/thermal/dynamo/numismatic.js
@@ -1,73 +1,24 @@
 onEvent('recipes', (event) => {
     event.remove({ type: 'thermal:numismatic_fuel' });
     var multiplier = 40;
-    var data = {
-        recipes: [
-            {
-                coin: 'forge:coins/gold',
-                energy: 64000
-            },
-            {
-                coin: 'forge:coins/invar',
-                energy: 48000
-            },
-            {
-                coin: 'forge:coins/iron',
-                energy: 32000
-            },
-            {
-                coin: 'forge:coins/enderium',
-                energy: 160000
-            },
-            {
-                coin: 'forge:coins/lead',
-                energy: 48000
-            },
-            {
-                coin: 'forge:coins/lumium',
-                energy: 80000
-            },
-            {
-                coin: 'forge:coins/nickel',
-                energy: 64000
-            },
-            {
-                coin: 'forge:coins/signalum',
-                energy: 80000
-            },
-            {
-                coin: 'forge:coins/silver',
-                energy: 48000
-            },
-            {
-                coin: 'forge:coins/tin',
-                energy: 32000
-            },
-            {
-                coin: 'forge:coins/bronze',
-                energy: 40000
-            },
-            {
-                coin: 'forge:coins/constantan',
-                energy: 56000
-            },
-            {
-                coin: 'forge:coins/copper',
-                energy: 32000
-            },
-            {
-                coin: 'forge:coins/electrum',
-                energy: 60000
-            }
-        ]
-    };
-    data.recipes.forEach((recipe) => {
-        event.recipes.thermal.numismatic_fuel({
-            type: 'thermal.numismatic_fuel',
-            ingredient: {
-                tag: recipe.coin
-            },
-            energy: recipe.energy * multiplier
-        });
+
+    const recipes = [
+        { input: '#forge:coins/gold', energy: 64000 },
+        { input: '#forge:coins/invar', energy: 48000 },
+        { input: '#forge:coins/iron', energy: 32000 },
+        { input: '#forge:coins/enderium', energy: 160000 },
+        { input: '#forge:coins/lead', energy: 48000 },
+        { input: '#forge:coins/lumium', energy: 80000 },
+        { input: '#forge:coins/nickel', energy: 64000 },
+        { input: '#forge:coins/signalum', energy: 80000 },
+        { input: '#forge:coins/silver', energy: 48000 },
+        { input: '#forge:coins/tin', energy: 32000 },
+        { input: '#forge:coins/bronze', energy: 40000 },
+        { input: '#forge:coins/constantan', energy: 56000 },
+        { input: '#forge:coins/copper', energy: 32000 },
+        { input: '#forge:coins/electrum', energy: 60000 }
+    ];
+    recipes.forEach((recipe) => {
+        event.recipes.thermal.numismatic_fuel(recipe.input).energy(recipe.energy * multiplier);
     });
 });
