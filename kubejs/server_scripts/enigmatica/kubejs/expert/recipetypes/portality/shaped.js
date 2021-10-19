d9262ac063eb68ac01d4b2230ea8cb7d29fc98ce
MuteTiefling <mutetiefling@gmail.com>
2021-10-19 12:45:15 -0400

    portality modules

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/portality/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/portality/shaped.js
@@ -13,6 +13,50 @@ onEvent('recipes', (event) => {
                 C: 'immersiveengineering:electron_tube'
             },
             id: 'portality:frame'
+        },
+        {
+            output: Item.of('portality:module_energy'),
+            pattern: ['ABA', 'CDC', 'ABA'],
+            key: {
+                A: 'portality:frame',
+                B: 'thermal:energy_cell',
+                C: 'pneumaticcraft:logistics_core',
+                D: 'xnet:wireless_router'
+            },
+            id: 'portality:energy_input'
+        },
+        {
+            output: Item.of('portality:module_items'),
+            pattern: ['ABA', 'CDC', 'ABA'],
+            key: {
+                A: 'portality:frame',
+                B: 'immersiveengineering:sorter',
+                C: 'pneumaticcraft:logistics_core',
+                D: 'xnet:wireless_router'
+            },
+            id: 'portality:items_input'
+        },
+        {
+            output: Item.of('portality:module_fluids'),
+            pattern: ['ABA', 'CDC', 'ABA'],
+            key: {
+                A: 'portality:frame',
+                B: 'immersiveengineering:fluid_sorter',
+                C: 'pneumaticcraft:logistics_core',
+                D: 'xnet:wireless_router'
+            },
+            id: 'portality:fluids_input'
+        },
+        {
+            output: Item.of('portality:module_interdimensional'),
+            pattern: ['ABA', 'CDC', 'ABA'],
+            key: {
+                A: 'portality:frame',
+                B: 'occultism:stable_wormhole',
+                C: 'pneumaticcraft:logistics_core',
+                D: 'xnet:wireless_router'
+            },
+            id: 'portality:interdimensional'
         }
     ];
 
