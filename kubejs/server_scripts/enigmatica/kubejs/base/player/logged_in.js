e58e1b50049e10571cff24e5459cc9b94b081720
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-10-22 21:07:54 +0200

    strings -> const

--- kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
@@ -1,13 +1,14 @@
 onEvent('player.logged_in', (event) => {
+    const startingItemsGameStage = 'starting_items';
     randomWaystone = () => {
         const waystones = ['waystones:waystone', 'waystones:mossy_waystone', 'waystones:sandy_waystone'];
         return waystones[Math.floor(Math.random() * waystones.length + 1)];
     };
 
-    if (!event.hasGameStage('starting_items')) {
+    if (!event.hasGameStage(startingItemsGameStage)) {
         event.player.give(Item.of('ftbquests:book'));
         event.player.give(Item.of(randomWaystone()));
 
-        event.addGameStage('starting_items');
+        event.addGameStage(startingItemsGameStage);
     }
 });
