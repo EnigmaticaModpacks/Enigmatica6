8bd4bc157dfecbe04667f70af00429f50514a5a3
James G. E. McIlhargey <jmcilhargey@bloomberg.net>
2021-11-10 10:04:49 -0500

    Progress

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipes/remove.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipes/remove.js
@@ -26,6 +26,9 @@ onEvent('recipes', (event) => {
         'botania:mana_infusion/mana_diamond_block',
         'byg:compat/create/black_sand_from_crushing',
 
+        'computercraft:computer_advanced_upgrade',
+        'computercraft:turtle_advanced_upgrade',
+
         /compactmachines:machine_/,
         /create:pressing\/\w*_ingot/,
 
