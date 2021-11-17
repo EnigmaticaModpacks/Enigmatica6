d295698c9c1f1f47c1bf3b5ff273a0825b7fd654
MuteTiefling <mutetiefling@gmail.com>
2021-11-16 23:20:15 -0500

    Update multiblock_gamestages.js

--- kubejs/startup_scripts/multiblock_gamestages.js
+++ kubejs/startup_scripts/multiblock_gamestages.js
@@ -3,14 +3,11 @@ let requiredGameStage = new Map([
     ['immersiveengineering:multiblocks/excavator', 'medium_machinery_schematics'],
     ['immersiveengineering:multiblocks/bucket_wheel', 'medium_machinery_schematics'],
     ['immersiveengineering:multiblocks/excavator_full', 'medium_machinery_schematics'],
-
     ['immersiveengineering:multiblocks/mixer', 'medium_machinery_schematics'],
-
     ['immersiveengineering:multiblocks/sawmill', 'medium_machinery_schematics'],
     ['immersiveengineering:multiblocks/squeezer', 'medium_machinery_schematics'],
     ['immersiveengineering:multiblocks/lightning_rod', 'medium_machinery_schematics'],
     ['immersiveengineering:multiblocks/metal_press', 'medium_machinery_schematics'],
-    ['immersivepetroleum:multiblocks/hydrotreater', 'medium_machinery_schematics'],
 
     ['immersiveengineering:multiblocks/auto_workbench', 'heavy_machinery_schematics'],
     ['immersiveengineering:multiblocks/arcfurnace', 'heavy_machinery_schematics'],
@@ -20,7 +17,8 @@ let requiredGameStage = new Map([
     ['immersiveengineering:multiblocks/refinery', 'heavy_machinery_schematics'],
     ['immersiveengineering:multiblocks/fermenter', 'heavy_machinery_schematics'],
     ['immersivepetroleum:multiblocks/cokerunit', 'heavy_machinery_schematics'],
-    ['immersivepetroleum:multiblocks/distillationtower', 'heavy_machinery_schematics']
+    ['immersivepetroleum:multiblocks/distillationtower', 'heavy_machinery_schematics'],
+    ['immersivepetroleum:multiblocks/hydrotreater', 'medium_machinery_schematics']
 ]);
 
 onForgeEvent('blusunrize.immersiveengineering.api.multiblocks.MultiblockHandler$MultiblockFormEvent', (event) => {
