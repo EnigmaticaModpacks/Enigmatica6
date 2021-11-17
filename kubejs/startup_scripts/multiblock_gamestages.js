8627abc54e718c16eef443bad21377bbfee1b80f
MuteTiefling <mutetiefling@gmail.com>
2021-11-17 09:15:16 -0500

    Update multiblock_gamestages.js

--- kubejs/startup_scripts/multiblock_gamestages.js
+++ kubejs/startup_scripts/multiblock_gamestages.js
@@ -1,30 +1,30 @@
-let requiredGameStage = new Map([
-    ['immersivepetroleum:multiblocks/pumpjack', 'medium_machinery_schematics'],
-    ['immersiveengineering:multiblocks/excavator', 'medium_machinery_schematics'],
-    ['immersiveengineering:multiblocks/bucket_wheel', 'medium_machinery_schematics'],
-    ['immersiveengineering:multiblocks/excavator_full', 'medium_machinery_schematics'],
-    ['immersiveengineering:multiblocks/mixer', 'medium_machinery_schematics'],
-    ['immersiveengineering:multiblocks/sawmill', 'medium_machinery_schematics'],
-    ['immersiveengineering:multiblocks/squeezer', 'medium_machinery_schematics'],
-    ['immersiveengineering:multiblocks/lightning_rod', 'medium_machinery_schematics'],
-    ['immersiveengineering:multiblocks/metal_press', 'medium_machinery_schematics'],
-
-    ['immersiveengineering:multiblocks/auto_workbench', 'heavy_machinery_schematics'],
-    ['immersiveengineering:multiblocks/arcfurnace', 'heavy_machinery_schematics'],
-    ['immersiveengineering:multiblocks/assembler', 'heavy_machinery_schematics'],
-    ['immersiveengineering:multiblocks/crusher', 'heavy_machinery_schematics'],
-    ['immersiveengineering:multiblocks/diesel_generator', 'heavy_machinery_schematics'],
-    ['immersiveengineering:multiblocks/refinery', 'heavy_machinery_schematics'],
-    ['immersiveengineering:multiblocks/fermenter', 'heavy_machinery_schematics'],
-    ['immersivepetroleum:multiblocks/cokerunit', 'heavy_machinery_schematics'],
-    ['immersivepetroleum:multiblocks/distillationtower', 'heavy_machinery_schematics'],
-    ['immersivepetroleum:multiblocks/hydrotreater', 'heavy_machinery_schematics']
-]);
-
 onForgeEvent('blusunrize.immersiveengineering.api.multiblocks.MultiblockHandler$MultiblockFormEvent', (event) => {
     if (global.isExpertMode == false) {
         return;
     }
+
+    let requiredGameStage = new Map([
+        ['immersivepetroleum:multiblocks/pumpjack', 'medium_machinery_schematics'],
+        ['immersiveengineering:multiblocks/excavator', 'medium_machinery_schematics'],
+        ['immersiveengineering:multiblocks/bucket_wheel', 'medium_machinery_schematics'],
+        ['immersiveengineering:multiblocks/excavator_full', 'medium_machinery_schematics'],
+        ['immersiveengineering:multiblocks/mixer', 'medium_machinery_schematics'],
+        ['immersiveengineering:multiblocks/sawmill', 'medium_machinery_schematics'],
+        ['immersiveengineering:multiblocks/squeezer', 'medium_machinery_schematics'],
+        ['immersiveengineering:multiblocks/lightning_rod', 'medium_machinery_schematics'],
+        ['immersiveengineering:multiblocks/metal_press', 'medium_machinery_schematics'],
+
+        ['immersiveengineering:multiblocks/auto_workbench', 'heavy_machinery_schematics'],
+        ['immersiveengineering:multiblocks/arcfurnace', 'heavy_machinery_schematics'],
+        ['immersiveengineering:multiblocks/assembler', 'heavy_machinery_schematics'],
+        ['immersiveengineering:multiblocks/crusher', 'heavy_machinery_schematics'],
+        ['immersiveengineering:multiblocks/diesel_generator', 'heavy_machinery_schematics'],
+        ['immersiveengineering:multiblocks/refinery', 'heavy_machinery_schematics'],
+        ['immersiveengineering:multiblocks/fermenter', 'heavy_machinery_schematics'],
+        ['immersivepetroleum:multiblocks/cokerunit', 'heavy_machinery_schematics'],
+        ['immersivepetroleum:multiblocks/distillationtower', 'heavy_machinery_schematics'],
+        ['immersivepetroleum:multiblocks/hydrotreater', 'heavy_machinery_schematics']
+    ]);
 
     const name = '' + event.multiblock.getUniqueName();
     if (requiredGameStage.has(name)) {
