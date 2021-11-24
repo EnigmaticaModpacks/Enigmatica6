EnigmaticaModpacks/Enigmatica6

commit b36497ac995e064870e186478999eca828b1fe66

set all recipes using `#minecraft:stone_tool_materials` to use the quark variety instead

kubejs/server_scripts/enigmatica/kubejs/base/recipes/replace_input.js
@@ -6,6 +6,7 @@ onEvent('recipes', (event) => {
             replaceWith: '#forge:chests/wooden'
         }
     ];
+    event.replaceInput({}, '#minecraft:stone_tool_materials', '#quark:stone_tool_materials');
     event.replaceInput({}, 'thermal:sawdust', 'emendatusenigmatica:wood_dust');
     event.replaceInput({}, 'architects_palette:withered_bone', '#forge:bones/wither');
     event.replaceInput({}, 'refinedstorage:silicon', '#forge:silicon');

commit a6064d6406bfa90f7a212700c647b2ac77a35830

fixed super weird recipes caused by commit b36497ac995e064870e186478999eca828b1fe66

kubejs/server_scripts/enigmatica/kubejs/base/recipes/replace_input.js
@@ -4,9 +4,13 @@ onEvent('recipes', (event) => {
             replaceTarget: { id: 'entangled:block' },
             toReplace: 'minecraft:chest',
             replaceWith: '#forge:chests/wooden'
+        },
+        {
+            replaceTarget: { id: 'constructionwand:stone_wand' },
+            toReplace: '#minecraft:stone_tool_materials',
+            replaceWith: '#quark:stone_tool_materials'
         }
     ];
-    event.replaceInput({}, '#minecraft:stone_tool_materials', '#quark:stone_tool_materials');
     event.replaceInput({}, 'thermal:sawdust', 'emendatusenigmatica:wood_dust');
     event.replaceInput({}, 'architects_palette:withered_bone', '#forge:bones/wither');
     event.replaceInput({}, 'refinedstorage:silicon', '#forge:silicon');