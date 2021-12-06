911f39446c7315276ba9be86bef123376a3d58ec
MuteTiefling <mutetiefling@gmail.com>
2021-12-06 14:20:15 -0500

    material cleanup
    Hide more unused EE materials
    Re-instate tinkers hardened glass for all modes

--- kubejs/server_scripts/enigmatica/kubejs/normal/recipetypes/tconstruct/melting.js
+++ kubejs/server_scripts/enigmatica/kubejs/normal/recipetypes/tconstruct/melting.js
@@ -2,8 +2,9 @@ onEvent('recipes', (event) => {
     if (global.isNormalMode == false) {
         return;
     }
-    const id_prefix = 'enigmatica:base/tconstruct/melting/';
+    const id_prefix = 'enigmatica:normal/tconstruct/melting/';
     const recipes = [
+        /*
         {
             ingredient: { item: 'thermal:obsidian_glass' },
             result: { fluid: 'kubejs:molten_hardened_glass', amount: 1000 },
@@ -11,27 +12,7 @@ onEvent('recipes', (event) => {
             time: 64,
             id: `${id_prefix}molten_hardened_glass`
         },
-        {
-            ingredient: { item: 'thermal:signalum_glass' },
-            result: { fluid: 'kubejs:molten_signalum_glass', amount: 1000 },
-            temperature: 1400,
-            time: 64,
-            id: `${id_prefix}molten_signalum_glass`
-        },
-        {
-            ingredient: { item: 'thermal:lumium_glass' },
-            result: { fluid: 'kubejs:molten_lumium_glass', amount: 1000 },
-            temperature: 1400,
-            time: 64,
-            id: `${id_prefix}molten_lumium_glass`
-        },
-        {
-            ingredient: { item: 'thermal:enderium_glass' },
-            result: { fluid: 'kubejs:molten_enderium_glass', amount: 1000 },
-            temperature: 1400,
-            time: 64,
-            id: `${id_prefix}molten_enderium_glass`
-        }
+        */
     ];
 
     recipes.forEach((recipe) => {
