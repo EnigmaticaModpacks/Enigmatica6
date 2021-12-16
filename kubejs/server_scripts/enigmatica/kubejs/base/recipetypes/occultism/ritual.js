6b8c1ae95122bedc431c8638233cbed245c45763
MuteTiefling <mutetiefling@gmail.com>
2021-12-16 08:37:32 -0500

    Re-instate decay removal for spirits

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/occultism/ritual.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/occultism/ritual.js
@@ -1,6 +1,98 @@
 onEvent('recipes', (event) => {
     const id_prefix = 'enigmatica:base/occultism/ritual/';
     const recipes = [
+        {
+            ritual_type: 'occultism:summon_spirit_with_job',
+            activation_item: 'occultism:book_of_binding_bound_afrit',
+            pentacle_id: 'occultism:summon_afrit',
+            duration: 60,
+            spirit_max_age: 40,
+            spirit_job_type: 'occultism:rain_weather',
+            entity_to_sacrifice: { tag: 'forge:cows', display_name: 'ritual.occultism.sacrifice.cows' },
+            entity_to_summon: 'occultism:afrit',
+            ritual_dummy: 'occultism:ritual_dummy/summon_afrit_rain_weather',
+            ingredients: ['#forge:sand', '#forge:gems/diamond', 'minecraft:cactus', 'minecraft:dead_bush'],
+            result: 'occultism:jei_dummy/none'
+        },
+        {
+            ritual_type: 'occultism:summon_spirit_with_job',
+            activation_item: 'occultism:book_of_binding_bound_afrit',
+            pentacle_id: 'occultism:summon_afrit',
+            duration: 60,
+            spirit_max_age: 40,
+            spirit_job_type: 'occultism:thunder_weather',
+            entity_to_sacrifice: { tag: 'forge:cows', display_name: 'ritual.occultism.sacrifice.cows' },
+            entity_to_summon: 'occultism:afrit',
+            ritual_dummy: 'occultism:ritual_dummy/summon_afrit_thunder_weather',
+            ingredients: ['#forge:bones', '#forge:gunpowder', '#forge:gunpowder', 'minecraft:ghast_tear'],
+            result: 'occultism:jei_dummy/none'
+        },
+        {
+            ritual_type: 'occultism:summon_spirit_with_job',
+            activation_item: 'occultism:book_of_binding_bound_djinni',
+            pentacle_id: 'occultism:summon_djinni',
+            duration: 60,
+            spirit_max_age: 40,
+            spirit_job_type: 'occultism:clear_weather',
+            entity_to_summon: 'occultism:djinni',
+            ritual_dummy: 'occultism:ritual_dummy/summon_djinni_clear_weather',
+            ingredients: ['#forge:crops/beetroot', '#forge:crops/carrot', '#forge:crops/potato', '#forge:crops/wheat'],
+            result: 'occultism:jei_dummy/none'
+        },
+        {
+            ritual_type: 'occultism:summon_spirit_with_job',
+            activation_item: 'occultism:book_of_binding_bound_djinni',
+            pentacle_id: 'occultism:summon_djinni',
+            duration: 60,
+            spirit_max_age: 40,
+            spirit_job_type: 'occultism:day_time',
+            entity_to_summon: 'occultism:djinni',
+            ritual_dummy: 'occultism:ritual_dummy/summon_djinni_day_time',
+            ingredients: ['minecraft:torch', '#minecraft:saplings', 'minecraft:wheat', '#forge:dyes/yellow'],
+            result: 'occultism:jei_dummy/none'
+        },
+        {
+            ritual_type: 'occultism:summon_spirit_with_job',
+            activation_item: 'occultism:book_of_binding_bound_djinni',
+            pentacle_id: 'occultism:summon_djinni',
+            duration: 60,
+            spirit_max_age: 40,
+            spirit_job_type: 'occultism:night_time',
+            entity_to_summon: 'occultism:djinni',
+            ritual_dummy: 'occultism:ritual_dummy/summon_djinni_night_time',
+            ingredients: ['#minecraft:beds', 'minecraft:rotten_flesh', '#forge:bones', '#forge:dyes/black'],
+            result: 'occultism:jei_dummy/none'
+        },
+        {
+            ritual_type: 'occultism:summon_spirit_with_job',
+            activation_item: 'occultism:book_of_binding_bound_foliot',
+            pentacle_id: 'occultism:summon_foliot',
+            duration: 30,
+            spirit_max_age: -1,
+            spirit_job_type: 'occultism:trade_otherstone_t1',
+            entity_to_summon: 'occultism:foliot',
+            ritual_dummy: 'occultism:ritual_dummy/summon_foliot_otherstone_trader',
+            ingredients: ['minecraft:stone', 'minecraft:granite', 'minecraft:diorite', 'minecraft:andesite'],
+            result: 'occultism:jei_dummy/none'
+        },
+        {
+            ritual_type: 'occultism:summon_spirit_with_job',
+            activation_item: 'occultism:book_of_binding_bound_foliot',
+            pentacle_id: 'occultism:summon_foliot',
+            duration: 30,
+            spirit_max_age: -1,
+            spirit_job_type: 'occultism:trade_otherworld_saplings_t1',
+            entity_to_summon: 'occultism:foliot',
+            ritual_dummy: 'occultism:ritual_dummy/summon_foliot_sapling_trader',
+            ingredients: [
+                'minecraft:oak_sapling',
+                'minecraft:birch_sapling',
+                'minecraft:spruce_sapling',
+                'minecraft:jungle_sapling'
+            ],
+            result: 'occultism:jei_dummy/none'
+        },
+
         /// Custom Rituals
         {
             ritual_type: 'occultism:summon',
@@ -31,9 +123,6 @@ onEvent('recipes', (event) => {
             result: 'occultism:jei_dummy/none',
             id: `${id_prefix}summon_otherwordly_bee`
         },
-
-        // Item.of('resourcefulbees:bee_jar', '{Entity:"resourcefulbees:otherworldly_bee",display:{Name:\'{"italic":false,"extra":[{"text":" - "},{"translate":"entity.resourcefulbees.otherworldly_bee"}],"translate":"item.resourcefulbees.bee_jar_filled"}\'},Color:"#80251f"}')
-
         {
             ritual_type: 'occultism:craft',
             activation_item: 'resourcefulbees:illuminating_honey_bottle',
@@ -58,6 +147,27 @@ onEvent('recipes', (event) => {
             id: `${id_prefix}craft_otherworldly_honey_bottle`
         }
     ];
+
+    /*
+        Ingredient Display in JEI
+            ingredients: [
+                 '12 o'clock' ,
+                 '3 o'clock' ,
+                 '6 o'clock' ,
+                 '9 o'clock' ,
+
+                 '1 o'clock',
+                 '2 o'clock' ,
+                 '7 o'clock' ,
+                 '8 o'clock' ,
+
+                 '11 o'clock' ,
+                 '4 o'clock' ,
+                 '5 o'clock' ,
+                 '10 o'clock',
+            ],
+    */
+
     recipes.forEach((recipe) => {
         recipe.type = 'occultism:ritual';
 
--- kubejs/server_scripts/enigmatica/kubejs/normal/recipetypes/occultism/ritual.js
+++ kubejs/server_scripts/enigmatica/kubejs/normal/recipetypes/occultism/ritual.js
@@ -1,1 +1,55 @@
-//TODO: Remove in 0.6.0
+onEvent('recipes', (event) => {
+    if (global.isNormalMode == false) {
+        return;
+    }
+
+    const id_prefix = 'enigmatica:normal/occultism/ritual/';
+    recipes = [
+        {
+            ritual_type: 'occultism:summon_spirit_with_job',
+            activation_item: 'occultism:book_of_binding_bound_foliot',
+            pentacle_id: 'occultism:summon_foliot',
+            duration: 60,
+            spirit_max_age: -1,
+            spirit_job_type: 'occultism:crush_tier1',
+            entity_to_summon: 'occultism:foliot',
+            ritual_dummy: 'occultism:ritual_dummy/summon_foliot_crusher',
+            ingredients: ['#forge:ores/iron', '#forge:ores/gold', '#forge:ores/copper', '#forge:ores/silver'],
+            result: 'occultism:jei_dummy/none'
+        }
+    ];
+
+    /*
+        Ingredient Display in JEI
+            ingredients: [
+                 '12 o'clock' ,
+                 '3 o'clock' ,
+                 '6 o'clock' ,
+                 '9 o'clock' ,
+
+                 '1 o'clock',
+                 '2 o'clock' ,
+                 '7 o'clock' ,
+                 '8 o'clock' ,
+
+                 '11 o'clock' ,
+                 '4 o'clock' ,
+                 '5 o'clock' ,
+                 '10 o'clock',
+            ],
+    */
+
+    recipes.forEach((recipe) => {
+        recipe.type = 'occultism:ritual';
+
+        recipe.activation_item = Ingredient.of(recipe.activation_item).toJson();
+        if (recipe.item_to_use) {
+            recipe.item_to_use = Ingredient.of(recipe.item_to_use).toJson();
+        }
+        recipe.ritual_dummy = Ingredient.of(recipe.ritual_dummy).toJson();
+        recipe.ingredients = recipe.ingredients.map((input) => Ingredient.of(input).toJson());
+        recipe.result = Item.of(recipe.result).toJson();
+
+        event.custom(recipe).id(recipe.id);
+    });
+});
