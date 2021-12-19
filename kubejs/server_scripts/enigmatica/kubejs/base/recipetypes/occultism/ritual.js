d956e34374b3dc4f8e18b4037ff53e0fa1c6e0c1
MuteTiefling <mutetiefling@gmail.com>
2021-12-18 22:37:52 -0500

    Fix broken ritual script for base. Add missing crusher to remove decay again in normal.

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/occultism/ritual.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/occultism/ritual.js
@@ -12,7 +12,8 @@ onEvent('recipes', (event) => {
             entity_to_summon: 'occultism:afrit',
             ritual_dummy: 'occultism:ritual_dummy/summon_afrit_rain_weather',
             ingredients: ['#forge:sand', '#forge:gems/diamond', 'minecraft:cactus', 'minecraft:dead_bush'],
-            result: 'occultism:jei_dummy/none'
+            result: 'occultism:jei_dummy/none',
+            id: 'occultism:ritual/summon_afrit_rain_weather'
         },
         {
             ritual_type: 'occultism:summon_spirit_with_job',
@@ -25,7 +26,8 @@ onEvent('recipes', (event) => {
             entity_to_summon: 'occultism:afrit',
             ritual_dummy: 'occultism:ritual_dummy/summon_afrit_thunder_weather',
             ingredients: ['#forge:bones', '#forge:gunpowder', '#forge:gunpowder', 'minecraft:ghast_tear'],
-            result: 'occultism:jei_dummy/none'
+            result: 'occultism:jei_dummy/none',
+            id: 'occultism:ritual/summon_afrit_thunder_weather'
         },
         {
             ritual_type: 'occultism:summon_spirit_with_job',
@@ -37,7 +39,8 @@ onEvent('recipes', (event) => {
             entity_to_summon: 'occultism:djinni',
             ritual_dummy: 'occultism:ritual_dummy/summon_djinni_clear_weather',
             ingredients: ['#forge:crops/beetroot', '#forge:crops/carrot', '#forge:crops/potato', '#forge:crops/wheat'],
-            result: 'occultism:jei_dummy/none'
+            result: 'occultism:jei_dummy/none',
+            id: 'occultism:ritual/summon_djinni_clear_weather'
         },
         {
             ritual_type: 'occultism:summon_spirit_with_job',
@@ -49,7 +52,8 @@ onEvent('recipes', (event) => {
             entity_to_summon: 'occultism:djinni',
             ritual_dummy: 'occultism:ritual_dummy/summon_djinni_day_time',
             ingredients: ['minecraft:torch', '#minecraft:saplings', 'minecraft:wheat', '#forge:dyes/yellow'],
-            result: 'occultism:jei_dummy/none'
+            result: 'occultism:jei_dummy/none',
+            id: 'occultism:ritual/summon_djinni_day_time'
         },
         {
             ritual_type: 'occultism:summon_spirit_with_job',
@@ -61,7 +65,8 @@ onEvent('recipes', (event) => {
             entity_to_summon: 'occultism:djinni',
             ritual_dummy: 'occultism:ritual_dummy/summon_djinni_night_time',
             ingredients: ['#minecraft:beds', 'minecraft:rotten_flesh', '#forge:bones', '#forge:dyes/black'],
-            result: 'occultism:jei_dummy/none'
+            result: 'occultism:jei_dummy/none',
+            id: 'occultism:ritual/summon_djinni_night_time'
         },
         {
             ritual_type: 'occultism:summon_spirit_with_job',
@@ -73,7 +78,8 @@ onEvent('recipes', (event) => {
             entity_to_summon: 'occultism:foliot',
             ritual_dummy: 'occultism:ritual_dummy/summon_foliot_otherstone_trader',
             ingredients: ['minecraft:stone', 'minecraft:granite', 'minecraft:diorite', 'minecraft:andesite'],
-            result: 'occultism:jei_dummy/none'
+            result: 'occultism:jei_dummy/none',
+            id: 'occultism:ritual/summon_foliot_otherstone_trader'
         },
         {
             ritual_type: 'occultism:summon_spirit_with_job',
@@ -90,7 +96,21 @@ onEvent('recipes', (event) => {
                 'minecraft:spruce_sapling',
                 'minecraft:jungle_sapling'
             ],
-            result: 'occultism:jei_dummy/none'
+            result: 'occultism:jei_dummy/none',
+            id: 'occultism:ritual/summon_foliot_sapling_trader'
+        },
+        {
+            ritual_type: 'occultism:summon_spirit_with_job',
+            activation_item: 'occultism:book_of_binding_bound_foliot',
+            pentacle_id: 'occultism:summon_foliot',
+            duration: 60,
+            spirit_max_age: -1,
+            spirit_job_type: 'occultism:crush_tier1',
+            entity_to_summon: 'occultism:foliot',
+            ritual_dummy: 'occultism:ritual_dummy/summon_foliot_crusher',
+            ingredients: ['#forge:ores/iron', '#forge:ores/gold', '#forge:ores/copper', '#forge:ores/silver'],
+            result: 'occultism:jei_dummy/none',
+            id: 'occultism:ritual/summon_foliot_crusher'
         },
 
         /// Custom Rituals
@@ -172,9 +192,9 @@ onEvent('recipes', (event) => {
         recipe.type = 'occultism:ritual';
 
         recipe.activation_item = Ingredient.of(recipe.activation_item).toJson();
-        if (recipe.item_to_use) {
+        /*if (recipe.item_to_use) {
             recipe.item_to_use = Ingredient.of(recipe.item_to_use).toJson();
-        }
+        }*/
         recipe.ritual_dummy = Ingredient.of(recipe.ritual_dummy).toJson();
         recipe.ingredients = recipe.ingredients.map((input) => Ingredient.of(input).toJson());
         recipe.result = Item.of(recipe.result).toJson();
