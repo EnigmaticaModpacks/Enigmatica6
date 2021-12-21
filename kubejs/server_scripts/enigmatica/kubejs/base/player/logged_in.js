de297ad6aa18de657a6b6a9436631fd3e407ccfe
Keegan <landreth.keegan@gmail.com>
2021-12-20 18:32:19 -0700

    Fix players not getting waystones
    I wondered why I didn't get a waystone and neither do many players in my server. The math in this function gives an array index in the range of [1,3]. 3 is out of bounds so returns nothing. Corrected to be in the range [0,2] so it's within the array bounds.
    
    random() -> [0,1) -> *3 -> [0,3) -> floor() -> [0,2]
    
    Double checked and this gives a uniform distribution too.

--- kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/player/logged_in.js
@@ -2,7 +2,7 @@ onEvent('player.logged_in', (event) => {
     const startingItemsGameStage = 'starting_items';
     randomWaystone = () => {
         const waystones = ['waystones:waystone', 'waystones:mossy_waystone', 'waystones:sandy_waystone'];
-        return waystones[Math.floor(Math.random() * waystones.length + 1)];
+        return waystones[Math.floor(Math.random() * waystones.length)];
     };
 
     setMode = (player) => {
