0fc782ef16d191546cce7b5ac18f6cad64dd5020
MuteTiefling <mutetiefling@gmail.com>
2022-01-02 23:45:17 -0500

    update issue tracker link

--- kubejs/client_scripts/item_modifiers/jei_descriptions.js
+++ kubejs/client_scripts/item_modifiers/jei_descriptions.js
@@ -457,7 +457,7 @@ onEvent('jei.information', (event) => {
     disabledItems.forEach((item) => {
         event.add(
             item,
-            "This item has been disabled, if you managed to obtain it please report it on Enigmatica 6's issue tracker: https://github.com/NillerMedDild/Enigmatica6/issues"
+            "This item has been disabled, if you managed to obtain it please report it on Enigmatica 6's issue tracker: https://github.com/EnigmaticaModpacks/Enigmatica6/issues"
         );
     });
 
