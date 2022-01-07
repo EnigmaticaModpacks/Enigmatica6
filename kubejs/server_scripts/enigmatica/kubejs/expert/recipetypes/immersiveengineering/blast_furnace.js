4d7dcd27f4342bdcd36615314745e54539a23362
MuteTiefling <mutetiefling@gmail.com>
2022-01-07 17:41:35 -0500

    extra plastic recipes

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/immersiveengineering/blast_furnace.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/immersiveengineering/blast_furnace.js
@@ -2,23 +2,23 @@ onEvent('recipes', (event) => {
     if (global.isExpertMode == false) {
         return;
     }
+    const id_prefix = 'enigmatica:expert/immersiveengineering/blast_furnace';
     const recipes = [
         {
             output: 'kubejs:smoldering_lapis_lazuli_compound',
             input: 'kubejs:coarse_lapis_lazuli_compound',
-            slag: '#forge:slag'
+            slag: 'thermal:slag',
+            id: `${id_prefix}smoldering_lapis_lazuli_compound`
         },
         {
-            output: 'industrialforegoing:plastic',
-            input: 'industrialforegoing:dryrubber',
-            slag: '#forge:slag',
+            output: Item.of('4x industrialforegoing:plastic'),
+            input: Item.of('4x industrialforegoing:dryrubber'),
+            slag: Item.of('4x thermal:slag'),
             id: 'industrialforegoing:plastic'
         }
     ];
 
     recipes.forEach((recipe) => {
-        recipe.id
-            ? event.recipes.immersiveengineering.blast_furnace(recipe.output, recipe.input, recipe.slag).id(recipe.id)
-            : event.recipes.immersiveengineering.blast_furnace(recipe.output, recipe.input, recipe.slag);
+        event.recipes.immersiveengineering.blast_furnace(recipe.output, recipe.input, recipe.slag).id(recipe.id);
     });
 });
