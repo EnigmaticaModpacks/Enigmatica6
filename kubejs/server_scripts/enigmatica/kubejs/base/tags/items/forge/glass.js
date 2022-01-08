4f112e290474103c1a1dde1b6dc11e7511561dfb
MuteTiefling <mutetiefling@gmail.com>
2022-01-07 23:43:42 -0500

    remove costly tinted glass from chisel

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/glass.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/glass.js
@@ -6,7 +6,6 @@ onEvent('item.tags', (event) => {
                 `connectedglass:borderless_glass_${color}`,
                 `connectedglass:clear_glass_${color}`,
                 `connectedglass:scratched_glass_${color}`,
-                `connectedglass:tinted_borderless_glass_${color}`,
                 `quark:${color}_framed_glass`
             ]);
     });
