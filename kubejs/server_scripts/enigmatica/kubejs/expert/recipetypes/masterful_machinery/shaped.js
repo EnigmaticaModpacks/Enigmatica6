e63b3f3b2257064471f340ebe62a887445c5b733
MuteTiefling <mutetiefling@gmail.com>
2022-01-12 23:06:50 -0500

    blocks

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/masterful_machinery/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/masterful_machinery/shaped.js
@@ -59,6 +59,49 @@ onEvent('recipes', (event) => {
                 D: 'xnet:advanced_connector_green'
             },
             id: `${id_prefix}auto_iridescent_altar_energy_port_energy_input`
+        },
+        {
+            output: 'masterfulmachinery:advanced_assembly_table_high_pressure_port_pncr_pressure_input',
+            pattern: ['BAB', 'ACA', 'BAB'],
+            key: {
+                A: '#forge:alloys/elite',
+                B: 'pneumaticcraft:advanced_pressure_tube',
+                C: '#industrialforegoing:machine_frame/simple'
+            },
+            id: `${id_prefix}advanced_assembly_table_high_pressure_port_pncr_pressure_input`
+        },
+        {
+            output: 'masterfulmachinery:advanced_assembly_table_controller',
+            pattern: ['BAB', 'DCD', 'BDB'],
+            key: {
+                A: 'rftoolsbase:tablet',
+                B: 'pneumaticcraft:advanced_pressure_tube',
+                C: '#industrialforegoing:machine_frame/supreme',
+                D: '#forge:alloys/elite'
+            },
+            id: `${id_prefix}advanced_assembly_table_controller`
+        },
+        {
+            output: 'masterfulmachinery:advanced_assembly_table_item_port_items_input',
+            pattern: ['BAB', 'ACA', 'BDB'],
+            key: {
+                A: '#forge:alloys/elite',
+                B: 'pneumaticcraft:advanced_pressure_tube',
+                C: 'pneumaticcraft:pressure_chamber_valve',
+                D: 'thermal:lime_rockwool'
+            },
+            id: `${id_prefix}advanced_assembly_table_item_port_items_input`
+        },
+        {
+            output: 'masterfulmachinery:advanced_assembly_table_item_port_items_output',
+            pattern: ['BAB', 'ACA', 'BDB'],
+            key: {
+                A: '#forge:alloys/elite',
+                B: 'pneumaticcraft:advanced_pressure_tube',
+                C: 'pneumaticcraft:pressure_chamber_valve',
+                D: 'thermal:red_rockwool'
+            },
+            id: `${id_prefix}advanced_assembly_table_item_port_items_output`
         }
     ];
 
