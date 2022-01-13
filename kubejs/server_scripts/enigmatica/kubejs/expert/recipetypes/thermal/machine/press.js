484762a56b498ff11b4d8c82cf6aebc2d976460d
MuteTiefling <mutetiefling@gmail.com>
2022-01-13 17:43:47 -0500

    Reduce power cost of processor pressing, speeding them up

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/thermal/machine/press.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/thermal/machine/press.js
@@ -19,25 +19,25 @@ onEvent('recipes', (event) => {
         {
             inputs: ['refinedstorage:raw_basic_processor', Ingredient.of('#thermal:crafting/dies/coin')],
             output: 'refinedstorage:basic_processor',
-            energy: 9000,
+            energy: 3000,
             id: 'refinedstorage:basic_processor'
         },
         {
             inputs: ['refinedstorage:raw_improved_processor', Ingredient.of('#thermal:crafting/dies/coin')],
             output: 'refinedstorage:improved_processor',
-            energy: 18000,
+            energy: 3000 * 2,
             id: 'refinedstorage:improved_processor'
         },
         {
             inputs: ['refinedstorage:raw_advanced_processor', Ingredient.of('#thermal:crafting/dies/coin')],
             output: 'refinedstorage:advanced_processor',
-            energy: 27000,
+            energy: 3000 * 3,
             id: 'refinedstorage:advanced_processor'
         },
         {
             inputs: ['extrastorage:raw_neural_processor', Ingredient.of('#thermal:crafting/dies/coin')],
             output: 'extrastorage:neural_processor',
-            energy: 36000,
+            energy: 3000 * 4,
             id: 'extrastorage:neural_processor'
         },
         {
