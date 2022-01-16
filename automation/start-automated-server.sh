cf8002843572e748e3d0a1cc00fbb9a1325c7b4e
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-01-16 16:26:53 +0100

    sorry @Darkere! :sweat_smile: somehow it worked when I tested it. Wrong `settings.cfg` perhaps

--- automation/start-automated-server.sh
+++ automation/start-automated-server.sh
@@ -196,7 +196,6 @@ echo "DEBUG: MAX_RAM=$MAX_RAM" >>serverstart.log 2>&1
 echo "DEBUG: JAVA_ARGS=$JAVA_ARGS" >>serverstart.log 2>&1
 echo "DEBUG: CRASH_COUNT=$CRASH_COUNT" >>serverstart.log 2>&1
 echo "DEBUG: RUN_FROM_BAD_FOLDER=$RUN_FROM_BAD_FOLDER" >>serverstart.log 2>&1
-echo "DEBUG: IGNORE_OFFLINE=$IGNORE_OFFLINE" >>serverstart.log 2>&1
 echo "DEBUG: MCVER=$MCVER" >>serverstart.log 2>&1
 echo "DEBUG: FORGEVER=$FORGEVER" >>serverstart.log 2>&1
 echo "DEBUG: FORGEURL=$FORGEURL" >>serverstart.log 2>&1
