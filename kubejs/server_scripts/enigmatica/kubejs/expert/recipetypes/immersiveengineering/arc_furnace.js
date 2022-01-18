8798f827d0e34f9f2c491fc4148e6347456ca233
MuteTiefling <mutetiefling@gmail.com>
2022-01-17 21:50:41 -0500

    [Expert] Buff power output of Modular Routers

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/immersiveengineering/arc_furnace.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/immersiveengineering/arc_furnace.js
@@ -28,11 +28,53 @@ onEvent('recipes', (event) => {
                 secondaries: [['#forge:dusts/coal_petcoke', '#forge:dusts/coal_coke']],
                 outputs: [Item.of('8x industrialforegoing:plastic'), Item.of('8x thermal:slag')],
                 id: `${id_prefix}plastic`
+            },
+            {
+                input1: Item.of(Item.of('powah:energizing_rod_basic').ignoreNBT()),
+                secondaries: ['mekanismgenerators:laser_focus_matrix', Item.of('4x modularrouters:blank_upgrade')],
+                outputs: [Item.of('4x modularrouters:energy_upgrade')],
+                time: 100 * 4,
+                energy: 51200 * 4,
+                id: `${id_prefix}energy_upgrade_from_energizing_rod_basic`
+            },
+            {
+                input1: Item.of(Item.of('powah:energizing_rod_hardened').ignoreNBT()),
+                secondaries: ['mekanismgenerators:laser_focus_matrix', Item.of('10x modularrouters:blank_upgrade')],
+                outputs: [Item.of('10x modularrouters:energy_upgrade')],
+                time: 100 * 10,
+                energy: 51200 * 10,
+                id: `${id_prefix}energy_upgrade_from_energizing_rod_hardened`
+            },
+            {
+                input1: Item.of(Item.of('powah:energizing_rod_blazing').ignoreNBT()),
+                secondaries: ['mekanismgenerators:laser_focus_matrix', Item.of('34x modularrouters:blank_upgrade')],
+                outputs: [Item.of('34x modularrouters:energy_upgrade')],
+                time: 100 * 34,
+                energy: 51200 * 34,
+                id: `${id_prefix}energy_upgrade_from_energizing_rod_blazing`
+            },
+            {
+                input1: Item.of(Item.of('powah:energizing_rod_niotic').ignoreNBT()),
+                secondaries: ['mekanismgenerators:laser_focus_matrix', Item.of('64x modularrouters:blank_upgrade')],
+                outputs: [Item.of('64x modularrouters:energy_upgrade')],
+                time: 100 * 64,
+                energy: 51200 * 64,
+                id: `${id_prefix}energy_upgrade_from_energizing_rod_niotic`
             }
         ]
     };
 
     data.recipes.forEach((recipe) => {
-        event.recipes.immersiveengineering.arc_furnace(recipe.outputs, recipe.input1, recipe.secondaries).id(recipe.id);
+        const re = event.recipes.immersiveengineering
+            .arc_furnace(recipe.outputs, recipe.input1, recipe.secondaries)
+            .id(recipe.id);
+
+        if (recipe.time) {
+            re.time(recipe.time);
+        }
+
+        if (recipe.energy) {
+            re.energy(recipe.energy);
+        }
     });
 });
