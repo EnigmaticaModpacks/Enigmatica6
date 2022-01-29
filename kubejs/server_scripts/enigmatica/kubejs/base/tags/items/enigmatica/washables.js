d50d20c67d04a83e780b3ce9113955a90825a1f1
MuteTiefling <mutetiefling@gmail.com>
2022-01-28 22:14:00 -0500

    Fix Botany Pot washable recipes
    Resolves: #3865

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/enigmatica/washables.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/enigmatica/washables.js
@@ -1,6 +1,4 @@
 onEvent('item.tags', (event) => {
-    event.get('enigmatica:washables/simplebotanypots').add(/botanypots:\w+_botany_pot/);
-    event.get('enigmatica:washables/hopperbotanypots').add(/botanypots:hopper_\w+_botany_pot/);
     event.get('enigmatica:washables/terracotta').add(/minecraft:\w+_terracotta/);
     event
         .get('enigmatica:washables/ceramic')
