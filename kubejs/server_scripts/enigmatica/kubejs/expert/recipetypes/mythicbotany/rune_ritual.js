0c030921e812e8a870fabc1090b2fa2c9ab35595
MuteTiefling <mutetiefling@gmail.com>
2022-02-01 11:43:05 -0500

    MM Controller

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/mythicbotany/rune_ritual.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/mythicbotany/rune_ritual.js
@@ -22,6 +22,7 @@ onEvent('recipes', (event) => {
                 { item: '#botania:runes/fire', pos: { x: 3, z: -1 } },
                 { item: '#botania:runes/fire', pos: { x: -3, z: 1 } }
             ],
+            consume_runes: false,
             mana: 4000000,
             ticks: 200,
             inputs: [
@@ -33,6 +34,91 @@ onEvent('recipes', (event) => {
             outputs: ['mythicbotany:kvasir_blood'],
             special_input: 'mythicbotany:wandering_trader',
             id: 'mythicbotany:mythicbotany_rune_rituals/kvasir_blood'
+        },
+        {
+            group: 'rune_rituals',
+            center: { tag: 'botania:runes/midgard' },
+            runes: [
+                { pos: { x: -4, z: 4 }, item: 'ars_nouveau:ritual_moonfall' },
+                { pos: { x: -3, z: 4 }, item: 'astralsorcery:shifting_star_aevitas' },
+                { pos: { x: -2, z: 4 }, item: 'astralsorcery:resonating_gem' },
+                { pos: { x: -1, z: 4 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: 0, z: 4 }, item: '#botania:runes/asgard' },
+                { pos: { x: 1, z: 4 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: 2, z: 4 }, item: 'astralsorcery:resonating_gem' },
+                { pos: { x: 3, z: 4 }, item: 'astralsorcery:shifting_star_aevitas' },
+                { pos: { x: 4, z: 4 }, item: 'ars_nouveau:ritual_sunrise' },
+
+                { pos: { x: -4, z: 3 }, item: 'astralsorcery:shifting_star_aevitas' },
+                { pos: { x: -3, z: 3 }, item: '#botania:runes/alfheim' },
+                { pos: { x: -2, z: 3 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: -1, z: 3 }, item: 'byg:flowering_palo_verde_leaves' },
+                { pos: { x: 0, z: 3 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: 1, z: 3 }, item: 'byg:flowering_palo_verde_leaves' },
+                { pos: { x: 2, z: 3 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: 3, z: 3 }, item: '#botania:runes/alfheim' },
+                { pos: { x: 4, z: 3 }, item: 'astralsorcery:shifting_star_aevitas' },
+
+                { pos: { x: -4, z: 2 }, item: 'astralsorcery:resonating_gem' },
+                { pos: { x: -3, z: 2 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: -2, z: 2 }, item: 'byg:flowering_palo_verde_leaves' },
+                { pos: { x: -1, z: 2 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: 0, z: 2 }, item: 'botania:livingwood' },
+                { pos: { x: 1, z: 2 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: 2, z: 2 }, item: 'byg:flowering_palo_verde_leaves' },
+                { pos: { x: 3, z: 2 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: 4, z: 2 }, item: 'astralsorcery:resonating_gem' },
+
+                { pos: { x: -4, z: 1 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: -3, z: 1 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: -2, z: 1 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: -1, z: 1 }, item: 'byg:flowering_palo_verde_leaves' },
+                { pos: { x: 0, z: 1 }, item: 'botania:livingwood' },
+                { pos: { x: 1, z: 1 }, item: 'byg:flowering_palo_verde_leaves' },
+                { pos: { x: 2, z: 1 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: 3, z: 1 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: 4, z: 1 }, item: 'minecraft:oak_leaves' },
+
+                { pos: { x: -4, z: 0 }, item: '#botania:runes/muspelheim' },
+                { pos: { x: -3, z: 0 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: -2, z: 0 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: -1, z: 0 }, item: 'minecraft:oak_leaves' },
+                //{ pos: { x: 0, z: 0 }, item: center },
+                { pos: { x: 1, z: 0 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: 2, z: 0 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: 3, z: 0 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: 4, z: 0 }, item: '#botania:runes/niflheim' },
+
+                { pos: { x: -2, z: -1 }, item: 'minecraft:oak_leaves' },
+                { pos: { x: -1, z: -1 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: 0, z: -1 }, item: 'botania:livingwood' },
+                { pos: { x: 1, z: -1 }, item: 'mythicbotany:yggdrasil_branch' },
+                { pos: { x: 2, z: -1 }, item: 'minecraft:oak_leaves' },
+
+                { pos: { x: 0, z: -2 }, item: 'botania:livingwood' },
+
+                { pos: { x: -4, z: -3 }, item: 'quark:root_item' },
+                { pos: { x: -3, z: -3 }, item: '#botania:runes/joetunheim' },
+                { pos: { x: 0, z: -3 }, item: 'botania:livingwood' },
+                { pos: { x: 3, z: -3 }, item: '#botania:runes/nidavellir' },
+                { pos: { x: 4, z: -3 }, item: 'quark:root_item' },
+
+                { pos: { x: -4, z: -4 }, item: 'ars_nouveau:ritual_awakening' },
+                { pos: { x: -3, z: -4 }, item: 'quark:root_item' },
+                { pos: { x: -2, z: -4 }, item: 'naturesaura:generator_limit_remover' },
+                { pos: { x: -1, z: -4 }, item: 'botania:livingwood' },
+                { pos: { x: 0, z: -4 }, item: '#botania:runes/helheim' },
+                { pos: { x: 1, z: -4 }, item: 'botania:livingwood' },
+                { pos: { x: 2, z: -4 }, item: 'naturesaura:generator_limit_remover' },
+                { pos: { x: 3, z: -4 }, item: 'quark:root_item' },
+                { pos: { x: 4, z: -4 }, item: 'ars_nouveau:ritual_awakening' }
+            ],
+            consume_runes: true,
+            mana: 2000000,
+            ticks: 200,
+            inputs: ['naturesaura:ancient_sapling'],
+            outputs: ['masterfulmachinery:enigmatic_tree_of_life_controller'],
+            id: `${id_prefix}enigmatic_tree_of_life_controller`
         }
     ];
 
@@ -44,7 +130,7 @@ onEvent('recipes', (event) => {
                 rune: Item.of(rune.item).toJson(),
                 x: rune.pos.x,
                 z: rune.pos.z,
-                consume: false
+                consume: recipe.consume_runes
             });
         });
 
