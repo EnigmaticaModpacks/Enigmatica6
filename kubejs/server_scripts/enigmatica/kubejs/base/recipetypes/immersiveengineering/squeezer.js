b9ee87b2fb38fef6f90503b100d3fd7d66b751b1
MuteTiefling <mutetiefling@gmail.com>
2022-03-02 17:39:30 -0500

    Add more seeds to squeezer

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/immersiveengineering/squeezer.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/immersiveengineering/squeezer.js
@@ -11,7 +11,15 @@ onEvent('recipes', (event) => {
                 'simplefarming:honeydew_seeds',
                 'simplefarming:kenaf_seeds',
                 'simplefarming:lettuce_seeds',
-                'simplefarming:squash_seeds'
+                'simplefarming:squash_seeds',
+                'supplementaries:flax_seeds',
+                'atum:flax_seeds',
+                'undergarden:gloomgourd_seeds',
+                'sushigocrafting:rice_seeds',
+                'sushigocrafting:cucumber_seeds',
+                'sushigocrafting:soy_seeds',
+                'sushigocrafting:wasabi_seeds',
+                'sushigocrafting:sesame_seeds'
             ],
             value: 20
         },
@@ -23,7 +31,17 @@ onEvent('recipes', (event) => {
                 'simplefarming:pea_seeds',
                 'simplefarming:soybean_seeds',
                 'simplefarming:spinach_seeds',
-                'simplefarming:zucchini_seeds'
+                'simplefarming:zucchini_seeds',
+                'betterendforge:glowing_pillar_seed',
+                'betterendforge:lumecorn_seed',
+                'betterendforge:lanceleaf_seed',
+                'betterendforge:end_lotus_seed',
+                'betterendforge:end_lily_seed',
+                'betterendforge:blue_vine_seed',
+                'betterendforge:amber_root_seed',
+                'betterendforge:bulb_vine_seed',
+                'betterendforge:blossom_berry_seed',
+                'betterendforge:shadow_berry'
             ],
             value: 40
         },
@@ -38,7 +56,8 @@ onEvent('recipes', (event) => {
                 'simplefarming:sweet_potato_seeds',
                 'simplefarming:tomato_seeds',
                 'simplefarming:turnip_seeds',
-                'simplefarming:yam_seeds'
+                'simplefarming:yam_seeds',
+                'occultism:datura_seeds'
             ],
             value: 60
         },
@@ -53,7 +72,8 @@ onEvent('recipes', (event) => {
                 'simplefarming:rice_seeds',
                 'simplefarming:rye_seeds',
                 'simplefarming:sorghum_seeds',
-                'simplefarming:sunflower_seeds'
+                'simplefarming:sunflower_seeds',
+                'atum:emmer_seeds'
             ],
             value: 80
         },
