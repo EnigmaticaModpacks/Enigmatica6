94098239f4be63ea80c32ecd7fd29c8d4cdfaa40
MuteTiefling <mutetiefling@gmail.com>
2022-03-23 16:34:17 -0400

    more kit, lesser soul gems

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/masterful_machinery/wicked_altar.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/masterful_machinery/wicked_altar.js
@@ -5,7 +5,7 @@ onEvent('recipes', (event) => {
     const id_prefix = 'enigmatica:expert/masterful_machinery/wicked_altar/';
     const recipes = [
         {
-            outputs: [{ type: 'masterfulmachinery:items', data: { item: 'eidolon:reaper_scythe', count: 8 } }],
+            outputs: [{ type: 'masterfulmachinery:items', data: { item: 'eidolon:reaper_scythe', count: 1 } }],
             inputs: [
                 { type: 'masterfulmachinery:items', data: { tag: 'forge:ingots/pewter', count: 3 } },
                 { type: 'masterfulmachinery:items', data: { item: 'betterendforge:leather_wrapped_stick', count: 2 } },
@@ -15,11 +15,60 @@ onEvent('recipes', (event) => {
                 {
                     type: 'masterfulmachinery:fluids',
                     perTick: true,
-                    data: { fluid: 'bloodmagic:life_essence', amount: 10 }
+                    data: { fluid: 'bloodmagic:life_essence_fluid', amount: 10 }
                 }
             ],
             ticks: 100,
             id: `${id_prefix}reaper_scythe`
+        },
+        {
+            outputs: [{ type: 'masterfulmachinery:items', data: { item: 'eidolon:cleaving_axe', count: 1 } }],
+            inputs: [
+                { type: 'masterfulmachinery:items', data: { tag: 'forge:ingots/pewter', count: 3 } },
+                { type: 'masterfulmachinery:items', data: { item: 'betterendforge:leather_wrapped_stick', count: 2 } },
+                { type: 'masterfulmachinery:items', data: { item: 'minecraft:prismarine_crystals', count: 2 } },
+                { type: 'masterfulmachinery:items', data: { tag: 'forge:inlays/pewter', count: 1 } },
+                { type: 'masterfulmachinery:items', data: { item: 'atum:anput_godshard', count: 1 } },
+                {
+                    type: 'masterfulmachinery:fluids',
+                    perTick: true,
+                    data: { fluid: 'bloodmagic:life_essence_fluid', amount: 10 }
+                }
+            ],
+            ticks: 100,
+            id: `${id_prefix}cleaving_axe`
+        },
+        {
+            outputs: [{ type: 'masterfulmachinery:items', data: { item: 'eidolon:prestigious_palm', count: 1 } }],
+            inputs: [
+                { type: 'masterfulmachinery:items', data: { item: 'eidolon:wicked_weave', count: 4 } },
+                { type: 'masterfulmachinery:items', data: { item: 'eidolon:ender_calx', count: 2 } },
+                { type: 'masterfulmachinery:items', data: { item: 'eidolon:lesser_soul_gem', count: 1 } },
+                { type: 'masterfulmachinery:items', data: { item: 'bloodmagic:reagentvoid', count: 1 } },
+                { type: 'masterfulmachinery:items', data: { item: 'eidolon:warped_sprouts', count: 1 } },
+                {
+                    type: 'masterfulmachinery:fluids',
+                    perTick: true,
+                    data: { fluid: 'bloodmagic:life_essence_fluid', amount: 10 }
+                }
+            ],
+            ticks: 100,
+            id: `${id_prefix}prestigious_palm`
+        },
+        {
+            outputs: [{ type: 'masterfulmachinery:items', data: { item: 'eidolon:lesser_soul_gem', count: 4 } }],
+            inputs: [
+                { type: 'masterfulmachinery:items', data: { item: 'occultism:spirit_attuned_gem', count: 4 } },
+                { type: 'masterfulmachinery:items', data: { item: 'eidolon:ender_calx', count: 8 } },
+                { type: 'masterfulmachinery:items', data: { item: 'atum:nepthys_godshard', count: 1 } },
+                {
+                    type: 'masterfulmachinery:fluids',
+                    perTick: true,
+                    data: { fluid: 'bloodmagic:life_essence_fluid', amount: 10 }
+                }
+            ],
+            ticks: 400,
+            id: `${id_prefix}lesser_soul_gem`
         }
     ];
 
