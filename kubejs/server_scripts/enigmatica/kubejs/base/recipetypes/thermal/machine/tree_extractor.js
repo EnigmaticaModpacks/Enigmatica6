a9048d998eb01c8381d03cb955cd18468923abc6
MuteTiefling <mutetiefling@gmail.com>
2022-03-27 18:28:46 -0400

    Make Blood available from trees again

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/thermal/machine/tree_extractor.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/thermal/machine/tree_extractor.js
@@ -2,15 +2,17 @@ onEvent('recipes', (event) => {
     treeRegistry.forEach((treeCategories) => {
         treeCategories.trees.forEach((tree) => {
             if (tree.sap) {
-                event.custom({
-                    type: 'thermal:tree_extractor',
-                    trunk: tree.trunk,
-                    leaves: tree.leaf,
-                    result: {
-                        fluid: tree.sap,
-                        amount: tree.rate.living
-                    }
-                });
+                if (tree.rate.living > 0) {
+                    event.custom({
+                        type: 'thermal:tree_extractor',
+                        trunk: tree.trunk,
+                        leaves: tree.leaf,
+                        result: {
+                            fluid: tree.sap,
+                            amount: tree.rate.living
+                        }
+                    });
+                }
             }
         });
     });
