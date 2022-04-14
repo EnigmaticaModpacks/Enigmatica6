a379efb2e96f48b855f749ee260c4eab6538a506
MuteTiefling <mutetiefling@gmail.com>
2022-04-14 13:15:49 -0400

    [Expert] Remove dependency on IE coke oven
    It's slow, you use it once, and then you're done forever. No point.

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/thermal/replace_input.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/thermal/replace_input.js
@@ -5,16 +5,6 @@ onEvent('recipes', (event) => {
 
     const recipes = [
         {
-            replaceTarget: { id: 'thermal:machine_pyrolyzer' },
-            toReplace: 'thermal:machine_frame',
-            replaceWith: Item.of('thermal:fluid_cell').ignoreNBT()
-        },
-        {
-            replaceTarget: { id: 'thermal:machine_pyrolyzer' },
-            toReplace: '#forge:netherbricks',
-            replaceWith: 'immersiveengineering:blastbrick'
-        },
-        {
             replaceTarget: { id: 'thermal:machine_bottler' },
             toReplace: 'thermal:machine_frame',
             replaceWith: 'thermal:fluid_cell_frame'
