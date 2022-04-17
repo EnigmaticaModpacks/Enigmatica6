a92c8ae661357b8e0ad662397c39d46e4c47912c
theboo <virgin6609@gmail.com>
2022-04-17 11:36:34 -0700

    Remove tags from atum glass/panes in expert

---
+++ kubejs/server_scripts/enigmatica/kubejs/expert/tags/items/forge/glass_panes.js
@@ -0,0 +1,11 @@
+onEvent('item.tags', (event) => {
+    if (global.isExpertMode == false) {
+        return;
+    }
+    event
+        .removeAllTagsFrom('atum:crystal_glass_pane');
+    colors.forEach((color) => {
+        event
+            .removeAllTagsFrom('atum:' + color + '_stained_crystal_glass_pane')
+    })
+});
