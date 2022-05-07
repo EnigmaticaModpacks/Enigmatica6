a86f91a50f5a9757849e8427feac236c0d2571ca
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-05-07 21:22:42 +0200

    Add named laser lens constants

---
+++ kubejs/server_scripts/enigmatica/kubejs/constants/industrialforegoing.js
@@ -0,0 +1,22 @@
+//priority: 1000
+
+const industrialforegoing = {
+  laser_lens: {
+    white:      'industrialforegoing:laser_lens0',
+    orange:     'industrialforegoing:laser_lens1',
+    magenta:    'industrialforegoing:laser_lens2',
+    light_blue: 'industrialforegoing:laser_lens3',
+    yellow:     'industrialforegoing:laser_lens4',
+    lime:       'industrialforegoing:laser_lens5',
+    pink:       'industrialforegoing:laser_lens6',
+    gray:       'industrialforegoing:laser_lens7',
+    light_gray: 'industrialforegoing:laser_lens8',
+    cyan:       'industrialforegoing:laser_lens9',
+    purple:     'industrialforegoing:laser_lens10',
+    blue:       'industrialforegoing:laser_lens11',
+    brown:      'industrialforegoing:laser_lens12',
+    green:      'industrialforegoing:laser_lens13',
+    red:        'industrialforegoing:laser_lens14',
+    black:      'industrialforegoing:laser_lens15'
+  }
+}
