e9a01508fc5fc55d8cbc92a11955f3c8f06c0c48
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-05-08 20:37:32 +0200

    bump modpack version to 1.3.0, for future release

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -29,11 +29,11 @@ $MODPACK_NAME = "Enigmatica6"
 $CLIENT_NAME = "Enigmatica6"
 
 # Version Of The Modpack
-$MODPACK_VERSION = "1.2.1"
+$MODPACK_VERSION = "1.3.0"
 
 # Last Version Of The Modpack
 # Needed For Changelog Parsing
-$LAST_MODPACK_VERSION = "1.2.0"
+$LAST_MODPACK_VERSION = "1.2.1"
 
 # =====================================================================//
 #  CHANGELOG SETTINGS
