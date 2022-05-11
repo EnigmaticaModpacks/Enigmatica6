0eeb8aa8ea1d528d39c2032694a28db30fc8d3f7
MuteTiefling <mutetiefling@gmail.com>
2022-05-11 12:41:46 -0400

    [Expert] Hide disabled routing node recipe

--- kubejs/client_scripts/constants.js
+++ kubejs/client_scripts/constants.js
@@ -582,7 +582,8 @@ const recipesToHide = [
             'bloodmagic:alchemytable/router_filter',
             'bloodmagic:alchemytable/tag_router_filter',
             'bloodmagic:alchemytable/enchant_router_filter',
-            'bloodmagic:alchemytable/composite_router_filter'
+            'bloodmagic:alchemytable/composite_router_filter',
+            'bloodmagic:alchemytable/mod_router_filter'
         ]
     },
     {
@@ -846,22 +847,22 @@ const craftedBees = [
 ];
 
 const industrialforegoing = {
-  laser_lens: {
-    white:      'industrialforegoing:laser_lens0',
-    orange:     'industrialforegoing:laser_lens1',
-    magenta:    'industrialforegoing:laser_lens2',
-    light_blue: 'industrialforegoing:laser_lens3',
-    yellow:     'industrialforegoing:laser_lens4',
-    lime:       'industrialforegoing:laser_lens5',
-    pink:       'industrialforegoing:laser_lens6',
-    gray:       'industrialforegoing:laser_lens7',
-    light_gray: 'industrialforegoing:laser_lens8',
-    cyan:       'industrialforegoing:laser_lens9',
-    purple:     'industrialforegoing:laser_lens10',
-    blue:       'industrialforegoing:laser_lens11',
-    brown:      'industrialforegoing:laser_lens12',
-    green:      'industrialforegoing:laser_lens13',
-    red:        'industrialforegoing:laser_lens14',
-    black:      'industrialforegoing:laser_lens15'
-  }
+    laser_lens: {
+        white: 'industrialforegoing:laser_lens0',
+        orange: 'industrialforegoing:laser_lens1',
+        magenta: 'industrialforegoing:laser_lens2',
+        light_blue: 'industrialforegoing:laser_lens3',
+        yellow: 'industrialforegoing:laser_lens4',
+        lime: 'industrialforegoing:laser_lens5',
+        pink: 'industrialforegoing:laser_lens6',
+        gray: 'industrialforegoing:laser_lens7',
+        light_gray: 'industrialforegoing:laser_lens8',
+        cyan: 'industrialforegoing:laser_lens9',
+        purple: 'industrialforegoing:laser_lens10',
+        blue: 'industrialforegoing:laser_lens11',
+        brown: 'industrialforegoing:laser_lens12',
+        green: 'industrialforegoing:laser_lens13',
+        red: 'industrialforegoing:laser_lens14',
+        black: 'industrialforegoing:laser_lens15'
+    }
 };
