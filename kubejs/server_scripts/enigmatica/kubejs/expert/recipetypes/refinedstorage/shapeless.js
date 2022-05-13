3121a26fa6d09451f7af869dfbc8477bd0996f49
MuteTiefling <mutetiefling@gmail.com>
2022-05-13 06:47:34 -0400

    fairer storage parts upgrades

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/refinedstorage/shapeless.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/refinedstorage/shapeless.js
@@ -10,9 +10,9 @@ onEvent('recipes', (event) => {
             id: `${id_prefix}1k_to_16k_conversion`
         },
         {
-            output: 'refinedstorage:16k_storage_part',
+            output: 'refinedstorage:64k_storage_part',
             inputs: ['refinedstorage:4k_storage_part'],
-            id: `${id_prefix}4k_to_16k_conversion`
+            id: `${id_prefix}4k_to_64k_conversion`
         },
         {
             output: 'refinedstorage:1024k_fluid_storage_part',
@@ -20,9 +20,9 @@ onEvent('recipes', (event) => {
             id: `${id_prefix}64k_fluid_to_1024k_fluid_conversion`
         },
         {
-            output: 'refinedstorage:1024k_fluid_storage_part',
+            output: 'refinedstorage:4096k_fluid_storage_part',
             inputs: ['refinedstorage:256k_fluid_storage_part'],
-            id: `${id_prefix}256k_fluid_to_1024k_fluid_conversion`
+            id: `${id_prefix}256k_fluid_to_4096k_fluid_conversion`
         }
     ];
 
