d8f22ae30f33d40573a80d21a2881f85a2baaa08
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-05-15 18:35:20 +0200

    Ignore warnings turned error in debug mode

--- .github/actions/log-kubejs-warnings.js
+++ .github/actions/log-kubejs-warnings.js
@@ -8,11 +8,11 @@ const server = fs.readFileSync('logs/kubejs/server.txt', 'utf-8');
 let code = 0;
 
 server.split(/\r?\n/).forEach((line) => {
-    if (line.includes('[WARN ]')) {
-        console.log(line);
-    } else if (line.includes('[ERR  ]')) {
+    if (line.includes('[ERR  ] Error')) {
         console.log(`::warning::${line}`);
         // code = 1;
+    } else if (line.includes('[ERR  ]') || line.includes('[WARN ]')) {
+        console.log(line);
     }
 });
 
