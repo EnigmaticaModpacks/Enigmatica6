4195f812ec424ddad5b4072b15aeebd9189e5042
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-05-15 22:09:06 +0200

    does.. this fix pressure stuff?

--- kubejs/server_scripts/enigmatica/kubejs/normal/recipetypes/pneumaticcraft/pressure_chamber.js
+++ kubejs/server_scripts/enigmatica/kubejs/normal/recipetypes/pneumaticcraft/pressure_chamber.js
@@ -7,28 +7,28 @@ onEvent('recipes', (event) => {
         {
             inputs: [
                 { type: 'pneumaticcraft:stacked_item', tag: 'forge:nuggets/gold_copper', count: 2 },
-                { tag: 'forge:slimeballs' },
-                { item: 'pneumaticcraft:plastic' }
+                { tag: 'forge:slimeballs', count: 1 },
+                { item: 'pneumaticcraft:plastic', count: 1 }
             ],
             pressure: 1.0,
-            results: [{ item: 'pneumaticcraft:capacitor' }],
+            results: [{ item: 'pneumaticcraft:capacitor', count: 1 }],
             id: 'pneumaticcraft:pressure_chamber/capacitor'
         },
         {
             inputs: [
                 { type: 'pneumaticcraft:stacked_item', tag: 'forge:nuggets/gold_copper', count: 3 },
-                { tag: 'forge:dusts/redstone' },
-                { item: 'pneumaticcraft:plastic' }
+                { tag: 'forge:dusts/redstone', count: 1 },
+                { item: 'pneumaticcraft:plastic', count: 1 }
             ],
             pressure: 1.0,
-            results: [{ item: 'pneumaticcraft:transistor' }],
+            results: [{ item: 'pneumaticcraft:transistor', count: 1 }],
             id: 'pneumaticcraft:pressure_chamber/transistor'
         },
         {
             inputs: [
                 { type: 'pneumaticcraft:stacked_item', tag: 'forge:nuggets/gold_copper', count: 3 },
                 { type: 'pneumaticcraft:stacked_item', item: 'minecraft:redstone_torch', count: 2 },
-                { item: 'pneumaticcraft:plastic' }
+                { item: 'pneumaticcraft:plastic', count: 1 }
             ],
             pressure: 1.5,
             results: [{ item: 'pneumaticcraft:empty_pcb', count: 3 }],
