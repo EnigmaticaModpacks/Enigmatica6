13e15c9dffed29f941815fa54885203ca7f1bda9
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-05-17 09:43:58 +0200

    Name base pneumaticcraft assembly recipes

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/pneumaticcraft/assembly.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/pneumaticcraft/assembly.js
@@ -1,18 +1,23 @@
 onEvent('recipes', (event) => {
+    const id_prefix = 'enigmatica:base/pneumaticcraft/assembly_/';
+
     recipes = [
         {
             input: '#forge:ingots/compressed_iron',
             input_count: 4,
             output: { item: 'pneumaticcraft:elevator_frame', count: 8 },
-            program: 'drill'
+            program: 'drill',
+            id: `${id_prefix}elevator_frame`
         },
         {
             input: 'pneumaticcraft:reinforced_brick_wall',
             input_count: 6,
             output: { item: 'pneumaticcraft:cannon_barrel', count: 2 },
-            program: 'drill'
+            program: 'drill',
+            id: `${id_prefix}cannon_barrel`
         }
     ];
+
     recipes.forEach((recipe) => {
         let constructed_input = recipe.input.charAt(0) == '#' ? { tag: recipe.input.slice(1) } : { item: recipe.input };
         if (recipe.input_count) {
@@ -20,14 +25,11 @@ onEvent('recipes', (event) => {
             constructed_input.count = recipe.input_count;
         }
 
-        let re = event.custom({
+        event.custom({
             type: `pneumaticcraft:assembly_${recipe.program}`,
             input: constructed_input,
             result: recipe.output,
             program: recipe.program
-        });
-        if (recipe.id) {
-            re.id(recipe.id);
-        }
+        }).id(recipe.id.replace('assembly_', `assembly_${recipe.program}`));
     });
 });
