6f8bb0be6668abe25e5a51edd3ded46e45882080
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-05-17 12:00:02 +0200

    End the fertilizer prefix with /

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/immersiveengineering/fertilizer.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/immersiveengineering/fertilizer.js
@@ -1,32 +1,32 @@
 onEvent('recipes', (event) => {
-    const id_prefix = 'enigmatica:base/immersiveengineering/fertilizer';
+    const id_prefix = 'enigmatica:base/immersiveengineering/fertilizer/';
 
     var data = {
         recipes: [
             {
                 input: 'industrialforegoing:fertilizer',
                 growthModifier: 1.7,
-                id: `${id_prefix}/fertilizer`
+                id: `${id_prefix}fertilizer`
             },
             {
                 input: 'thermal:phytogro',
                 growthModifier: 3.0,
-                id: `${id_prefix}/phyto_gro`
+                id: `${id_prefix}phyto_gro`
             },
             {
                 input: 'botania:fertilizer',
                 growthModifier: 1.5,
-                id: `${id_prefix}/floral_fertilizer`
+                id: `${id_prefix}floral_fertilizer`
             },
             {
                 input: 'farmingforblockheads:red_fertilizer',
                 growthModifier: 2.0,
-                id: `${id_prefix}/red_fertilizer`
+                id: `${id_prefix}red_fertilizer`
             },
             {
                 input: 'farmingforblockheads:green_fertilizer',
                 growthModifier: 1.5,
-                id: `${id_prefix}/green_fertilizer`
+                id: `${id_prefix}green_fertilizer`
             }
         ]
     };
