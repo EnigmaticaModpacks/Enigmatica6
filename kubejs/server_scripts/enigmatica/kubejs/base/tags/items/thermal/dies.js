ccb0cba7ccdfdcd590e622a4c6b4ed951277c51b
MuteTiefling <mutetiefling@gmail.com>
2022-05-28 15:30:17 -0400

    Fix dies being consumed by Thermal Machines
    Resolves: #4914

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/thermal/dies.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/thermal/dies.js
@@ -1,13 +1,16 @@
 onEvent('item.tags', (event) => {
     sharedDies.forEach((die) => {
+        event.add(`thermal:crafting/dies`, [`immersiveengineering:mold_${die.immersiveEngineeringName}`]);
         event.add(`thermal:crafting/dies/${die.thermalName}`, [
             `immersiveengineering:mold_${die.immersiveEngineeringName}`
         ]);
     });
+
     thermalDies.forEach((dieName) => {
         event.add('thermal:crafting/dies', [`#thermal:crafting/dies/${dieName}`]);
         event.add(`thermal:crafting/dies/${dieName}`, [`thermal:press_${dieName}_die`]);
     });
+
     immersiveEngineeringDies.forEach((dieName) => {
         event.add('thermal:crafting/dies', [`#thermal:crafting/dies/${dieName}`]);
         event.add(`thermal:crafting/dies/${dieName}`, [`immersiveengineering:mold_${dieName}`]);
