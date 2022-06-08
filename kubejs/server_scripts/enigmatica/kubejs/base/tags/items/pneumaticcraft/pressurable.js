d7fdce85199b9a1a4e19e6d7331497c98dd3314a
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-06-08 12:13:48 +0200

    Tag pneumaticcraft items that can contain pressure

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/pneumaticcraft/pressurable.js
@@ -0,0 +1,19 @@
+onEvent('item.tags', (event) => {
+  var items = [
+    /pneumaticcraft:(reinforced_)?air_canister/, // 10 & 20 bar canisters
+    /pneumaticcraft:(\w*_)?drone/,               // all 5 craftable drones
+    /pneumaticcraft:pneumatic_\w+/,              // wrench and armor pieces
+
+    'pneumaticcraft:vortex_cannon',
+    'pneumaticcraft:manometer',
+    'pneumaticcraft:logistics_configurator',
+    'pneumaticcraft:amadron_tablet',
+    'pneumaticcraft:minigun',
+    'pneumaticcraft:camo_applicator',
+    'pneumaticcraft:jackhammer',
+  ];
+
+  tags.forEach((tag) => {
+      event.get('pneumaticcraft:pressurable').add(items);
+  });
+});
