7df02abc4e4327cb44e60fd3de113d8486eed520
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-06-09 19:20:34 +0200

    Update to use the official curseforge api
    An official api key has yet to be obtained.

--- .github/actions/ci/node/curse-distributable.js
+++ .github/actions/ci/node/curse-distributable.js
@@ -3,26 +3,32 @@ const axios = require('axios');
 const https = require('https');
 const clc = require('cli-color');
 
-const minecraftinstance = JSON.parse(fs.readFileSync('minecraftinstance.json'));
+const axiosThrottle = require('axios-request-throttle');
+axiosThrottle.use(axios, { requestsPerSecond: 4 });
 
-https.globalAgent.maxSockets = 5; // concurrent api requests
+const minecraftinstance = JSON.parse(fs.readFileSync('minecraftinstance.json'));
 
 const no_3rd_party_downloads = [];
 
 const requests = [];
 
+axios.defaults.headers.common = {
+    'Accept': 'application/json',
+    'x-api-key': `<api key>`,
+}
+
 minecraftinstance.installedAddons.forEach(addon => {
-    requests.push(axios.get(`https://auto.xmdhs.top/curse/api/v2/addon/${addon.addonID}`)
+    requests.push(axios.get(`https://api.curseforge.com/v1/mods/${addon.addonID}`)
         .then(response => {
-            const allowed = String(response.data.allowModDistribution);
+            const allowed = String(response.data.data.allowModDistribution);
 
             let color = clc.yellow;
             if(allowed === 'true') color = clc.green;
             if(allowed === 'false') color = clc.red;
 
-            console.log(response.data.name, color(allowed));
+            console.log(response.data.data.name, color(allowed));
 
-            if(response.data.allowModDistribution === false) no_3rd_party_downloads.push(response.data.name);
+            if(response.data.data.allowModDistribution === false) no_3rd_party_downloads.push(response.data.data.name);
         })
         .catch(error => {
             console.log(error.response.status);
@@ -34,6 +40,6 @@ Promise.all(requests)
     .then(() => {
         console.log(no_3rd_party_downloads);
         if (no_3rd_party_downloads.length > 0) {
-            console.log(`::warning::${no_3rd_party_downloads.length} mods have opted out of distribution: ${no_3rd_party_downloads.join(', ')}`);
+            console.log(`::warning::${no_3rd_party_downloads.length} mods would like to get paid for their work: ${no_3rd_party_downloads.join(', ')}`);
         }
     });
