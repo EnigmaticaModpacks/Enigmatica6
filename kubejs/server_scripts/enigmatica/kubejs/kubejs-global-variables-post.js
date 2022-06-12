ce4c4c204135bbaa789099031c692b9d56b665cf
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-06-12 17:08:01 +0200

    Experiment with keys(this)

---
+++ kubejs/server_scripts/enigmatica/kubejs/kubejs-global-variables-post.js
@@ -0,0 +1,4 @@
+// priority: 10000
+
+console.log('kubejs-global-variables-post');
+console.log(keys(this));
---
+++ kubejs/server_scripts/enigmatica/kubejs/kubejs-global-variables-pre.js
@@ -0,0 +1,4 @@
+// priority: -10000
+
+console.log('kubejs-global-variables-pre');
+console.log(keys(this));
