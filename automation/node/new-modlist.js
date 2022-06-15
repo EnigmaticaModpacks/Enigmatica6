1fb3a70633f921cb204e8515f0db37c02710980a
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-06-15 22:01:18 +0200

    Stash initial modlist regenerator

---
+++ automation/node/new-modlist.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const axios = require('axios');
+
+const { execSync } = require('child_process');
+
+argv_token  = process.argv[2];
+argv_owner  = process.argv[3];
+argv_repo   = process.argv[4];
+argv_branch = process.argv[5];
+
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({ auth: argv_token });
+
+const minecraftinstance_json = JSON.parse(fs.readFileSync(`${__dirname}/../../minecraftinstance.json`));
+
+const settings_ps1 = fs.readFileSync(`${__dirname}/../../automation/settings.ps1`).toString();
+const CLIENT_NAME = settings_ps1.match(/\$CLIENT_NAME = "(.*)"/)[1];
+const MODPACK_VERSION = settings_ps1.match(/\$MODPACK_VERSION = "(.*)"/)[1];
+
+const header = `## ${CLIENT_NAME} - ${MODPACK_VERSION}`;
+console.log(header);
+
+//
+
+function by_project_name(project_a, project_b) {
+    if      (project_a.name.toLowerCase() > project_b.name.toLowerCase()) return +1;
+    else if (project_a.name.toLowerCase() < project_b.name.toLowerCase()) return -1;
+    return 0;
+}
+
+const projects = [];
+
+const max = minecraftinstance_json.installedAddons.length;
+
+function next(i) {
+    const installedAddon = minecraftinstance_json.installedAddons[i];
+
+    if (installedAddon) {
+        const melanx_url = `https://curse.melanx.de/project/${installedAddon.addonID}`;
+        console.log(`${i+1}/${max}: ${melanx_url}`);
+
+        axios.get(melanx_url)
+            .then(response => {
+                const project = response.data;
+                project.file = {id: installedAddon.installedFile.id, name: installedAddon.installedFile.fileName};
+
+                console.log(project);
+                projects.push(project);
+
+                next(i+1);
+            })
+            .catch(error => {
+                next(i);
+            });
+    } else {
+        done();
+    }
+}
+
+function done() {
+    projects.sort(by_project_name);
+
+    const readme = [];
+    readme.push(header);
+
+    projects.forEach(project => {
+        readme.push(`- [${project.file.name}](${project.website}/files/${project.file.id}) (by [${project.owner}](https://www.curseforge.com/members/${project.owner.toLowerCase()}/projects))`)
+    });
+
+    fs.writeFileSync(`${__dirname}/../../MODLIST.md`, readme.join('\n') + '\n');
+}
+
+//
+
+// next(0);
+
+let params = {
+    owner: argv_owner,
+    repo:  argv_repo,
+    path: 'MODLIST.md',
+    ref: argv_branch,
+};
+
+octokit.rest.repos.getContent(params).then(response => {
+    console.log(response);
+    console.log(response.data.sha);
+
+    octokit.rest.repos.createOrUpdateFileContents({
+        ...params,
+        message: 'Update MODLIST.md',
+        content: Buffer.from(fs.readFileSync(`${__dirname}/../../MODLIST.md`)).toString('base64'),
+        sha: response.data.sha,
+        branch: params["ref"],
+        author: { // attribute the changelog to whomever last edited minecraftinstance.json:)
+            name: execSync('git log -n 1 --pretty=format:%an minecraftinstance.json').toString().trim(),
+            email: execSync('git log -n 1 --pretty=format:%ae minecraftinstance.json').toString().trim(),
+        }
+    })
+
+});
+
