a6320eb7d6fc239115eb2b9f880d852df2f07cd2
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-06-15 13:45:27 +0200

    Fix `kubejs:catnip_larva` not being reported

--- .github/actions/ci/log-kubejs-warnings.js
+++ .github/actions/ci/log-kubejs-warnings.js
@@ -2,18 +2,26 @@
 // and determining whether it contained errors or not.
 
 const fs = require('fs');
-
 const server = fs.readFileSync('logs/kubejs/server.txt', 'utf-8');
 
-let code = 0;
+let warnings = [];
 
 server.split(/\r?\n/).forEach((line) => {
-    if (line.includes('[ERR  ] Error') && !line.includes('shadows.menu.PackMenuClient')) {
-        console.log(`::warning::${line}`);
-        code = 1;
-    } else if (line.includes('[ERR  ]') || line.includes('[WARN ]')) {
-        console.log(line);
+
+    // the 2nd reload causes this error, avoid reporting this
+    if (line.includes('shadows.menu.PackMenuClient')) return;
+
+    // get the log level from this line, expected values: [ undefined, INFO, WARN, ERR ]
+    const in_brackets = line.match(/\[\d{2}:\d{2}:\d{2}] \[([A-Z\s]+)]/)?.[1];
+
+    switch (in_brackets) {
+        case 'ERR  ': warnings.push(line);
+        case 'WARN ': console.log(line);
     }
 });
 
-process.exit(code);
+warnings.forEach(warning => {
+    console.log(`::warning::${warning}`);
+});
+
+process.exit(warnings.length === 0 ? 0 : 1);
