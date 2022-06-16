b973606d7b640c0e6a8ab2d77e97228e78105a9d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2022-06-16 07:47:20 +0200

    Cleanup the modlist generator

--- automation/node/modlist-generate.js
+++ automation/node/modlist-generate.js
@@ -2,13 +2,59 @@ const fs = require('fs');
 const axios = require('axios');
 
 const minecraftinstance_json = JSON.parse(fs.readFileSync(`${__dirname}/../../minecraftinstance.json`));
+const total_addons = minecraftinstance_json.installedAddons.length;
 
-const settings_ps1 = fs.readFileSync(`${__dirname}/../../automation/settings.ps1`).toString();
+// grab the name and the version for the markdown header from the pwsh settings
+const settings_ps1    = fs.readFileSync(`${__dirname}/../../automation/settings.ps1`).toString();
 const CLIENT_NAME     = settings_ps1.match(/\$CLIENT_NAME = "(.*)"/)[1];
 const MODPACK_VERSION = settings_ps1.match(/\$MODPACK_VERSION = "(.*)"/)[1];
 
-const header = `## ${CLIENT_NAME} - ${MODPACK_VERSION}`;
-console.log(header);
+const readme = [`## ${CLIENT_NAME} - ${MODPACK_VERSION}`];
+console.log(readme[0]);
+
+const projects = [];
+
+function next(i) {
+    const installedAddon = minecraftinstance_json.installedAddons[i];
+    if (installedAddon) {
+
+        // this is the api behind the current modlist generator, not some custom one :)
+        const melanx_url = `https://curse.melanx.de/project/${installedAddon.addonID}`;
+
+        // log the progress to the console, old one does not.
+        console.log(`${i+1}/${total_addons}: ${melanx_url}`);
+
+        axios.get(melanx_url)
+            .then(response => {
+                const project = response.data;
+
+                // the api itself does not return the filename without an additional call, but we already know it.
+                project.file = {id: installedAddon.installedFile.id, name: installedAddon.installedFile.fileName};
+
+                console.log(project);
+                projects.push(project);
+
+                // {
+                //     project: 345779,
+                //     slug: 'plonk',
+                //     name: 'Plonk',
+                //     owner: 'BlueAgent',
+                //     summary: 'Place items down in the world',
+                //     distribution: true,
+                //     website: 'https://www.curseforge.com/minecraft/mc-mods/plonk',
+                //     thumbnail: 'https://media.forgecdn.net/avatars/230/201/637059414601035196.png',
+                //     file: { id: 3299406, name: 'plonk-1.16.5-9.0.8.jar' }
+                // }
+
+                next(i + 1); // process the next addon synchronously.
+            })
+            .catch(error => {
+                next(i); // the api has a tendency to be slow (or even time out), so in case of failure retry the current addon.
+            });
+    } else {
+        done(); // all the addons have been processed, finalize.
+    }
+}
 
 // match the sorting of MelanX/ModListCreator
 function by_project_name(project_a, project_b) {
@@ -17,40 +63,8 @@ function by_project_name(project_a, project_b) {
     return 0;
 }
 
-const projects = [];
-
-const total = minecraftinstance_json.installedAddons.length;
-
-function next(i) {
-    const installedAddon = minecraftinstance_json.installedAddons[i];
-
-    if (installedAddon) {
-        const melanx_url = `https://curse.melanx.de/project/${installedAddon.addonID}`;
-        console.log(`${i+1}/${total}: ${melanx_url}`);
-
-        axios.get(melanx_url)
-            .then(response => {
-                const project = response.data;
-                project.file = {id: installedAddon.installedFile.id, name: installedAddon.installedFile.fileName};
-
-                console.log(project);
-                projects.push(project);
-
-                next(i+1);
-            })
-            .catch(error => {
-                next(i);
-            });
-    } else {
-        done();
-    }
-}
-
 function done() {
     projects.sort(by_project_name);
-
-    const readme = [];
-    readme.push(header);
 
     projects.forEach(project => {
         readme.push(`- [${project.file.name}](${project.website}/files/${project.file.id}) (by [${project.owner}](https://www.curseforge.com/members/${project.owner.toLowerCase()}/projects))`)
@@ -59,5 +73,4 @@ function done() {
     fs.writeFileSync(`${__dirname}/../../MODLIST.md`, readme.join('\n') + '\n');
 }
 
-// start
-next(0);
+next(0); // all variables and functions have been defined above, now we can start it here.
