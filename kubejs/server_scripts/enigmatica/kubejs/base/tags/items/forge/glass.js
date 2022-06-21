ad6dbb71f860f736b918b2f61d5895c4e8f8bb8a
MuteTiefling <mutetiefling@gmail.com>
2022-06-21 10:02:05 -0400

    Remove glass panes from the glass tag
    Remove glass panes from the glass tag. Resolves #5024

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/glass.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/forge/glass.js
@@ -8,5 +8,7 @@ onEvent('item.tags', (event) => {
                 `connectedglass:scratched_glass_${color}`,
                 `quark:${color}_framed_glass`
             ]);
+
+        event.get('forge:glass').remove([`#chipped:${color}_stained_glass_pane`]);
     });
 });
