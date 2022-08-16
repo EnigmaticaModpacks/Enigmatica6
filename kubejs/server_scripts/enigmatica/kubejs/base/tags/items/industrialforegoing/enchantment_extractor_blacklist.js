3248a5f0949d75fb683aee53eff6b18ff4ad50a9
MuteTiefling <mutetiefling@gmail.com>
2022-08-15 22:57:24 -0400

    Blacklist anvils from PNC and IF enchant extraction
    Partially resolves #5167. Leaves tome of scrapping as I don't see a way to blacklist items from that...

--- kubejs/server_scripts/enigmatica/kubejs/base/tags/items/industrialforegoing/enchantment_extractor_blacklist.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/tags/items/industrialforegoing/enchantment_extractor_blacklist.js
@@ -1,10 +1,5 @@
 onEvent('item.tags', (event) => {
-    event
-        .get('industrialforegoing:enchantment_extractor_blacklist')
-        .add('tetra:modular_double')
-        .add('tetra:modular_shield')
-        .add('tetra:modular_single')
-        .add('tetra:modular_sword')
-        .add('tetra:modular_crossbow')
-        .add('tetra:modular_bow');
+    const items = ['minecraft:anvil', /tetra:modular_.*/];
+
+    event.get('industrialforegoing:enchantment_extractor_blacklist').add(items);
 });
