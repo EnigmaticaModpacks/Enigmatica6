fe69cf0f3c77a474509a375410fc1737b2513034
MuteTiefling <mutetiefling@gmail.com>
2022-10-08 18:14:12 -0400

    Fix Thermal's Centrifuge Honey Recipe
    fixes 5211

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/thermal/machine/centrifuge.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/thermal/machine/centrifuge.js
@@ -7,10 +7,11 @@ onEvent('recipes', (event) => {
         {
             input: 'kubejs:ground_meat',
             outputs: [Item.of('minecraft:bone_meal').withChance(0.15), Fluid.of('industrialforegoing:meat', 100)],
+            energy: 2000,
             id: `${id_prefix}meat`
         }
     ];
     recipes.forEach((recipe) => {
-        event.recipes.thermal.centrifuge(recipe.outputs, recipe.input).id(recipe.id);
+        event.recipes.thermal.centrifuge(recipe.outputs, recipe.input).energy(recipe.energy).id(recipe.id);
     });
 });
