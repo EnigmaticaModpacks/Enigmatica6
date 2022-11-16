adf8bd86a9e96cdb8e96e0301eea50ac67c5046a
MuteTiefling <mutetiefling@gmail.com>
2022-11-15 22:08:14 -0500

    Update gamestages.js

--- kubejs/server_scripts/enigmatica/kubejs/expert/item/gamestages.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/item/gamestages.js
@@ -14,7 +14,7 @@ onEvent('item.right_click', (event) => {
             let stage_name = item.split(':')[1];
             if (stage_name == 'masterbloodorb') stage_name = 'master_blood_orb';
             if (stage_name == 'soulgemlesser') stage_name = 'lesser_tartaric_gem';
-            if (stage_name == 'chalk_red') stage_name = 'chalk_red';
+            if (stage_name == 'chalk_red') stage_name = 'red_chalk';
 
             if (!event.getEntity().stages.has(`${stage_name}`)) {
                 event.player.tell(`Gamestage Granted: ${titleCase(stage_name.replace(/_+/g, ' '))}`);
