db2175b3b1d16b66cf36cf4683422e84699c18cb
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2026-07-25 08:03:14 +0200

    bump ServerStarter to 2.4.6

--- server_files_expert/start-server.bat
+++ server_files_expert/start-server.bat
@@ -12,16 +12,16 @@ CD "%~dp0" >nul 2>&1
 
 :CHECK
 REM Check if serverstarter JAR is already downloaded
-IF NOT EXIST "%cd%\serverstarter-2.4.0.jar" (
+IF NOT EXIST "%cd%\serverstarter-2.4.6.jar" (
 	ECHO serverstarter binary not found, downloading serverstarter...
-	%SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground https://github.com/EnigmaticaModpacks/ServerStarter/releases/download/v2.4.0/serverstarter-2.4.0.jar "%cd%\serverstarter-2.4.0.jar"
+	%SYSTEMROOT%\SYSTEM32\bitsadmin.exe /rawreturn /nowrap /transfer starter /dynamic /download /priority foreground https://github.com/EnigmaticaModpacks/ServerStarter/releases/download/v2.4.6/serverstarter-2.4.6.jar "%cd%\serverstarter-2.4.6.jar"
    GOTO MAIN
 ) ELSE (
    GOTO MAIN
 )
 
 :MAIN
-java -jar serverstarter-2.4.0.jar
+java -jar serverstarter-2.4.6.jar
 GOTO EOF
 
 :EOF
